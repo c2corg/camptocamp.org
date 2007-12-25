9355d5524474acb0e7159a51de0d05686fa9d4bf
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2007-12-25 19:48:01 +0000

    removed activities filter for huts
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@12 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- lib/model/doctrine/Hut.class.php
+++ lib/model/doctrine/Hut.class.php
@@ -60,7 +60,6 @@ class Hut extends BaseHut
         elseif (c2cPersonalization::isMainFilterSwitchOn())
         {
             self::filterOnRegions($q);
-            self::filterOnActivities($q);
         }
 
         return $pager;
