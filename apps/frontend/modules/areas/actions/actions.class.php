a0accaba46a13c8e70f749b3fdaabd7c86962920
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2008-01-02 22:29:40 +0000

    fixed a bug in areas search, dued to a variable name typo
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@21 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/areas/actions/actions.class.php
+++ apps/frontend/modules/areas/actions/actions.class.php
@@ -122,7 +122,7 @@ class areasActions extends documentsActions
         if ($aname = $this->getRequestParameter('anam'))
         {
             $conditions[] = 'mi.search_name LIKE remove_accents(?)';
-            $values[] = "%$sname%";
+            $values[] = "%$aname%";
         }
 
         if ($atyp = $this->getRequestParameter('atyp'))
