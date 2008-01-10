d225953fc933306e5d55efcad7e3e0a79b9cc482
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2008-01-10 23:22:11 +0000

    fixed typo in list headers field codes
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@37 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/areas/templates/_list_header.php
+++ apps/frontend/modules/areas/templates/_list_header.php
@@ -1,5 +1,5 @@
 <?php
-echo header_list_tag('aname', 'name');
-echo header_list_tag('atype', 'area_type');
+echo header_list_tag('anam', 'name');
+echo header_list_tag('atyp', 'area_type');
 echo images_header_list_tag();
 echo comments_header_list_tag();
