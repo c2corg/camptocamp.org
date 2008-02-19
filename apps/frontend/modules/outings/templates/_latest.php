e0ae57e5eb901a55be36050a32f76fcdeae3261f
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2008-02-19 11:42:48 +0000

    added processing to display range info
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@97 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/outings/templates/_latest.php
+++ apps/frontend/modules/outings/templates/_latest.php
@@ -1,7 +1,6 @@
 <div class="latest" id="last-outings">
 <?php
 use_helper('SmartDate', 'Pagination');
-
 include_partial('documents/latest_title', array('module' => 'outings'));
 
 if (count($items) == 0): ?>
@@ -21,10 +20,62 @@ if (count($items) == 0): ?>
                 $date = $timedate;
             }
 
+            // FIXME: test prefered language? (most outings are monolingual)
             $id = $item['id'];
             $lang = $item['OutingI18n'][0]['culture'];
             
             echo link_to($item['OutingI18n'][0]['name'], "@document_by_id_lang?module=outings&id=$id&lang=$lang");
+
+            $geo = $item['geoassociations'];
+            $nb_geo = count($geo);
+            if ($nb_geo == 1)
+            {
+                // FIXME: prefered language?
+                echo ' (' . $geo[0]['AreaI18n'][0]['name'] . ')';
+            }
+            elseif ($nb_geo > 1)
+            {
+                $areas = $types = $regions = array();
+                
+                foreach ($geo as $g)
+                {
+                    // FIXME: prefered language?
+                    if (empty($g['AreaI18n'][0])) continue;
+
+                    $area = $g['AreaI18n'][0];
+                    $types[] = !empty($area['Area']['area_type']) ? $area['Area']['area_type'] : 0;
+                    $areas[] = $area['name'];
+                }
+
+                // use ranges if any
+                $rk = array_keys($types, 1);
+                if ($rk)
+                {
+                    foreach ($rk as $r)
+                    {
+                        $regions[] = $areas[$r];
+                    }
+                }
+                else
+                {
+                    // else use dept/cantons if any
+                    $ak = array_keys($types, 3);
+                    if ($ak)
+                    {
+                        foreach ($ak as $a)
+                        {
+                            $regions[] = $areas[$a];
+                        }
+                    }
+                    else
+                    {
+                        // else use what's left (coutries)
+                        $regions = $areas;
+                    }
+                }
+
+                echo ' (' . implode(', ', $regions) . ')';
+            }
             ?>
             </li>
     <?php endforeach ?>
