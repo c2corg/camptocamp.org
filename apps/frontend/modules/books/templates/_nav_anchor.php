c04280412925cca97b4af817fbe46a40ff886280
Lionel Besson <lionel.besson@gmail.com>
2008-07-18 07:50:14 +0000

    Ajout des raccourcis manquant dans le menu gauche des pages livres
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@246 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/books/templates/_nav_anchor.php
+++ apps/frontend/modules/books/templates/_nav_anchor.php
@@ -11,6 +11,10 @@ $id = $sf_params->get('id');
         <ul>
             <li><?php echo button_anchor('Information', 'data', 'action_informations', $module, $id, $lang); ?></li>
             <li><?php echo button_anchor('Description', 'description', 'action_description', $module, $id, $lang); ?></li>
+            <li><?php echo button_anchor('Linked summits', 'linked_summits', 'action_summits', $module, $id, $lang); ?></li>
+            <li><?php echo button_anchor('Linked routes', 'linked_routes', 'action_routes', $module, $id, $lang); ?></li>
+            <li><?php echo button_anchor('Linked huts', 'linked_huts', 'action_huts', $module, $id, $lang); ?></li>
+            <li><?php echo button_anchor('Linked sites', 'linked_sites', 'action_sites', $module, $id, $lang); ?></li>
             <li><?php echo button_anchor('Images', 'images', 'action_images', $module, $id, $lang); ?></li>
         </ul>
     </div>
