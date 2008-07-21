cbdbd7cd388651a921cd51df8cad4104003335f9
Lionel Besson <lionel.besson@gmail.com>
2008-07-21 15:29:48 +0000

    correction des liens internes
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@252 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- plugins/sfPunBBCodeParserPlugin/lib/sfPunBBCodeParser.class.php
+++ plugins/sfPunBBCodeParserPlugin/lib/sfPunBBCodeParser.class.php
@@ -201,7 +201,7 @@ class sfPunBBCodeParser
 
 	// Check if internal or external link
         if (preg_match('#^https?://'.$_SERVER['SERVER_NAME'].'#', $full_url))
-            return '<a  href="'.$full_url.'">'.$link.'</a>';
+            return '<a href="'.$full_url.'">'.$link.'</a>';
         return '<a class="external_link" href="'.$full_url.'">'.$link.'</a>';
     }
     
