a6f3b4b08552565d928a81c52b49ec717df58ce8
Lionel Besson <lionel.besson@gmail.com>
2008-09-11 09:39:57 +0000

    focus sur le champ 'nom' pour les recherches d'articles, d'images et de contributeurs
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@297 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/images/templates/_filter_form.php
+++ apps/frontend/modules/images/templates/_filter_form.php
@@ -2,6 +2,9 @@
 use_helper('FilterForm', 'Form');
 echo update_on_select_change();
 
+// put focus on the name field on window load
+echo javascript_tag('Event.observe(window, \'load\', function(){$(\'inam\').focus();});');
+
 echo __('Name:') . ' ' . input_tag('inam') . ' ';
 //echo __('author') . ' ' . input_tag('auth') . ' ';
 echo __('categories') . ' ' . field_value_selector('cat', 'mod_images_categories_list', true) . ' ';
--- apps/frontend/modules/users/templates/_filter_form.php
+++ apps/frontend/modules/users/templates/_filter_form.php
@@ -1,6 +1,9 @@
 <?php
 use_helper('FilterForm');
 
+// put focus on the name field on window load
+echo javascript_tag('Event.observe(window, \'load\', function(){$(\'unam\').focus();});');
+
 echo __('User:') . ' ' . input_tag('unam') . ' ';
 echo georef_selector();
 include_partial('areas/areas_selector', array('ranges' => $ranges));
