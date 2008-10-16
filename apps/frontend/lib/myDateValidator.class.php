2a7933aad65d9633a6a021d90bdda3489d277aa5
Lionel Besson <lionel.besson@gmail.com>
2008-10-16 18:39:34 +0000

    Added validator to avoid invalid dates for outings (simply invalid, or in the future)
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@412 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ apps/frontend/lib/myDateValidator.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class myDateValidator extends sfValidator
+{
+  public function execute (&$value, &$error)
+  {
+    $error = $this->getParameter('date_error');
+
+    if (!checkdate($value['month'], $value['day'], $value['year']))
+      return false;
+
+    $today = date("Ymd");
+    $outind_date = $value['year'].$value['month'].$value['day'];
+
+    return $outind_date < $today;
+  }
+
+}
