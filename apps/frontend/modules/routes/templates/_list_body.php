c820dffa079dc6e8ee08f40ccde62c32db030c6e
Lionel Besson <lionel.besson@gmail.com>
2008-10-21 06:35:50 +0000

    don't display activities pictos for cotations in routes list
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@450 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/routes/templates/_list_body.php
+++ apps/frontend/modules/routes/templates/_list_body.php
@@ -2,7 +2,7 @@
 <td><?php echo get_paginated_activities($item['activities']) ?></td>
 <td><?php echo get_paginated_value($item['facing'], 'app_routes_facings') ?></td>
 <td><?php echo displayWithSuffix($item['height_diff_up'], 'meters') ?></td>
-<td><?php echo field_route_ratings_data($item) // helper is included in _list_header ?></td>
+<td><?php echo field_route_ratings_data($item, false) // helper is included in _list_header ?></td>
 <td><?php include_partial('documents/regions4list', array('geoassociations' => $item['geoassociations']))?></td>
 <td><?php echo (strlen($item['geom_wkt'])) ? __('yes') : __('no') ;?></td>
 <td><?php echo (isset($item['nb_images'])) ?  $item['nb_images'] : '' ;?></td>
