5158301292afa90e466d295ec1af928a27464077
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2008-11-11 20:01:44 +0000

    script retrieving BRA from MF website and sending them on the mailing-lists
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@517 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ batch/bulletins-2005.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+export LANG=fr_FR.UTF-8
+
+WORKDIR="/var/vmail/camptocamp.org/meteofrance/work/"
+TYPE="bulletinneige"
+LIST="DEPT74 DEPT73 DEPT38 DEPT04 DEPT05 DEPT06 DEPT2A DEPT2B DEPT66 DEPT31 DEPT09 ANDORRE DEPT64 DEPT65"
+TITLE="Bulletin Neige et Avalanches"
+
+PATH="/usr/bin:/usr/sbin:/bin:/sbin"
+LANG="fr_CH"
+unset http_proxy
+
+TODAY=`date +"%d %b %y"`
+
+# debug
+NOW=`date +"%d%b%Y-%Hh%Mm%Ss"`
+
+if [ ! -d $WORKDIR ] || [ ! -w $WORKDIR ]
+then
+    echo "$0: ERROR: $WORKDIR not accessible !"
+    exit 0
+fi
+
+for DPT in $LIST; do
+
+    URL="http://france.meteofrance.com/france/montagne?19987.path=montagne$TYPE%252F$DPT"
+    FILE=$WORKDIR/$DPT-$TYPE
+
+    # test if server is up && file exists
+    if w3m -dump_head -no-cookie $URL | egrep --quiet '200 OK$'; then
+
+        # dump bulletin to a file
+        curl -s $URL | grep "onlyText bulletinText" | perl -pe 's/.*bulletinText">(.+?)<\/div.*/$1/' | w3m -M -dump -T "text/html" -I "utf-8" -cols 74 > $FILE.txt
+# debug
+#        cp $FILE.txt $WORKDIR/debug/$DPT-$TYPE.$NOW.curl.txt
+
+        echo -e "\n\n\n" >> $FILE.txt
+        sed -i ':a;N;$!ba;s/\n\{2,\}/\n\n/g' $FILE.txt
+# debug
+#        cp $FILE.txt $WORKDIR/debug/$DPT-$TYPE.$NOW.sed.txt
+
+
+
+        # files differ: send it by email
+        if ! cmp --quiet $FILE.mail $FILE.txt; then
+# debug
+#            touch $WORKDIR/debug/$DPT-$TYPE.$NOW.mailsent
+
+            MAILDPT=`echo $DPT | sed -e 's/^dept//i'`
+
+            mv $FILE.txt $FILE.mail
+            mail -a "Content-Type: text/plain; charset=utf-8" -s "$TITLE $DPT - $TODAY" meteofrance-$MAILDPT@camptocamp.org < $FILE.mail
+
+        else
+            rm -f $FILE.txt
+        fi
+    else
+        # problem with server
+        logger "bulletin meteofrance: unable to download $DPT ($URL)"
+        # false
+    fi
+
+done
