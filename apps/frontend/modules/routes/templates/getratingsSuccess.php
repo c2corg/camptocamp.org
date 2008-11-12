b8375954e55a91c0701d7c56fb90e56d5a0fdffe
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2008-11-12 19:50:42 +0000

    removed activities info since it is already available in ratings info
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@521 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/routes/templates/getratingsSuccess.php
+++ apps/frontend/modules/routes/templates/getratingsSuccess.php
@@ -1,7 +1,6 @@
 <?php
 use_helper('Field');
 
-$activities = field_activities_data($data, true);
 $ratings = field_route_ratings_data($data);
 $facing = _get_field_value_in_list(sfConfig::get('app_routes_facings'), $data->get('facing'));
 $height = $data->get('height_diff_up');
@@ -11,7 +10,6 @@ if ($height)
 }
 
 $data = array();
-if ($activities) $data[] = $activities;
 if ($facing) $data[] = $facing;
 if ($height) $data[] = $height;
 if ($ratings) $data[] = $ratings;
