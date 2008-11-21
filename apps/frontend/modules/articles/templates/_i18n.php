870baf8c8d46dd5bb7318e420199cde1a7ecb6bd
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2008-11-21 21:35:21 +0000

    ticket #200: article abstract layout
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@561 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/articles/templates/_i18n.php
+++ apps/frontend/modules/articles/templates/_i18n.php
@@ -1,7 +1,9 @@
 <?php use_helper('sfBBCode', 'SmartFormat'); ?>
 
-<p class="abstract"><?php echo parse_links(parse_bbcode($document->get('abstract'))) ?></p>
+<p class="abstract"><?php echo $document->get('abstract') ?></p>
 
-<?php if ($document->get('description')): ?>
-<p><?php echo parse_links(parse_bbcode($document->get('description'))) ?></p>
-<?php endif ?>
+<?php
+if ($document->get('description'))
+{
+    echo parse_links(parse_bbcode($document->get('description')));
+}
