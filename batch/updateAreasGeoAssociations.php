fce13a6232531b8d48722b8f6bfbc97d7e9f4163
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2009-02-09 22:51:52 +0000

    #185 : (uncompleted) script that update geo associations of documents "contained" in given start areas
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@753 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ batch/updateAreasGeoAssociations.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Batch that creates initial geographic associations (maps and areas) for each document
+ * Must be launched after real data have been loaded
+ *
+ * @version $Id: createInitialAssociations.php 2021 2007-10-10 15:39:53Z fvanderbiest $
+ */
+
+define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
+define('SF_APP',         'frontend');
+define('SF_ENVIRONMENT', 'dev');
+define('SF_DEBUG',       true);
+
+require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+
+// needed for doctrine connection to work
+sfContext::getInstance();
+
+// ids of areas containing objects of whom geoassociations must be updated:
+$areas_ids = array(369, 379);
+
+$i = 0;
+foreach ($areas_ids as $id)
+{
+    // find all doc associated to current area
+    $associated_docs = GeoAssociation::findAllAssociations($id, null, 'linked');
+    foreach ($associated_docs as $doc)
+    {
+        $doc_id = $doc->get('main_id');
+
+        if (!$document = Document::find('Document', $doc_id, array('module'))) continue;
+
+        // no maps are linked to outings, users and other maps
+        $linkToMaps = !in_array($document->get('module'), array('outings', 'users', 'maps'));
+
+
+        gisQuery::createGeoAssociations($doc_id, true, $linkToMaps);
+        
+        // TODO: handle "inherited" geo associations such as those of routes and outings
+
+        $i++;
+    }
+}
+echo "$i documents updated\n";
