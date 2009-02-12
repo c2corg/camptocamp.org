e27cd5813f2cb87a867932a6a2b2d4fb500a1e5c
Lionel Besson <lionel.besson@gmail.com>
2009-02-12 08:06:17 +0000

    replace () by [] in parking names for google map links (closes #212)
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@759 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/lib/helper/GetDirectionsHelper.php
+++ apps/frontend/lib/helper/GetDirectionsHelper.php
@@ -4,7 +4,7 @@ function gmaps_direction_link($from_lat, $from_lon, $to_lat, $to_lon, $to_name, 
 {
     $baseurl = "http://maps.google.com/maps";
     $from_code = (empty($from_lat) || empty($from_lon)) ? '' : "$from_lat,$from_lon";
-    $to_code = "$to_lat,$to_lon" . (empty($to_name) ? '' : "+($to_name)");
+    $to_code = "$to_lat,$to_lon" . (empty($to_name) ? '' : '+%28'.strtr($to_name, array('%28'=>'%5B', '%29'=>'%5D')).'%29');
     $lang_code = "&hl=$lang&ie=UTF8";
     $zoom_code = empty($from_code) ? "&z=12" : '';
 
