277c9faa0e7608c04ef4b3408b0bc43413733c97
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2009-03-28 16:11:44 +0000

    proto for data export script with zoom level
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@854 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ batch/exportGp.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This script retrieves summits, huts and climbing sites in France and generates CSV files used to export to other sites.
+ */
+define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
+define('SF_APP',         'frontend');
+define('SF_ENVIRONMENT', 'prod');
+define('SF_DEBUG',       false);
+
+define('GP_DIR', SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'tmp/');
+
+require_once(SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'apps' . DIRECTORY_SEPARATOR . SF_APP.DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php');
+
+// needed for doctrine connection to work
+$context = sfContext::getInstance();
+
+$zoom_levels = array(
+'10' => 1,
+'20' => 2,
+'50' => 3,
+'100' => 4
+);
+$default_zoom = 5; 
+
+$csv = '';
+foreach(Doctrine_Query::create()->select('a.id')->from('Area a')->where('a.area_type = 1')->limit(3)->execute() as $area)
+{
+    // summits
+    $sql = "SELECT s.id, n.name, s.lon, s.lat, s.elevation FROM summits s, summits_i18n n, areas a " .
+           "WHERE a.id = " . $area->id . " AND s.id = n.id AND s.redirects_to IS NULL AND summit_type = 1 AND s.geom IS NOT NULL AND n.culture = 'fr' " .
+           "AND intersects(a.geom, s.geom) AND a.geom && s.geom " .
+           "ORDER BY s.elevation DESC, n.name ASC";
+    $summits = sfDoctrine::connection()->standaloneQuery($sql)->fetchAll();
+    $i = 0;
+    foreach ($summits as $summit)
+    {
+        $i++;
+        $zoom = $default_zoom;
+        foreach ($zoom_levels as $max => $level)
+        {
+            if ($i <= $max) {
+                $zoom = $level;
+                break;
+            }
+        }
+        $csv .= sprintf('"%s";"%s";"%s";"http://www.camptocamp.org/summits/popup/%d/fr";"%d";"%d";"%d"' . "\n",
+                        $summit['name'], $summit['lon'], $summit['lat'], $summit['id'], $summit['elevation'], $zoom, $area->id);
+    }
+}
+file_put_contents(GP_DIR . 'sommets_c2c.csv', $csv);
+$nb_summits = count($summits);
+echo "Summits exported: $nb_summits\n";
+
+echo "Done.\n";
