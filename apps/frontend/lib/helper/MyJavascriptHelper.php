d6caa80ce5439161fdb6d0fd80b7d34e69d44116
Lionel Besson <lionel.besson@gmail.com>
2009-04-08 15:28:18 +0000

    #272 last 2 known problems solved (double inclusion of prototype caused autocompletion to fail)
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@915 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/lib/helper/MyJavascriptHelper.php
+++ apps/frontend/lib/helper/MyJavascriptHelper.php
@@ -5,6 +5,7 @@
  * that should be put in the head section of the document
  * and the ones that can be left at the end of the body,
  * in order to optimize performances
+ * Look for include_javascripts from symfony to understand what happens here
  */
 
 function include_head_javascripts()
@@ -35,10 +36,40 @@ function include_head_javascripts()
             }
         }
     }
-    return $html;
+    echo $html;
 }
 
 function include_body_javascripts()
 {
-    include_javascripts();
+    $response = sfContext::getInstance()->getResponse();
+    $response->setParameter('javascripts_included', true, 'symfony/view/asset');
+
+    $already_seen = array();
+
+    // prototype is added with position='' by JavascriptHelper. We don't want it here (added in head)
+    $already_seen[javascript_path(sfConfig::get('app_static_url').sfConfig::get('sf_prototype_web_dir').'/js/prototype.js')] = 1;
+
+    $html = '';
+
+    foreach (array('first', '', 'last') as $position)
+    {
+        foreach ($response->getJavascripts($position) as $files)
+        {
+            if (!is_array($files))
+            {
+                $files = array($files);
+            }
+
+            foreach ($files as $file)
+            {
+                $file = javascript_path($file);
+
+                if (isset($already_seen[$file])) continue;
+
+                $already_seen[$file] = 1;
+                $html .= javascript_include_tag($file);
+            }
+        }
+    }
+    echo $html;
 }
