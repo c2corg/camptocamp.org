63b92667eb37e890c996dac2ebf62cacc9c2a6b1
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2009-04-17 19:24:08 +0000

    correction nom de fichier
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@934 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ web/static/js/parkings.js
@@ -0,0 +1,14 @@
+function hide_parkings_unrelated_fields()
+{
+    value = $('public_transportation_rating').options[$('public_transportation_rating').selectedIndex].value;
+    if(value != '3' && value != '0')
+    {
+        $(tp_types).show();
+    }
+    else
+    {
+        $(tp_types).hide();
+    }
+}
+
+Event.observe(window, 'load', hide_parkings_unrelated_fields);
