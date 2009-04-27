49011b8771fdf88f873606d2e60dd0372deecf4e
Lionel Besson <lionel.besson@gmail.com>
2009-04-27 07:29:16 +0000

    - correct site search with parkings - empty prepare_box for languages by default
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@973 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- lib/model/doctrine/generated/BaseSite.class.php
+++ lib/model/doctrine/generated/BaseSite.class.php
@@ -34,6 +34,7 @@ class BaseSite extends BaseDocument
     {
         $this->hasMany('SiteI18n as SiteI18n', array('local' => 'id', 'foreign' => 'id'));
         $this->hasI18nTable('SiteI18n', 'culture');
+        $this->hasMany('Association as associations', array('local' => 'id', 'foreign' => 'linked_id'));
         $this->hasMany('GeoAssociation as geoassociations', array('local' => 'id', 'foreign' => 'main_id'));
         $this->hasMany('DocumentVersion as versions', array('local' => 'id', 'foreign' => 'document_id'));
     }
