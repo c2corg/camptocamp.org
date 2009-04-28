1f709e784c9133c3ae9de5d6582d249237715b9a
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2009-04-28 22:43:20 +0000

    - suppression catégorie d'article "évènement" - design formulaires du topoguide - taille de boite de réponse dans le forum
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@987 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/forums/edit.php
+++ web/forums/edit.php
@@ -318,7 +318,7 @@ if ($is_admmod)
 <?php if ($can_edit_subject): ?>                      <label><?php echo $subject_title; ?>
 						<input class="longinput" type="<?php echo $input_type; ?>" name="req_subject" size="80" maxlength="100" tabindex="<?php echo $cur_index++ ?>" value="<?php echo pun_htmlspecialchars(isset($_POST['req_subject']) ? $_POST['req_subject'] : $cur_post['subject']) ?>" /><br /></label>
 <?php endif; $bbcode_form = 'edit'; $bbcode_field = 'req_message'; require PUN_ROOT.'mod_easy_bbcode.php'; ?><label><strong><?php echo $lang_common['Message'] ?></strong><br />
-						<textarea id="req_message" name="req_message" rows="20" cols="95" tabindex="<?php echo $cur_index++ ?>"><?php echo pun_htmlspecialchars(isset($_POST['req_message']) ? $message : $cur_post['message']) ?></textarea><br /></label>
+						<textarea id="req_message" name="req_message" rows="15" cols="95" tabindex="<?php echo $cur_index++ ?>"><?php echo pun_htmlspecialchars(isset($_POST['req_message']) ? $message : $cur_post['message']) ?></textarea><br /></label>
 						<ul class="bblinks">
 							<li><a href="help.php#bbcode" onclick="window.open(this.href); return false;"><?php echo $lang_common['BBCode'] ?></a>: <?php echo ($pun_config['p_message_bbcode'] == '1') ? $lang_common['on'] : $lang_common['off']; ?></li>
 							<li><a href="help.php#img" onclick="window.open(this.href); return false;"><?php echo $lang_common['img tag'] ?></a>: <?php echo ($pun_config['p_message_img_tag'] == '1') ? $lang_common['on'] : $lang_common['off']; ?></li>
