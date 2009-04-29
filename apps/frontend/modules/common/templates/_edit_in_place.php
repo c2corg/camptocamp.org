da681faac4471e2587e04bb92eee48dd951d4e21
Lionel Besson <lionel.besson@gmail.com>
2009-04-29 12:13:13 +0000

    icode enhancement
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@992 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/common/templates/_edit_in_place.php
+++ apps/frontend/modules/common/templates/_edit_in_place.php
@@ -1,19 +1,12 @@
 <?php use_helper('Javascript', 'Form');
 
-$default_message = __('No message defined. Click to edit');
-
 if (!empty($message) || $sf_user->hasCredential('moderator')):
-    if (empty($message) && $sf_user->hasCredential('moderator'))
-    {
-        $output_message = $default_message;
-    }
-    else
-    {
-        $output_message = $sf_data->getRaw('message'); // unescaped data : we trust moderators !
-    } ?>
+    $output_message = empty($message) ? __('No message defined. Click to edit')
+                                      : $sf_data->getRaw('message'); // unescaped data : we trust moderators ! 
+    ?>
     <div id="edit_me" class="front_message"><?php echo $output_message; ?></div>
     <?php 
-    if ($sf_user->hasCredential('moderator'))
+    if (empty($message)) // = moderator
     {
         echo input_in_place_editor_tag('edit_me', 'common/edit?lang=' . $sf_user->getCulture(), array(
                 'cols'              => 40,
