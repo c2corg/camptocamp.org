3a2a6ddb712011313ccea2b2e3e1494b48c4cb18
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2009-04-29 22:57:07 +0000

    typo classe CSS
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@996 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/maps/templates/_nav_anchor.php
+++ apps/frontend/modules/maps/templates/_nav_anchor.php
@@ -10,7 +10,7 @@ $id = $sf_params->get('id');
     <div id="nav_anchor_content">
         <ul>
             <li><?php echo button_anchor('Information', 'data', 'action_informations', $module, $id, $lang); ?></li>
-            <li><?php echo button_anchor('Interactive map', 'map_container', 'picto_map', $module, $id, $lang); ?></li>
+            <li><?php echo button_anchor('Interactive map', 'map_container', 'picto_maps', $module, $id, $lang); ?></li>
             <li><?php echo button_anchor('Description', 'description', 'action_description', $module, $id, $lang); ?></li>
             <li><?php echo button_anchor('Images', 'images', 'picto_images', $module, $id, $lang); ?></li>
         </ul>
