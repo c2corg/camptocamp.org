215455d4220d96f85180eed87232df5cc739de92
Lionel Besson <lionel.besson@gmail.com>
2009-05-07 13:23:38 +0000

    obsolete file
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1031 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/documents/templates/_home_nav_title.php
+++
@@ -1,7 +0,0 @@
-<?php
-$title = 'news';
-?>
-<div id="nav_news_section_title" class="nav_box_title" title="Replier" onclick="toggleHomeSectionView('nav_news', 'Replier', 'Déplier'); return false;">
-      <div id="nav_news_toggle" class="nav_box_image picto_list"></div>
-      <div class="nav_box_title_text">L'actualité de camptocamp.org</div>
-</div>
