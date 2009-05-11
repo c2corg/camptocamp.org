0ff5c0590d32a870e5b1e88ccb80fdf2d8d7a9e7
Lionel Besson <lionel.besson@gmail.com>
2009-05-11 14:27:28 +0000

    itypo
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1048 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/summits/templates/_nav_anchor.php
+++ apps/frontend/modules/summits/templates/_nav_anchor.php
@@ -12,7 +12,7 @@ $id = $sf_params->get('id');
             <li><?php echo button_anchor('Information', 'data', 'action_informations', $module, $id, $lang); ?></li>
             <li><?php echo button_anchor('Interactive map', 'map_container', 'picto_maps', $module, $id, $lang); ?></li>
             <li><?php echo button_anchor('Description', 'description', 'action_description', $module, $id, $lang); ?></li>
-            <li><?php echo button_anchor('Linked outings', 'outings', 'outingoutings', $module, $id, $lang); ?></li>
+            <li><?php echo button_anchor('Linked outings', 'outings', 'picto_outings', $module, $id, $lang); ?></li>
             <li><?php echo button_anchor('Linked routes', 'routes', 'picto_routes', $module, $id, $lang); ?></li>
             <li><?php echo button_anchor('Images', 'images', 'picto_images', $module, $id, $lang); ?></li>
         </ul>
