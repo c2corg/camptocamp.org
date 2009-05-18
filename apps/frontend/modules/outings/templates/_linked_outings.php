069ad6495156db5e521f5c9e192c6a9268b25f7a
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2009-05-18 23:38:36 +0000

    correction de CSS écrasés par une modif sauvage de main.css !
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1077 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/outings/templates/_linked_outings.php
+++ apps/frontend/modules/outings/templates/_linked_outings.php
@@ -11,7 +11,6 @@ echo '<p style="margin-top:0.7em;">' .
      '<span class="picto action_list"></span> ' .
      link_to(__('List all linked outings') . $nb_outings, "outings/list?$module=$id&orderby=date&order=desc") .
      ' - ' .
-     image_tag(sfConfig::get('app_static_url') . '/static/images/picto/rss.png',
-               array('alt'=> 'RSS', 'title'=>__('RSS list'))) . ' ' .
+     '<span class="picto picto_rss"></span> ' .
      link_to(__('RSS list'), "outings/rss?$module=$id&orderby=date&order=desc") .
      '</p>';
