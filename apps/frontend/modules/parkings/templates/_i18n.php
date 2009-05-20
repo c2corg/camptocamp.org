8fe85639e5a8231a7752c17e10f58fdc1501f610
Lionel Besson <lionel.besson@gmail.com>
2009-05-20 06:37:01 +0000

    typo
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1086 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/parkings/templates/_i18n.php
+++ apps/frontend/modules/parkings/templates/_i18n.php
@@ -4,7 +4,7 @@ use_helper('Field');
 echo field_text_data($document, 'description', 'road access');
 if ($document->get('geom_wkt'))
 {
-    field_getdirections($sf_params->get('id'));
+    echo field_getdirections($sf_params->get('id'));
 }
 echo field_text_data_if_set($document, 'public_transportation_description');
 echo field_text_data_if_set($document, 'snow_clearance_comment');
