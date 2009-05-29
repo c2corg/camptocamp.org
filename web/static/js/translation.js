410976e861ef1f04217d5a47983060a9399bc59e
Lionel Besson <lionel.besson@gmail.com>
2009-05-29 13:42:39 +0000

    break string into multiple part for translation
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1120 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/static/js/translation.js
+++ web/static/js/translation.js
@@ -1,7 +1,14 @@
 var translate_button = '<span class="translate_button" style="display:none">[ <a href="#" onclick="translate(this.parentNode);return false;">'+translate_string+'</a> ]</span>';
-var untranslate_button = '<div class="translate_button" style="display:none">[ <a href="#" onclick="untranslate(this.parentNode);return false;">'+untranslate_string+'</a> ]</div>';
+var untranslate_button = '<span class="translate_button" style="display:none">[ <a href="#" onclick="untranslate(this.parentNode);return false;">'+untranslate_string+'</a> ]</span>';
+var translate_wait = '<span class="translate_wait translate_button">Please wait...</span>';// translate
+var translate_error = 'Translation went wrog for some reason'; // translate
 
-var translate_timer = false;
+
+var conc_strings;
+var conc_translated;
+var conc_total;
+
+var translate_limit = 1000;
 
 function translation_api_loaded() {
   if (!google.language.isTranslatable(language_from)
@@ -22,18 +29,52 @@ function translation_api_loaded() {
 function translate(obj) {
   var original_div = obj.next(1);
   var content = original_div.innerHTML;
-  google.language.translate(content, language_from, language_to,
-    function(result) {
-      if (!result.error) {
-        var translated_div_content = '<div>'+result.translation+'</div>';
-        original_div.hide();
-        new Insertion.After(original_div, translated_div_content);
-        var translated_div = obj.next(2);
-        google.language.getBranding(translated_div);
-        new Effect.Highlight(translated_div);
-        obj.replace(untranslate_button);
-      }
-  });
+  obj.replace(translate_wait);
+  obj = original_div.previous(1);
+  if (content.length < translate_limit) {
+    google.language.translate(content, language_from, language_to,
+      function(result) {
+        if (!result.error) {
+          show_translation(obj, result.translation);
+        } // TODO ERROR
+    });
+  } else { // text is too long
+    var strings = cut(content, '</p>').flatten();
+    conc_total = strings.length;
+    conc_translated = 0;
+    conc_strings = new Array();
+    strings.each(function(s, index) {
+        google.language.translate(s, language_from, language_to,
+          function(result) {
+            if (!result.error) {
+              add_partial_translation(obj, result.translation, index);
+            } // TODO ERROR
+          });
+    });
+  }
+}
+
+function show_translation(obj, translated_text) {
+  var translated_div_content = '<div>'+translated_text+'</div>';
+  var original_div = obj.next(1);
+  original_div.hide();
+  new Insertion.After(original_div, translated_div_content);
+  var translated_div = obj.next(2);
+  google.language.getBranding(translated_div);
+  new Effect.Highlight(translated_div);
+  obj.replace(untranslate_button);
+}
+
+function add_partial_translation(obj, translated_part, index) {
+  conc_translated++;
+  conc_strings[index] = translated_part;
+  if (conc_translated == conc_total) {
+    var translated_text = '';
+    for (var i = 0, len = conc_strings.length; i < len; ++i) {
+      translated_text += conc_strings[i];
+    }
+    show_translation(obj, translated_text);
+  }
 }
 
 function untranslate(obj) {
@@ -43,6 +84,32 @@ function untranslate(obj) {
   translated_div.remove();
   new Effect.Highlight(original_div);
   obj.replace(translate_button);
+}
+
+function cut(text, delimiter, reset_delimiter) {
+//  alert ('try '+text+ ' ' + delimiter);
+
+  if (text.length < translate_limit) {
+    return text;
+  }
+  var posok = -1;
+  var pos1 = text.indexOf(delimiter);
+  while ((pos1 != -1) && (pos1 <= translate_limit)) {
+    posok = pos1;
+    pos1 = text.indexOf(delimiter, posok+delimiter.length);
+  }
+  if (posok != -1) {
+    if (reset_delimiter) {delimiter = '</p>';}
+    return [text.substr(0, posok+delimiter.length), cut(text.substr(posok+delimiter.length), delimiter)];
+  } else {
+    switch(delimiter) {
+      case '</p>': return cut(text, '<br />', true);
+      case '<br />': return cut(text, '</li>', true);
+      case '</li>': return cut(text, '.', true);
+      case '.':
+      default: return [text.substr(0, translate_limit), text.substr(translate_limit)];
+    }
+  }
 }
 
 google.load("language", "1", {"callback" : translation_api_loaded});
