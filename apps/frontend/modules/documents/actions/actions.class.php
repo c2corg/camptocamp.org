a3b1df5c00af2fc3f38e60967d3bda1b03a2e46b
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2009-06-09 01:27:24 +0000

    correction association itinéraires
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1186 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/documents/actions/actions.class.php
+++ apps/frontend/modules/documents/actions/actions.class.php
@@ -2635,7 +2635,7 @@ class documentsActions extends c2cActions
         $main_model = $models['main'];
         $linked_model = $models['linked'];
         
-        if ($type == 'ro')
+        if ($type == 'ro' || $type = 'rr')
         {
             // in that case, output not only route name but also best summit name whose id has been passed (summit_id)
             $summit = explode(' [',$this->getRequestParameter('summits_name'));
