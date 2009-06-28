d6aecc08a9c72b4c0b4ca5ac7451d46298c6542a
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2009-06-28 21:30:28 +0000

    correction chti bug sur parser de balise img des forums
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1280 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/forums/include/parser.php
+++ web/forums/include/parser.php
@@ -618,9 +618,9 @@ function do_bbcode($text, $is_signature = false, $post_list = array())
     
 	if ((!$is_signature && $pun_config['p_message_img_tag'] == '1') || ($is_signature && $pun_config['p_sig_img_tag'] == '1'))
 	{
-		$pattern[] = '#\[img\|?((?<=\|)\w*|)\]((ht|f)tps?://|/static/)([^\s<"]*?)\[/img\]\s?#ise';
-		$pattern[] = '#\[img=((ht|f)tps?://|/static/)([^\s"\[<]*?)\|?((?<=\|)\w*|)\](.*?)\[/img\]\s?#ise';
-		$pattern[] = '#\[img=([^\[<]*?)\|?((?<=\|)\w*|)\]((ht|f)tps?://|/static/)([^\s<"]*?)\[/img\]\s?#ise';
+		$pattern[] = '#\[img\|?((?<=\|)\w*|)\]((ht|f)tps?://|/static/|/uploads/)([^\s<"]*?)\[/img\]\s?#ise';
+		$pattern[] = '#\[img=((ht|f)tps?://|/static/|/uploads/)([^\s"\[<]*?)\|?((?<=\|)\w*|)\](.*?)\[/img\]\s?#ise';
+		$pattern[] = '#\[img=([^\[<]*?)\|?((?<=\|)\w*|)\]((ht|f)tps?://|/static/|/uploads/)([^\s<"]*?)\[/img\]\s?#ise';
 		$pattern[] = '#\[img\|?((?<=\|)\w*|)\]([0-9_]+)\.(\w+)\[/img\]\s?#ise';
 		$pattern[] = '#\[img=([0-9_]+)\.(\w+)\|?((?<=\|)\w*|)\](.*?)\[/img\]\s?#ise';
 		$pattern[] = '#\[img=([^\[<]*?)\|?((?<=\|)\w*|)\]([0-9_]+)\.(\w+)\[/img\]\s?#ise';
