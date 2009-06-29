d3067ee5fa089f47a7edf85ee295ee938656c2a1
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2009-06-29 06:54:22 +0000

    correction picto ampoule correction cache forum
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1281 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/forums/header.php
+++ web/forums/header.php
@@ -28,12 +28,12 @@ if (!defined('PUN'))
 	exit;
 
 // Send no-cache headers
-/*
-header('Expires: Thu, 21 Jul 1977 07:30:00 GMT');	// When yours truly first set eyes on this world! :)
+
+header('Expires:');	// When yours truly first set eyes on this world! :)
 header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-header('Cache-Control: post-check=0, pre-check=0', false);
-header('Pragma: no-cache');		// For HTTP/1.0 compability
-*/
+header('Cache-Control:');
+header('Pragma:');		// For HTTP/1.0 compability
+
 
 // Load the template
 if (defined('PUN_ADMIN_CONSOLE'))
