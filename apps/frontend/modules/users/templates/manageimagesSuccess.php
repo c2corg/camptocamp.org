310ea319c602037473717e25a73a9ae512fa36fc
Bruno Besson <bruno.besson@gmail.com>
2009-07-03 08:36:58 +0000

    Some i18n changes
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1311 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/users/templates/manageimagesSuccess.php
+++ apps/frontend/modules/users/templates/manageimagesSuccess.php
@@ -70,7 +70,7 @@ echo input_hidden_tag('page', $page);
 ?>
 <div style="clear:both"><?php echo $pager_navigation; ?></div>
 <ul class="action_buttons">
-    <li><?php echo submit_tag(__('Update'), array('class' => 'action_edit')); ?></li>
+    <li><?php echo submit_tag(__('Switch license'), array('class' => 'action_edit')); ?></li>
     <li><?php echo button_tag(__('Select all'), __('Select all'), array('onclick' => "$$('form#editform div.manageimages_info input[type=checkbox]').each(function(obj){obj.checked=true;});",
                                                                     'class' => 'action_create',
                                                                     'title' => null)); ?></li>
