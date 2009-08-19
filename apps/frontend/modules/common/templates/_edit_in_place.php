a195b6b0b615673c7be3b5b0b5d6b4a1b44a3a01
Lionel Besson <lionel.besson@gmail.com>
2009-08-19 08:55:13 +0000

    retrieve html code for inPlaceEditor on Home
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1452 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/common/templates/_edit_in_place.php
+++ apps/frontend/modules/common/templates/_edit_in_place.php
@@ -8,6 +8,21 @@ if (!empty($message) || $sf_user->hasCredential('moderator')):
     <?php 
     if ($sf_user->hasCredential('moderator'))
     {
+        echo javascript_tag("Object.extend(Ajax.InPlaceEditor.prototype, {
+    onLoadedExternalText: function(transport) {
+        Element.removeClassName(this.form, this.options.loadingClassName);
+        this.editField.disabled = false;
+        this.editField.value = transport.responseText;
+        Field.scrollFreeActivate(this.editField);
+    }
+});
+
+Object.extend(Ajax.InPlaceEditor.prototype, {
+    getText: function() {
+        return (this.element.innerHTML).replace(/<br>/, '<br />');
+    }
+});
+");
         echo input_in_place_editor_tag('edit_me', 'common/edit?lang=' . $sf_user->getCulture(), array(
                 'cols'              => 40,
                 'rows'              => 2,
