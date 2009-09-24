f2b9d113bc45f02e9e24df06b53776c2f67e1a14
Lionel Besson <lionel.besson@gmail.com>
2009-09-24 09:49:06 +0000

    batch for removing old files from images temp directory
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1653 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ batch/cleanImagesTempDirectory.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Batch that removes too old files from images temp directory
+ * Must be lauched by a cron
+ */
+
+$expire_time = 2; // files older than expire_time hours will be deleted
+
+define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
+define('SF_APP',         'frontend');
+define('SF_ENVIRONMENT', 'prod');
+define('SF_DEBUG',       false);
+require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
+
+$temp_dir = sfConfig::get('sf_upload_dir') . DIRECTORY_SEPARATOR .
+            sfConfig::get('app_images_temp_directory_name') . DIRECTORY_SEPARATOR;
+
+echo "$temp_dir";
+
+if ($handle = opendir($temp_dir))
+{
+    while (false !== ($file = readdir($handle)))
+    {
+        $file_creation = filectime($temp_dir.$file);
+        $file_age = time() - $file_creation;
+
+        if (($file != '.') && ($file_age > ($expire_time * 3600)))
+        {
+            unlink($temp_dir.$file);
+        }
+    }
+}
+?>
