0ba8ce2872246eb9b6f0db98a587ed46151124dd
Bruno Besson <bruno.besson@gmail.com>
2009-09-26 06:58:21 +0000

    typo
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1664 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/lib/c2cTools.class.php
+++ apps/frontend/lib/c2cTools.class.php
@@ -440,7 +440,7 @@ class c2cTools
         switch ($a)
         {
             case 'areas':
-                return 'areas';
+                return 'area';
             case 'books':
                 return 'book';
             case 'articles':
