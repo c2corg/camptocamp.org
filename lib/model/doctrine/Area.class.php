407591ea7939474810a0cc73202eb8e826bd8c7d
Lionel Besson <lionel.besson@gmail.com>
2009-10-09 12:29:05 +0000

    #357 (to be tested on test site)
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1756 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- lib/model/doctrine/Area.class.php
+++ lib/model/doctrine/Area.class.php
@@ -70,7 +70,7 @@ class Area extends BaseArea
         }
         elseif ($nb_geo == 1)
         {
-            $id = $geo->key();
+            $id = key($geo);
             $regions = array($id => $geo[$id]['AreaI18n'][0]['name']);
         }
         elseif ($nb_geo > 1)
