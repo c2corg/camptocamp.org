b45a2453f76d54990d8ccd9a2c70147ac63d7063
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2009-10-27 02:57:10 +0000

    #347 : CSS pour mobiles : forums
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1855 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/forums/include/user/symfony_style.php
+++ web/forums/include/user/symfony_style.php
@@ -1,8 +1,12 @@
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo PUN_STATIC_URL; ?>/static/css/main.css?<?php echo sfSVN::getHeadRevision('main.css') ?>" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo PUN_STATIC_URL; ?>/static/css/menu.css?<?php echo sfSVN::getHeadRevision('menu.css') ?>" />
+<link rel="stylesheet" type="text/css" media="handheld" href="<?php echo PUN_STATIC_URL; ?>/static/css/handheld.css?<?php echo sfSVN::getHeadRevision('handheld.css') ?>" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo PUN_STATIC_URL; ?>/sfModalBoxPlugin/css/modalbox.css?<?php echo sfSVN::getHeadRevision('modalbox.css') ?>" />
 <script type="text/javascript" src="<?php echo PUN_STATIC_URL; ?>/sfPrototypePlugin/js/prototype.js"></script>
 <script type="text/javascript" src="<?php echo PUN_STATIC_URL; ?>/sfPrototypePlugin/js/scriptaculous.js"></script>
+<!--[if !IE]>-->
+<link type="text/css" rel="stylesheet" media="only screen and (max-device-width: 480px)" href="<?php echo PUN_STATIC_URL; ?>/static/css/handheld.css?<?php echo sfSVN::getHeadRevision('handheld.css') ?>">
+<!--<![endif]-->
 <!--[if lt IE 7]>
     <link rel="stylesheet" type="text/css" media="all" href="<?php echo PUN_STATIC_URL; ?>/static/css/ie.css?<?php echo sfSVN::getHeadRevision('ie.css') ?>" />
 <![endif]-->
