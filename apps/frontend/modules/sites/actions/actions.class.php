5b07cc1de7046371255e75ad9d36ae47865b7bc6
Lionel Besson <lionel.besson@gmail.com>
2009-10-30 08:22:23 +0000

    properly translate site types in page title
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1868 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/sites/actions/actions.class.php
+++ apps/frontend/modules/sites/actions/actions.class.php
@@ -162,12 +162,12 @@ class sitesActions extends documentsActions
             $site_types_list = sfConfig::get('app_sites_site_types');
             foreach ($site_types as &$type)
             {
-                $type = $site_types_list[$type];
+                $type = $this->__($site_types_list[$type]);
             }
             $site_types = implode(', ', $site_types);
             if (!empty($site_types))
             {
-                $site_types = $this->__(' :') . ' ' . $site_types;
+                $site_types = $this->__(' :') . ' ' .  $site_types;
             }
             $site_types = $this->__('site') . $site_types;
             $doc_name = $this->document->get('name');
