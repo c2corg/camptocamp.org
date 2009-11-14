867a57733a5f10370960bf3a707c404aa94e45f9
Lionel Besson <lionel.besson@gmail.com>
2009-11-14 14:15:07 +0000

    culture for latest articles on home
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@1969 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/articles/templates/_latest.php
+++ apps/frontend/modules/articles/templates/_latest.php
@@ -29,7 +29,8 @@ include_partial('documents/home_section_title', array('module' => 'articles')); 
             <span class="home_article_title">
             <?php echo link_to($item['name'], 
                                "@document_by_id_lang_slug?module=articles&id=$id&lang=$lang&slug="
-                               . formate_slug($item['search_name'])); ?>
+                                   . formate_slug($item['search_name']),
+                               ($lang != $culture) ? array('hreflang' => $lang) : null); ?>
             </span>
             <?php echo truncate_article_abstract(parse_links(parse_bbcode_abstract($item['abstract'])),
                                                  sfConfig::get('app_recent_documents_articles_abstract_characters_limit')); ?>
