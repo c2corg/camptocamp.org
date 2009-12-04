b94480acc6d6a165dfec52ab1123171a0293aa88
Lionel Besson <lionel.besson@gmail.com>
2009-12-04 12:52:54 +0000

    missing file
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2079 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ apps/frontend/modules/parkings/templates/_parkings4list.php
@@ -0,0 +1,23 @@
+<?php
+$html = array();
+foreach ($parkings as $parking)
+{
+    $name = ucfirst($parking['ParkingI18n'][0]['name']);
+    $url = '@document_by_id_lang_slug?module=parkings&id=' . $parking['id'] . '&lang=' . $parking['ParkingI18n'][0]['culture'] .
+           '&slug=' . make_slug($parking['ParkingI18n'][0]['name']);
+    $link = link_to($name, $url);
+    if (isset($parking['lowest_elevation']) && is_scalar($parking['lowest_elevation']) && $parking['lowest_elevation'] != $parking['elevation'])
+    {
+        $link .= '&nbsp; ' . $parking['lowest_elevation'] . __('meters') . __('range separator') . $parking['elevation'] . __('meters');
+    }
+    else if (isset($parking['elevation']) && is_scalar($parking['elevation']))
+    {
+        $link .= '&nbsp; ' . $parking['elevation'] . __('meters');
+    }
+    if (isset($doc['public_transportation_types']))
+    {
+        $link .= field_pt_picto_if_set($parking, true, true, ' - ');
+    }
+    $html[] = $link;
+}
+echo implode('<br />', $html);
