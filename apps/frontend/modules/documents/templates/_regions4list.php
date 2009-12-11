29e8da949a623c6657bef06c82c1a821efb74546
Lionel Besson <lionel.besson@gmail.com>
2009-12-11 13:54:41 +0000

    display alla attached areas in lists (and not only the first one of each type)
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2116 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/documents/templates/_regions4list.php
+++ apps/frontend/modules/documents/templates/_regions4list.php
@@ -3,7 +3,7 @@ $a = array();
 foreach ($geoassociations as $geo_id => $geoP)
 {
     $i18n = $geoP['AreaI18n'][0];
-    $a[$geoP['type']] = link_to(ucfirst($i18n['name']),
+    $a[$geoP['type'].$geo_id] = link_to(ucfirst($i18n['name']),
                                         "@document_by_id_lang_slug?module=areas&id=$geo_id&lang=" . $i18n['culture'] . '&slug=' . make_slug($i18n['name']));
 }
 krsort($a);
