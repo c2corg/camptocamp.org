7f31745267ad8bdd742cddaa19e5a1a84c184fde
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2010-01-18 00:32:04 +0000

    design weather box
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2174 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/areas/templates/_association.php
+++ apps/frontend/modules/areas/templates/_association.php
@@ -122,7 +122,8 @@ if ($has_weather || $has_avalanche_bulletin)
             {
                 $label[] = __('Avalanche bulletin');
             }
-            $label = '<span class="assoc_img picto_open_light" id="toggle_weather"></span>' . implode(', ', $label);
+            $label = '<div class="assoc_img picto_open_light" id="toggle_weather"></div>'
+                   . '<div class="linked_elt">' . implode(', ', $label) . '</div>';
 
             echo '<div class="box_title" id="weather_box_title" title="' . __('section open') . '">'
            . link_to_function($label, "toggleBox('weather')") 
