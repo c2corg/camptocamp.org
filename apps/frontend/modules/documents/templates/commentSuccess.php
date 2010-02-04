99e374372fdad808923e4867cfbe1f84824f344c
Lionel Besson <lionel.besson@gmail.com>
2010-02-04 08:45:14 +0000

    fixes #504
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2236 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/documents/templates/commentSuccess.php
+++ apps/frontend/modules/documents/templates/commentSuccess.php
@@ -38,7 +38,7 @@ $nb_comments = $comments->count();
 echo display_title(isset($title_prefix) ? $title_prefix.__('&nbsp;:').' '.$document_name : $document_name, $module);
 
 echo '<div id="nav_space">&nbsp;</div>';
-echo tabs_list_tag($id, $lang, $exists_in_lang, 'comments', NULL, make_slug($name), $nb_comments);
+echo tabs_list_tag($id, $lang, $exists_in_lang, 'comments', NULL, make_slug($document_name), $nb_comments);
 
 echo display_content_top('doc_content');
 echo start_content_tag($module . '_content');
