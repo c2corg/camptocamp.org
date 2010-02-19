7fe87ce261247de0e615344ccd3395009cde211d
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2010-02-19 23:49:17 +0000

    added geo export links in areas page (only for moderators)
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2304 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/areas/templates/_data.php
+++ apps/frontend/modules/areas/templates/_data.php
@@ -4,6 +4,10 @@
     <ul id="article_gauche_5050" class="data">
         <?php 
         li(field_data_from_list($document, 'area_type', 'mod_areas_area_types_list'));
+        if ($sf_user->hasCredential('moderator') && $document->get('geom_wkt'))
+        {
+            li(field_export($document->get('module'), $sf_params->get('id'), $sf_params->get('lang')));
+        }
         ?>
     </ul>
 </div>
