eb1fefd559be48b76a61d7f0d0246ea12c587477
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2010-05-20 13:28:45 +0000

    map_filter de-escaping
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2663 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/portals/templates/changerdapprocheSuccess.php
+++ apps/frontend/modules/portals/templates/changerdapprocheSuccess.php
@@ -83,6 +83,7 @@ include_partial('portals/prepare', array('content_id' => 'cda_prepare_outing_box
 <?php
 if ($has_map)
 {
+    $map_filter = $sf_data->getRaw('map_filter');
     include_partial('documents/map_section', array('document' => $document,
                                                    'layers_list' => $map_filter['objects'],
                                                    'center' => $map_filter['center'],
