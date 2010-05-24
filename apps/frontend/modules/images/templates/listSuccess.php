438f6ed4a8c8a1b42279a1e0ff7e8c7e862cb305
Lionel Besson <lionel.besson@gmail.com>
2010-05-24 16:21:25 +0000

    #568
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2679 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/images/templates/listSuccess.php
+++ apps/frontend/modules/images/templates/listSuccess.php
@@ -28,6 +28,7 @@ else:
     echo '</p>';
     $pager_navigation = pager_navigation($pager);
     echo $pager_navigation;
+    echo pager_nb_results($pager);
     echo '<div class="clearer"></div>';
     $items = Language::parseListItems($items, 'Image');
     $static_base_url = sfConfig::get('app_static_url');
