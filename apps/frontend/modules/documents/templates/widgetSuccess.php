6f1b69fd96444c7ef4b448bb3802eaf3759ac453
Bruno Besson <bruno.besson@gmail.com>
2010-05-28 18:02:16 +0000

    escape html characters for widgets
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2707 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/documents/templates/widgetSuccess.php
+++ apps/frontend/modules/documents/templates/widgetSuccess.php
@@ -13,7 +13,7 @@ foreach ($items as $item)
     $i18n = $item[c2cTools::module2model($module) . 'I18n'][0];
     $lang = $i18n['culture'];
     $id = $item['id'];
-    echo '["' . $i18n['name'] . '","' . url_for("@document_by_id_lang_slug?module=$module&id=$id&lang=$lang&slug=" . make_slug($i18n['name']), true) . '"]';
+    echo '["' . htmlentities($i18n['name'], ENT_COMPAT, 'UTF-8') . '","' . url_for("@document_by_id_lang_slug?module=$module&id=$id&lang=$lang&slug=" . make_slug($i18n['name']), true) . '"]';
     if ($item != $last_item) {
         echo ',';
     }
