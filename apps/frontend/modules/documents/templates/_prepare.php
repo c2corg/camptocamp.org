58ec342234de36c85008105f1c10cb5bc4163a71
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2010-05-30 11:57:42 +0000

    i18n
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2709 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/documents/templates/_prepare.php
+++ apps/frontend/modules/documents/templates/_prepare.php
@@ -13,7 +13,7 @@ if ($html_content != 'donotshow'):
         <?php echo nav_title('prepare', __('Prepare outing'), 'outings'); ?>
         <div class="nav_box_text" id="nav_prepare_section_container">
             <?php echo $html_content ?>
-            <p class="nav_box_bottom_link"><?php echo link_to(__('More links'), getMetaArticleRoute('prepare_outings')) ?></p>
+            <p class="nav_box_bottom_link"><?php echo link_to(__('More links'), getMetaArticleRoute('prepare_outings', false)) ?></p>
         </div>
         <?php
         $cookie_position = array_search('nav_prepare', sfConfig::get('app_personalization_cookie_fold_positions'));
