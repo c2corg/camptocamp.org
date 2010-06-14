a4f972ee97d5fc983d554a27f08024be4ca7fa5b
Lionel Besson <lionel.besson@gmail.com>
2010-06-14 20:41:43 +0000

    mobile maps
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2775 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/documents/templates/_mobile_map_section.php
+++ apps/frontend/modules/documents/templates/_mobile_map_section.php
@@ -54,17 +54,50 @@ if ($has_geom || $show_map)
 
     echo start_section_tag($section_title, 'map_container', 'opened', true, false, false, $show_tip);
 
-    // TODO icons, polylines and shapes (?), section folding, extra objects (see routes)
-    $map_url = 'http://maps.google.com/maps/api/staticmap?size=300x300&amp;maptype=terrain&amp;';
+    // TODO icons, polylines and shapes (?), section folding, cache, extra objects (see routes)
+    $map_url = 'http://maps.google.com/maps/api/staticmap?size=300x300&amp;maptype=terrain&amp;mobile=true&amp;sensor=false&amp;';
     $map_options = array();
 
     
-    $map_options[] = 'center='.$document['lon'].','.$document['lat']; // TODO this won't work for everything (just temporary)
-    $map_options[] = 'zoom=14';
-    $map_options[] = 'sensor=false';
+//    $map_options[] = 'center='.$document['lon'].','.$document['lat']; // TODO this won't work for everything (just temporary)
+    $module = $document->module;
+    if ($module == 'summits' || $module == 'parkings' || $module == 'sites' ||
+        $module == 'huts' || $module == 'products' || $module == 'users' || $module == 'images')
+    {
+        $marker_url = urlencode('http://www.camptocamp.org/static/images/modules/'.$module.'_mini.png'); // TODO hard coded uri...+factorize
+        $map_options[] = 'markers=shadow:false|icon:'.$marker_url.'|'.$document['lon'].','.$document['lat'];
+        $map_options[] = 'zoom=14';
+    }
+    elseif ($module == 'outings' || $module == 'maps' || $module == 'routes' || $module == 'areas')
+    {
+        // TODO
+    }
+
+    // routes : display linked summits, parkings and huts
+    if ($module == 'routes')
+    {
+        foreach(array('summits', 'parkings', 'huts') as $type)
+        {
+            if (!isset($document->$type)) continue;
+            $marker_url = urlencode('http://www.camptocamp.org/static/images/modules/'.$type.'_mini.png'); // TODO hard coded uri...
+            $markers = array();
+            foreach ($document->$type as $doc)
+            {
+                if (!empty($doc['pointwkt']))
+                {
+                    $coords = explode(' ', gisQuery::getEWKT($doc['id'], true));
+                    $markers[] = substr($coords[0], 0, 7).','.substr($coords[1], 0, 7); // TODO not ok
+                }
+            }
+            if (count($markers))
+            {
+                $map_options[] = 'markers=shadow:false|icon:'.$marker_url.'|'.implode('|', $markers);
+            }
+        }
+    }
 
     $map_url .= implode('&amp;', $map_options);
-    echo '<img src="'.$map_url.'" />';
+    echo image_tag($map_url, array('alt' => __('map')));
 
     echo end_section_tag(true);
 }
