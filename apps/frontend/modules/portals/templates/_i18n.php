29c7cbadfce5d2e960c7fa9893eb831415dacc89
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2010-06-14 23:11:46 +0000

    correction design cda correction preview portail
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2777 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/portals/templates/_i18n.php
+++ apps/frontend/modules/portals/templates/_i18n.php
@@ -2,7 +2,11 @@
 use_helper('Field'); 
 
 $is_archive = $document->isArchive();
-if ($is_archive)
+if  (!isset($preview))
+{
+    $preview = false;
+}
+if ($is_archive || $preview)
 {
     echo field_text_data_if_set($document, 'abstract', null, array('needs_translation' => $needs_translation, 'show_images' => false));
 }
