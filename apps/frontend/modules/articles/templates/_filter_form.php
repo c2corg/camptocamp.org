4d2cdb7d56a5023f7dabee7bc6e334d3356beb85
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2010-06-18 00:16:53 +0000

    filtre d'article : ajout régions
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2788 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/articles/templates/_filter_form.php
+++ apps/frontend/modules/articles/templates/_filter_form.php
@@ -4,11 +4,15 @@ use_helper('FilterForm', 'General');
 // put focus on the name field on window load
 echo javascript_tag('focus_field = \'cnam\';');
 
-echo picto_tag('picto_articles') . __('Name:') . ' ' . input_tag('cnam');
-echo __('categories') . ' ' . field_value_selector('ccat', 'mod_articles_categories_list', true);
+echo picto_tag('picto_articles') . __('name:') . ' ' . input_tag('cnam');
+echo __('categories') . ' ' . field_value_selector('ccat', 'mod_articles_categories_list', false, false, true, 9);
+?>
+<br />
+<?php
 echo __('article_type') . ' ' . field_value_selector('ctyp', 'mod_articles_article_types_list', true);
 ?>
 <br /><br />
 <?php
 echo __('activities') . ' ' . activities_selector();
+include_partial('areas/areas_selector', array('ranges' => $ranges, 'use_personalization' => false));
 include_partial('documents/filter_sort');
