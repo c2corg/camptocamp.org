fdd39bd0a54aa892bec6169404e2d2d0271c18a9
Lionel Besson <lionel.besson@gmail.com>
2010-06-19 14:21:53 +0000

    mobile version
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2793 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/common/templates/_mobile_menu.php
+++ apps/frontend/modules/common/templates/_mobile_menu.php
@@ -2,40 +2,43 @@
 use_helper('General', 'Forum');
 
 $menu_search = array(
-    '@filter?module=outings' => __('outings'),
-    '@filter?module=routes' => __('routes'),
-    '@filter?module=images' => __('images'),
-    '@filter?module=summits' => __('summits'),
-    '@filter?module=sites' => __('sites'),
-    '@filter?module=parkings' => __('parkings'),
-    '@filter?module=huts' => __('huts'),
-    '@filter?module=articles' => __('articles'),
-    '@filter?module=areas' => __('areas'),
-    '@filter?module=maps' => __('maps'),
-    '@filter?module=products' => __('products'),
-    '@filter?module=users' => __('users'),
+    '#' => __('back'),
+    url_for('@filter?module=outings') => __('outings'),
+    url_for('@filter?module=routes') => __('routes'),
+    url_for('@filter?module=images') => __('images'),
+    url_for('@filter?module=summits') => __('summits'),
+    url_for('@filter?module=sites') => __('sites'),
+    url_for('@filter?module=parkings') => __('parkings'),
+    url_for('@filter?module=huts') => __('huts'),
+    url_for('@filter?module=articles') => __('articles'),
+    url_for('@filter?module=areas') => __('areas'),
+    url_for('@filter?module=maps') => __('maps'),
+    url_for('@filter?module=products') => __('products'),
+    url_for('@filter?module=users') => __('users'),
 );
 
 $menu_see = array(
-    '@default_index?module=outings&orderby=date&order=desc' => __('outings'),
-    '@default_index?module=routes' => __('routes'),
-    '@default_index?module=images' => __('images'),
-    '@default_index?module=summits' => __('summits'),
-    '@default_index?module=sites' => __('sites'),
-    '@default_index?module=parings' => __('parkings'),
-    '@default_index?module=huts' => __('huts'),
-    '@default_index?module=articles' => __('articles'),
-    '@default_index?module=areas' => __('areas'),
-    '@default_index?module=maps' => __('maps'),
-    '@default_index?module=products' => __('products'),
-    '@default_index?module=users' => __('users')
+    '#' => __('back'),
+    url_for('@default_index?module=outings&orderby=date&order=desc') => __('outings'),
+    url_for('@default_index?module=routes') => __('routes'),
+    url_for('@default_index?module=images') => __('images'),
+    url_for('@default_index?module=summits') => __('summits'),
+    url_for('@default_index?module=sites') => __('sites'),
+    url_for('@default_index?module=parings') => __('parkings'),
+    url_for('@default_index?module=huts') => __('huts'),
+    url_for('@default_index?module=articles') => __('articles'),
+    url_for('@default_index?module=areas') => __('areas'),
+    url_for('@default_index?module=maps') => __('maps'),
+    url_for('@default_index?module=products') => __('products'),
+    url_for('@default_index?module=users') => __('users')
 );
 
 $menu_more = array(
-    getMetaArticleRoute('association') => __('Association'),
-    getMetaArticleRoute('help', false) => __('Global help'),
-    getMetaArticleRoute('home_guide') => __('Help').__(' :').' '.__('Guidebook'),
-    getMetaArticleRoute('help_forum', false) => __('Help').__(' :').' '.__('Forums')
+    '#' => __('back'),
+    url_for(getMetaArticleRoute('association')) => __('Association'),
+    url_for(getMetaArticleRoute('help', false)) => __('Global help'),
+    url_for(getMetaArticleRoute('home_guide')) => __('Help').__(' :').' '.__('Guidebook'),
+    url_for(getMetaArticleRoute('help_forum', false)) => __('Help').__(' :').' '.__('Forums')
 );
 ?>
 <div id="mobile_menu">
@@ -66,3 +69,8 @@ $menu_more = array(
     </div>
   </div>
 </div>
+<?php echo javascript_tag('$$(\'#mobile_menu select\').each(function(o) {
+                             o.observe(\'change\', function(e) {
+                               window.location=this.options[this.selectedIndex].value }
+                             )}
+                           );');
