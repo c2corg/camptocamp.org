c6e5891dfd5df073af6647b09507be505723fa5e
Lionel Besson <lionel.besson@gmail.com>
2010-08-24 17:47:27 +0000

    enable multi geometry for importing new areas
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2875 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- batch/importKml.php
+++ batch/importKml.php
@@ -1,16 +1,71 @@
 <?php
-/**
- * Config:
- */
 
-$filepath = '../tmp/kml/puna.kml';
-//$filepath = '../tmp/kml/queyras.kml';
-$region_id = 0; // new region if 0
-$region_type = 1; // range
-$culture = 'fr';
+if ($argc < 2)
+{
+    echo "Usage: php $argv[0] <kml file> [<region id> [<region type> [<culture>]]\n";
+    exit;
+}
 
-/**
- */
+if (!file_exists($argv[1]))
+{
+    echo "Please provide a valid kml file\n";
+    exit;
+} 
+$filepath = $argv[1];
+
+// id of the region to update, or 0 if it's a new region
+if ($argc >= 3)
+{
+    if (!is_numeric($argv[2]))
+    {
+        echo "Invalid region id\n";
+        exit;
+    }
+    $region_id =  intval($argv[2]);
+}
+else
+{
+    $region_id = 0;
+}
+
+// region type
+if ($argc >= 4)
+{
+    if (!is_numeric($argv[3]) || intval($argv[3]) > 3 || intval($argv[2]) < 1)
+    {
+        echo "Invalid region type\n";
+        exit;
+    }
+    $region_type = $argv[3];
+}
+else
+{
+    $region_type = 1;
+}
+
+// culture
+if ($argc >= 5)
+{
+    switch ($argv[4])
+    {
+        case 'fr':
+        case 'it':
+        case 'en':
+        case 'de':
+        case 'es':
+        case 'eu':
+        case 'ca': break;
+        default:
+            echo "Invalid culture";
+            break;
+    }
+    $culture = $argv[4];
+}
+else
+{
+    $culture = 'fr';
+}
+
 define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/..'));
 define('SF_APP',         'frontend');
 define('SF_ENVIRONMENT', 'dev');
@@ -23,34 +78,63 @@ require_once(SF_ROOT_DIR . DIRECTORY_SEPARATOR . 'apps' . DIRECTORY_SEPARATOR . 
 // needed for doctrine connection to work
 $context = sfContext::getInstance();
 
+// Construct geometry from kml
 $xml = simplexml_load_file($filepath);
 $region = $xml->Document->Placemark;
 
 $name = $region->name;
 
-$is_line = empty($region->Polygon);
-if (!$is_line)
+// detect if polygon or multipolygon
+$is_multi = !empty($region->MultiGeometry);
+if ($is_multi)
 {
-    $border = $region->Polygon->outerBoundaryIs->LinearRing->coordinates;
+    $region = $region->MultiGeometry;
+}
+
+$vertexes_list = array();
+$is_line = empty($region->Polygon);
+$polygons = $is_line ? $region->LineString : $region->Polygon;
+for ($i = 0; $i < count($polygons); $i++) // warning: foreach won't work
+{
+    if (!$is_line)
+    {
+        $border = $polygons[$i]->outerBoundaryIs->LinearRing->coordinates;
+    }
+    else
+    {
+        $border = $polygons[$i]->coordinates;
+    }
+    $border = preg_split("/[\s]+/", trim($border));
+
+    $vertexes = array();
+    foreach ($border as $point)
+    {
+        $data = explode(',', $point);
+        $vertexes[] = $data[0] . ' ' . $data[1];
+    }
+    if ($is_line)
+    {
+        $vertexes[] = $vertexes[0]; // closes line to make a polygon
+    }
+
+    $vertexes_list[] = $vertexes;
+}
+
+foreach ($vertexes_list as &$vertexes)
+{
+    $vertexes = implode(', ', $vertexes);
+}
+
+if (!$is_multi)
+{
+    $geom = 'POLYGON((' . $vertexes_list[0] . '))';
 }
 else
 {
-    $border = $region->LineString->coordinates;
-}
-$border = explode(' ', trim($border));
-
-$vertexes = array();
-foreach ($border as $point) {
-    $data = explode(',', $point);
-    $vertexes[] = $data[0] . ' ' . $data[1];
-}
-if ($is_line)
-{
-    $vertexes[] = $vertexes[0]; // closes line to make a polygon
+    $geom = 'MULTIPOLYGON(((' . implode(')),((', $vertexes_list) . ')))';
 }
 
-$geom = 'POLYGON((' . implode(', ', $vertexes) . '))';
-
+// Update database
 $conn = sfDoctrine::Connection();
 
 try
@@ -74,9 +158,9 @@ try
     }
     else
     {
-        // FIXME
+        // FIXME
         // reset geom before updating it (workaround to avoid update crash)
-        //$query = "UPDATE app_areas_archives set geom = NULL WHERE id = $region_id and is_latest_version";
+        //$query = "UPDATE app_areas_archives set geom = NULL WHERE id = $region_id and is_latest_version";
         //sfDoctrine::connection()->standaloneQuery($query);
 
         $area = Document::find('Area', $region_id);
