a4afbba053c4af67695d3284e250f9246c666ace
Lionel Besson <lionel.besson@gmail.com>
2010-09-19 10:26:57 +0000

    analytics & mobile version
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2924 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/common/templates/_tracker.php
+++ apps/frontend/modules/common/templates/_tracker.php
@@ -3,7 +3,7 @@ var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "htt
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
 <script type="text/javascript">
-var pageTracker = _gat._getTracker("<?php echo sfConfig::get('app_ganalytics_key') ?>");
+var pageTracker = _gat._getTracker("<?php echo c2cTools::mobileVersion() ? sfConfig::get('app_mobile_ganalytics_key') : sfConfig::get('app_ganalytics_key') ?>");
 pageTracker._trackPageview();
 ga_done = true;
 </script>
