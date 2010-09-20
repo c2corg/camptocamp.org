fdea429c1ed58da7cffb809149ba27fab855d699
Lionel Besson <lionel.besson@gmail.com>
2010-09-20 15:27:33 +0000

    viewport meta tag for mobile layout
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@2926 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/templates/mobile_layout.php
+++ apps/frontend/templates/mobile_layout.php
@@ -31,6 +31,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
         minify_include_head_javascripts($combine, $debug);
         echo include_meta_links();
     ?>
+    <meta name ="viewport" content = "width=device-width; initial-scale=1.0; maximum-scale=2.0; user-scalable=1;" />
     <link rel="shortcut icon" href="<?php echo $static_base_url; ?>/static/images/favicon.ico" />
 </head>
 <body>
