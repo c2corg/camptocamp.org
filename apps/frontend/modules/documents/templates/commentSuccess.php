0c52c5b2aa862b74b0993a0a6ec4005ac0d10d4c
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2010-10-11 19:12:52 +0000

    correction smiley dans commentaires de doc
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3053 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/documents/templates/commentSuccess.php
+++ apps/frontend/modules/documents/templates/commentSuccess.php
@@ -81,6 +81,12 @@ use_stylesheet('/static/css/forums.css');
 </div>
 
 <?php
+$post_id_list = array();
+foreach ($comments as $comment)
+{
+    $post_id_list[] = $comment['id'];
+}
+
 foreach ($comments as $comment):
     // Switch the background color for every message.
     $bg_switch = ($bg_switch) ? $bg_switch = false : $bg_switch = true;
@@ -140,7 +146,7 @@ foreach ($comments as $comment):
                         <p>
                         <?php
                             $text = $comment->message;
-                            $text = parse_message($text);
+                            $text = parse_message($text, false, $post_id_list);
                             $text = htmlspecialchars_decode($text, ENT_NOQUOTES); // parse_message always use html_special_chars, and so does retrieval of the text
                             echo $text;
                             ?>
