b3bd18615b9722afb8badbe63e5336ec7d826f01
Lionel Besson <lionel.besson@gmail.com>
2010-10-19 16:43:40 +0000

    do not show signatures in mobile version
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3096 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/forums/viewtopic.php
+++ web/forums/viewtopic.php
@@ -663,7 +663,7 @@ foreach ($posts_list as $cur_post)
 	$cur_post['message'] = parse_message($cur_post['message'], $cur_post['hide_smilies'], $post_id_list);
 
 	// Do signature parsing/caching
-	if ($poster_data['signature'] != '' && $pun_user['show_sig'] != '0')
+	if ($poster_data['signature'] != '' && $pun_user['show_sig'] != '0' && !c2cTools::mobileVersion())
 	{
 		if (isset($signature_cache[$cur_post['poster_id']]))
 			$signature = $signature_cache[$cur_post['poster_id']];
