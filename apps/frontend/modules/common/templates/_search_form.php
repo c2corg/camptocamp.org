435512310de0905ff2d0a954a1d53e4a8ddf7134
Lionel Besson <lionel.besson@gmail.com>
2011-01-03 19:04:19 +0000

    disable autocomplete in search form (because text search is far too slow at the moment)
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3450 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/common/templates/_search_form.php
+++ apps/frontend/modules/common/templates/_search_form.php
@@ -3,7 +3,7 @@ use_helper('MyForm');
 echo form_tag('documents/search', array('method' => 'get', 'class' => 'search'));
 echo '<div class="sbox">';
 $prefix = isset($prefix) ? $prefix : '';
-$autocomplete = c2cTools::mobileVersion() ? false : (isset($autocomplete) ? $autocomplete : true);
+$autocomplete = c2cTools::mobileVersion() ? false : (isset($autocomplete) ? $autocomplete : false);
 echo search_box_tag($prefix, $autocomplete);
 echo '</div></form>';
 ?>
