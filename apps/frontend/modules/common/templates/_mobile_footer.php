ac0bf53b1b0a178b002abf71629a42fded6ef134
Lionel Besson <lionel.besson@gmail.com>
2011-01-04 11:42:51 +0000

    mobile footer
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3453 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/common/templates/_mobile_footer.php
+++ apps/frontend/modules/common/templates/_mobile_footer.php
@@ -27,7 +27,7 @@ if ((bool)sfConfig::get('app_mobile_version_ads'))
 <div id="footer"<?php echo $class ?>>
     <div id="footer_cc">
         <div id="footer_cc_text">
-            <p><?php echo link_to(__('Home'), '@homepage'), ' ', link_to(__('web version of the site'), 'http://'.$classic_hostname,
+            <p><?php echo link_to(__('Home'), '@homepage'), ' | ', link_to(__('web version of the site'), 'http://'.$classic_hostname,
                                   array('onclick' => "document.location.href = document.location.href.replace('$mobile_hostname', '$classic_hostname'); return false;")) ?></p>
             <p>&copy; 1997-<?php echo date('Y') . ' ' . link_to('Camptocamp-Association', getMetaArticleRoute('association')) ?></p>
             <p><?php echo __('CNIL declaration #') ?>1175560 - <?php echo __('disclaimer notice') ?></p>
