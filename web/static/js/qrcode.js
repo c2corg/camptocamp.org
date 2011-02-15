a0695d6e3c03c019033e88eebc62d80d923256fb
Lionel Besson <lionel.besson@gmail.com>
2011-02-15 13:44:02 +0000

    smaller qr code (with minimal url)
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3560 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/static/js/qrcode.js
+++ web/static/js/qrcode.js
@@ -3,7 +3,7 @@ Event.observe(window, 'load', function()
   if ($('footer')) {
     $('footer').insert({ after: new Element('img',
                                 { 'class':'qrcode printonly',
-                                  src: 'https://chart.googleapis.com/chart?chs=120x120&cht=qr&choe=UTF-8&chl='
-                                       + encodeURIComponent(window.location.href) }) });
+                                  src: 'https://chart.googleapis.com/chart?chs=70x70&cht=qr&choe=UTF-8&chl='
+                                       + encodeURIComponent($$('.mobile_link').first().href + /\d+/.exec(window.location.href)) }) });
   }
 });
