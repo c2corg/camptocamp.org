16489baa2deca696b15d704e9bed9b437304e94f
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2011-03-15 22:30:43 +0000

    suppression liste des cartes associées aux docs associés à un article
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3646 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/articles/templates/viewSuccess.php
+++ apps/frontend/modules/articles/templates/viewSuccess.php
@@ -25,7 +25,6 @@ if ($is_not_archive)
 {
     echo '<div class="all_associations">';
     include_partial('areas/association', array('associated_docs' => $associated_areas, 'module' => 'areas'));
-    include_partial('documents/association', array('associated_docs' => $associated_maps, 'module' => 'maps')); 
     echo '</div>';
 }
 echo end_section_tag();
