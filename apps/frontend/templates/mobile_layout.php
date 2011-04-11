8f20c6897b95a4cb8eac581b12d4685cdd8a9c18
Lionel Besson <lionel.besson@gmail.com>
2011-04-11 15:01:41 +0000

    revert bad commit
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@3690 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/templates/mobile_layout.php
+++ apps/frontend/templates/mobile_layout.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Layout for the mobile version. html5, baby!
+ * Layout for the mobile version
  */
 $lang_code = __('meta_language');
 $module = $sf_context->getModuleName();
@@ -14,11 +14,12 @@ use_helper('MyMinify', 'MetaLink');
 $static_base_url = sfConfig::get('app_static_url');
 $response = sfContext::getInstance()->getResponse();
 $response->addJavascript('/static/js/fold.js', 'head_last');
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
-<!doctype html>
-<html lang="<?php echo $lang_code ?>">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang_code ?>">
 <head>
-    <meta charset="utf-8">
     <?php
         $debug = (bool)sfConfig::get('app_minify_debug');
         $combine = !$debug;
