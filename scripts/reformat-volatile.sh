32cd7272d48dee92f767b6cac207dc738ef64acd
Marc Fournier <marc.fournier@camptocamp.org>
2011-11-03 06:54:40 +0000

    symfony cache purging improvements
     - added support for different purging methods
     - added device reformating method
    
    
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4107 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ scripts/reformat-volatile.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+DEV="/dev/mapper/vg0-volatile"
+
+if [ $(id -u) != 0 ]; then
+  echo "this script can only be run as root"
+  exit 1
+fi
+
+MNT=$(grep $DEV /etc/mtab | cut -f2 -d' ')
+
+if test -d $MNT; then
+  DIRS=$(ls $MNT)
+
+  (umount $DEV && mkfs.xfs -f $DEV && mount $DEV) || exit 1
+
+  chown www-data. $MNT
+
+  for dir in $DIRS; do
+    mkdir -p $MNT/$dir
+    chown www-data. $MNT/$dir
+  done
+
+fi
