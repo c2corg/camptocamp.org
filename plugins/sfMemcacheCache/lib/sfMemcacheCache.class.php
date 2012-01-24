ea2d4c2c5b8576955b500a9ca11f6329db04201d
Marc Fournier <marc.fournier@camptocamp.org>
2012-01-24 08:24:22 +0000

    1e tentative de migrer le cache à memcached.
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4346 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ plugins/sfMemcacheCache/lib/sfMemcacheCache.class.php
@@ -0,0 +1,192 @@
+<?php
+
+/**
+ * Cache class that stores content in a memcached k/v store. Based on the
+ * sqlite store.
+ *
+ * @package    symfony
+ * @subpackage cache
+ * @author     Marc Fournier <marc.fournier@camptocamp.org>
+ * @version    SVN: $Id: sfSQLiteCache.class.php 3935 2007-05-06 10:07:08Z fabien $
+ */
+class sfMemcacheCache extends sfCache
+{
+
+  protected $memcache = null;
+  protected $debug = null;
+
+
+ /**
+  * Constructor.
+  *
+  * @param string The database name
+  */
+  public function __construct($database = null)
+  {
+    if (!extension_loaded('memcache'))
+    {
+      throw new sfConfigurationException('sfMemcacheCache class needs "memcache" extension');
+    }
+
+  }
+
+ /**
+   * Initializes the cache.
+   *
+   * @param array An array of options
+   * Available options:
+   *  - database:                database name
+   *  - automaticCleaningFactor: disable / tune automatic cleaning process (int)
+   *
+   */
+  public function initialize($options = array())
+  {
+
+    $this->memcache = new Memcache();
+    $this->debug = isset($options['debug']) ? $options['debug'] : null;
+
+    foreach ($options['servers'] as $server)
+    {
+      $port = isset($server['port']) ? $server['port'] : 11211;
+        if (!$this->memcache->addServer($server['host'], $port, isset($server['persistent']) ? $server['persistent'] : true))
+        {
+          throw new sfInitializationException(sprintf('Unable to connect to the memcache server (%s:%s).', $server['host'], $port));
+        }
+    }
+  }
+
+
+ /**
+   * Destructor.
+   */
+  #public function __destruct()
+  #{
+  #  sqlite_close($this->conn);
+  #}
+
+ /**
+  * Tests if a cache is available and (if yes) returns it.
+  *
+  * @param  string  The cache id
+  * @param  string  The name of the cache namespace
+  * @param  boolean If set to true, the cache validity won't be tested
+  *
+  * @return string  The data in the cache (or null if no cache available)
+  *
+  * @see sfCache
+  */
+  public function get($id, $namespace = self::DEFAULT_NAMESPACE, $doNotTestCacheValidity = false)
+  {
+    $default = null;
+    $value = $this->memcache->get($this->keyName($id, $namespace));
+
+    if ($this->debug) { $this->debugMsg("GET", $id, $namespace, $value); }
+    return false === $value ? $default : $value;
+  }
+
+  /**
+   * Returns true if there is a cache for the given id and namespace.
+   *
+   * @param  string  The cache id
+   * @param  string  The name of the cache namespace
+   * @param  boolean If set to true, the cache validity won't be tested
+   *
+   * @return boolean true if the cache exists, false otherwise
+   *
+   * @see sfCache
+   */
+  public function has($id, $namespace = self::DEFAULT_NAMESPACE, $doNotTestCacheValidity = false)
+  {
+    if ($this->debug) { $this->debugMsg("HAS", $id, $namespace); }
+    return !(false === $this->memcache->get($this->keyName($id, $namespace)));
+  }
+  
+ /**
+  * Saves some data in the cache.
+  *
+  * @param string The cache id
+  * @param string The name of the cache namespace
+  * @param string The data to put in cache
+  *
+  * @return boolean true if no problem
+  *
+  * @see sfCache
+  */
+  public function set($id, $namespace = self::DEFAULT_NAMESPACE, $data)
+  {
+    #$lifetime = null === $lifetime ? $this->getOption('lifetime') : $lifetime;
+    #$lifetime = time() + null;
+    $lifetime = 0;
+
+    if (false !== $this->memcache->replace($this->keyName($id, $namespace), $data, false, $lifetime))
+    {
+      if ($this->debug) { $this->debugMsg("REPLACE", $id, $namespace, $data); }
+      return true;
+    }
+
+    if ($this->debug) { $this->debugMsg("SET", $id, $namespace, $data); }
+    return $this->memcache->set($this->keyName($id, $namespace), $data, false, $lifetime);
+  }
+
+ /**
+  * Removes an element from the cache.
+  *
+  * @param string The cache id
+  * @param string The name of the cache namespace
+  *
+  * @return boolean true if no problem
+  *
+  * @see sfCache
+  */
+  public function remove($id, $namespace = self::DEFAULT_NAMESPACE)
+  {
+    if ($this->debug) { $this->debugMsg("REMOVE", $id, $namespace); }
+    return $this->memcache->delete($this->keyName($id, $namespace), 0);
+  }
+
+ /**
+  * Cleans the cache.
+  *
+  * If no namespace is specified all cache files will be destroyed
+  * else only cache files of the specified namespace will be destroyed.
+  *
+  * @param string The name of the cache namespace
+  *
+  * @return boolean true if no problem
+  */
+  public function clean($namespace = null, $mode = 'all')
+  {
+    if ('all' === $mode)
+    {
+      return $this->memcache->flush();
+    }
+  }
+
+  public function lastModified($id, $namespace = self::DEFAULT_NAMESPACE)
+  {
+    if ($this->debug) { $this->debugMsg("LASTMOD", $id, $namespace); }
+    return 0;
+  }
+
+ /**
+  * @return an md5 string
+  */
+  protected function keyName($id, $namespace)
+  {
+    return md5("sf_" . $id . $namespace);
+  }
+
+  protected function debugMsg($cmd, $id, $namespace, $value=null)
+  {
+    error_log("$cmd: id  = $id");
+    error_log("$cmd: ns  = $namespace");
+    error_log("$cmd: key = " . $this->keyName($id, $namespace));
+    error_log("$cmd: val = " . ($value ? $value : "no value"));
+    error_log("limetime = " . $this->lifeTime . " refresh = " . date('c', $this->refreshTime));
+    #if (sfConfig::get('sf_logging_enabled'))
+    #{
+    #  sfContext::getInstance()->getLogger()->info($message);
+    #}
+  }
+
+}
