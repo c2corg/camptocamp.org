cf830286e16d9dac55df2ba2e070e81d22417ac3
Lionel Besson <lionel.besson@gmail.com>
2012-03-21 17:33:39 +0000

    missing file for json export
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4567 5b3c568a-86f9-4a59-a47a-7be284f604b4

---
+++ apps/frontend/modules/documents/templates/jsonlistSuccess.php
@@ -0,0 +1,55 @@
+<?php 
+use_helper('Pagination', 'Link');
+
+$id = $sf_params->get('id');
+$lang = $sf_params->get('lang');
+$module = $sf_context->getModuleName();
+
+if (!isset($items) && $nb_results > 0)
+{
+    $items = $pager->getResults('array', ESC_RAW);
+    $items = Language::parseListItems($items, c2cTools::module2model($module));
+}
+elseif ($nb_results == 0)
+{
+    $items = array();
+}
+
+$totalItems = $pager->getNbResults();
+$startIndex = $pager->getMaxPerPage() * ($pager->getPage() - 1) + 1;
+$count = min($pager->getMaxPerPage(), $pager->getNbResults() - ($pager->getPage() - 1) * ($pager->getMaxPerPage()));
+$hasPreviousPage = ($pager->getPage() != 1);
+$hasNextPage = ($pager->getPage() != $pager->getLastPage() && $nb_results);
+
+// compute prev and next uris
+if ($hasPreviousPage || $hasNextPage)
+{
+    $request = sfContext::getInstance()->getRequest();
+    $orderby = $request->getParameter('orderby');
+    $order = $request->getParameter('order');
+    $params = array('orderby' => $orderby, 'order' => $order);
+    $uri = _addUrlParameters(_getBaseUri(), array(), $params);
+    $uri .= _getSeparator($uri) . 'page=';
+}
+?>
+{                                                                                          
+  "type": "application/json",                                                              
+  "totalItems": <?php echo $totalItems; ?>,
+  "count": <?php echo $count; ?>,
+  "startIndex": <?php echo $startIndex; ?>,
+<?php if ($hasNextPage): ?>
+  "nextPage": "<?php echo absolute_link(url_for($uri . $pager->getNextPage())); ?>",
+<?php endif; if ($hasPreviousPage): ?>
+  "previousPage": "<?php echo absolute_link(url_for($uri . $pager->getPreviousPage())); ?>",
+<?php endif; ?>
+  "items": [
+  <?php
+  $sep = '';
+  foreach ($items as $item)
+  {
+      echo $sep;
+      include_partial($module . '/jsonlist_body',  array('item' => $item));
+      $sep = ',';
+  } ?>
+  ]
+}
