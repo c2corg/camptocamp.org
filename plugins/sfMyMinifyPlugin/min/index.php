3ca9c3f54880546714cd84d38afd326a4e6d5435
Lionel Besson <lionel.besson@gmail.com>
2012-03-22 18:28:44 +0000

    update minify lib to 2.1.5
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4570 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- plugins/sfMyMinifyPlugin/min/index.php
+++ plugins/sfMyMinifyPlugin/min/index.php
@@ -25,24 +25,27 @@ Minify::setCache(
 
 if ($min_documentRoot) {
     $_SERVER['DOCUMENT_ROOT'] = $min_documentRoot;
-} elseif (0 === stripos(PHP_OS, 'win')) {
-    Minify::setDocRoot(); // IIS may need help
+    Minify::$isDocRootSet = true;
 }
 
 $min_serveOptions['minifierOptions']['text/css']['symlinks'] = $min_symlinks;
+// auto-add targets to allowDirs
+foreach ($min_symlinks as $uri => $target) {
+    $min_serveOptions['minApp']['allowDirs'][] = $target;
+}
 
-if ($min_allowDebugFlag && isset($_GET['debug'])) {
-    $min_serveOptions['debug'] = true;
+if ($min_allowDebugFlag) {
+    require_once 'Minify/DebugDetector.php';
+    $min_serveOptions['debug'] = Minify_DebugDetector::shouldDebugRequest($_COOKIE, $_GET, $_SERVER['REQUEST_URI']);
 }
 
 if ($min_errorLogger) {
     require_once 'Minify/Logger.php';
     if (true === $min_errorLogger) {
         require_once 'FirePHP.php';
-        Minify_Logger::setLogger(FirePHP::getInstance(true));
-    } else {
-        Minify_Logger::setLogger($min_errorLogger);
+        $min_errorLogger = FirePHP::getInstance(true);
     }
+    Minify_Logger::setLogger($min_errorLogger);
 }
 
 // check for URI versioning
@@ -55,7 +58,12 @@ if (isset($_GET['g'])) {
 }
 if (isset($_GET['f']) || isset($_GET['g'])) {
     // serve!   
-    Minify::serve('MinApp', $min_serveOptions);
+
+    if (! isset($min_serveController)) {
+        require 'Minify/Controller/MinApp.php';
+        $min_serveController = new Minify_Controller_MinApp();
+    }
+    Minify::serve($min_serveController, $min_serveOptions);
         
 } elseif ($min_enableBuilder) {
     header('Location: builder/');
