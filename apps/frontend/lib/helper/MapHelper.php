81056f3c8eef4f93be14355212c7de533c9448a9
Lionel Besson <lionel.besson@gmail.com>
2012-04-10 19:11:14 +0000

    revert r4669 since function called in fold.js
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4673 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/lib/helper/MapHelper.php
+++ apps/frontend/lib/helper/MapHelper.php
@@ -107,7 +107,9 @@ function show_map($container_div, $document, $lang, $layers_list = null, $height
 
         // FIXME extjs uses document.write with ie, so we cannot for the moment use async loading with ie
         $html .= javascript_tag('
-if (!Prototype.Browser.IE) { var c2corgloadMapAsync = true; var jsurl = \''.$c2c_script_url.'\';  var a = document.createElement(\'script\'), h = document.getElementsByTagName(\'head\')[0]; a.async = 1; a.src = jsurl; h.appendChild(a); }');
+if (!Prototype.Browser.IE) { var c2corgloadMapAsync = true; }
+function c2c_asyncload(jsurl) { var a = document.createElement(\'script\'), h = document.getElementsByTagName(\'head\')[0]; a.async = 1; a.src = jsurl; h.appendChild(a); }
+function asyncloadmap() { if (!Prototype.Browser.IE) { c2c_asyncload(\''.$c2c_script_url.'\'); }}');
     }
 
     return $html;
