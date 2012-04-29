59b06b84ba2db4303ac6f70f65d6d6626cd858de
Lionel Besson <lionel.besson@gmail.com>
2012-04-29 11:00:57 +0000

    sass
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@4821 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/static/css/c2c_sass_functions.rb
+++ web/static/css/c2c_sass_functions.rb
@@ -25,6 +25,17 @@ module Sass::Script::Functions
      raise Sass::SyntaxError.new("File " + file.value + " not found")
   end
 
+  # Returns a new list after removing any non-true values
+  def compact(*args)
+    sep = :comma
+    if args.size == 1 && args.first.is_a?(Sass::Script::List)
+      list = args.first
+      args = list.value
+      sep = list.separator
+    end
+    Sass::Script::List.new(args.reject{|a| !a.to_bool}, sep)
+  end
+
   declare :c2chash, :args => [:file]
   declare :datauri, :args => [:file]
 
