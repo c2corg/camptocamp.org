be333b6065b4be1dc99e89034d3191a1c1e8e444
Lionel Besson <lionel.besson@gmail.com>
2012-06-18 17:32:10 +0000

    enable zoom for forums in mobile version
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5037 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/forums/include/user/symfony_style.php
+++ web/forums/include/user/symfony_style.php
@@ -18,9 +18,18 @@ $sf_response->addJavascript('/static/js/prototype.js', 'head_first');
 $sf_response->addJavascript('/static/js/effects.js', 'head');
 $sf_response->addJavascript('/static/js/controls.js', 'head');
 $sf_response->addJavascript('/static/js/submit.js');
+if ($mobile_version)
+{
+    $sf_response->addJavascript('/static/js/viewport_fix.js');
+}
 
 sfLoader::loadHelpers(array('Helper', 'MyMinify', 'Asset'));
 $debug = defined('PUN_DEBUG');
+
+if ($mobile_version): ?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<?php endif;
+
 minify_include_main_stylesheets(!$debug, $debug);
 minify_include_head_javascripts(!$debug, $debug);
 
@@ -29,7 +38,6 @@ if (!$mobile_version): ?>
 <!--[if IE 7]><link rel="stylesheet" type="text/css" media="all" href="<?php echo PUN_STATIC_URL . '/' . sfTimestamp::getTimestamp(PUN_STATIC_URL . '/static/css/ie7.css'); ?>/static/css/ie7.css" /><![endif]-->
 <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <?php else: ?>
-<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 <link rel="apple-touch-icon" href="<?php echo PUN_STATIC_URL; ?>/static/images/apple-touch-icon.png" />
 <link rel="apple-touch-icon-precomposed" href="<?php echo PUN_STATIC_URL; ?>/static/images/apple-touch-icon.png" />
 <?php endif; ?>
