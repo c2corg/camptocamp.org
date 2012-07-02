c22ab6bf3de0b5c548dd91a5f95cad1203811b33
Lionel Besson <lionel.besson@gmail.com>
2012-07-02 18:17:44 +0000

    host html5shiv on c2c (considered best practice) v3.6rc1
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5067 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -53,7 +53,7 @@ $response->addJavascript('/static/js/fold.js', 'head_last');
     ?>
     <!--[if IE 6]><link rel="stylesheet" type="text/css" media="all" href="<?php echo $static_base_url . '/' . sfTimestamp::getTimestamp('/static/css/ie6.css'); ?>/static/css/ie6.css" /><![endif]-->
     <!--[if IE 7]><link rel="stylesheet" type="text/css" media="all" href="<?php echo $static_base_url. '/' . sfTimestamp::getTimestamp('/static/css/ie7.css'); ?>/static/css/ie7.css" /><![endif]-->
-    <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="<?php echo $static_base_url. '/' . sfTimestamp::getTimestamp('/static/js/html5shiv.js'); ?>/static/js/html5shiv.js"></script><![endif]-->
     <?php
         minify_include_head_javascripts($combine, $debug);
         echo auto_discovery_link_tag('rss', $rss);
