52940ed7d766d45b70913414e356f1d810a3b5c0
Lionel Besson <lionel.besson@gmail.com>
2012-07-11 06:47:18 +0000

    do not let non-moderators change area type
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5082 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/areas/templates/_data.php
+++ apps/frontend/modules/areas/templates/_data.php
@@ -3,9 +3,9 @@
 <div class="article_contenu">
     <ul id="article_gauche_5050" class="data">
         <?php 
-        li(field_data_from_list($document, 'area_type', 'mod_areas_area_types_list'));
         if ($sf_user->hasCredential('moderator') && $document->get('geom_wkt'))
         {
+        	li(field_data_from_list($document, 'area_type', 'mod_areas_area_types_list'));
             li(field_export($document->get('module'), $sf_params->get('id'), $sf_params->get('lang'), $sf_params->get('version')));
         }
         ?>
