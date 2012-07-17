ec0079e2668acd3eec30157cf9bcbf1b273004b4
Lionel Besson <lionel.besson@gmail.com>
2012-07-17 07:40:39 +0000

    revert r5082
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5083 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/areas/templates/_data.php
+++ apps/frontend/modules/areas/templates/_data.php
@@ -2,10 +2,10 @@
 
 <div class="article_contenu">
     <ul id="article_gauche_5050" class="data">
-        <?php 
+        <?php
+        li(field_data_from_list($document, 'area_type', 'mod_areas_area_types_list')); 
         if ($sf_user->hasCredential('moderator') && $document->get('geom_wkt'))
         {
-        	li(field_data_from_list($document, 'area_type', 'mod_areas_area_types_list'));
             li(field_export($document->get('module'), $sf_params->get('id'), $sf_params->get('lang'), $sf_params->get('version')));
         }
         ?>
