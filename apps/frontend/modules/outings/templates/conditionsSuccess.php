3256203d72393ad13dbbe099d9ec2f658325d623
Frédéric Bunoz <frederic.bunoz@camptocamp.org>
2012-09-15 01:04:39 +0000

    page conditions : lien vers la liste d'images associées aux sorties
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5124 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/outings/templates/conditionsSuccess.php
+++ apps/frontend/modules/outings/templates/conditionsSuccess.php
@@ -21,7 +21,8 @@ if (!isset($items) || count($items) == 0):
 else:
     
     echo '<p class="list_header">'
-       . link_to_outings(__('Show as a list'));
+       . link_to_outings(__('Show as a list'))
+       . ' &nbsp; ' . link_to_associated_images(__('List all linked images'), $module, array('orderby' => 'odate', 'order' => 'desc'));
     if ($show_images)
     {
         echo '';
