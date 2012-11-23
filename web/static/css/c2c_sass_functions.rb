f7d96b075745f023e02beee40b175f6da3440206
Lionel Besson <lionel.besson@gmail.com>
2012-11-23 07:13:17 +0000

    sass ruby functions
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5325 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/static/css/c2c_sass_functions.rb
+++ web/static/css/c2c_sass_functions.rb
@@ -6,24 +6,69 @@ module Sass::Script::Functions
   # Given the (css) path to a file, returns it prefixed with the 'hash'
   # (which is in fact the 8 first chars of mda5, which is shorter and
   # provides enough differentiation between two versions of a file)
-  def c2chash(file)
+  def c2chash(file, pixelratio = Sass::Script::Number.new(1))
     assert_type file, :String
-    ffile = "../.." + file.value
-    timestamp = MD5.new(File.read(ffile))
-    Sass::Script::String.new("/" + timestamp.to_s[0,8] + file.value)
+    assert_type pixelratio, :Number
+
+    ffile = pixelratio_file(file, pixelratio)
+    hash = MD5.new(File.read(ffile))
+    Sass::Script::String.new("/" + hash.to_s[0,8] + file.value)
   rescue
-    raise Sass::SyntaxError.new("File " + file.value + " not found")
+    raise Sass::SyntaxError.new("File " + file.value + ffile + " not found")
   end
 
+  declare :c2chash, :args => [:file]
+  declare :c2chash, :args => [:file, :pixelratio]
+
   # Given the (css) path to a file, returns it as data:uri
-  def datauri(file)
+  def datauri(file, pixelratio = Sass::Script::Number.new(1))
     assert_type file, :String
-    ffile = "../.." + file.value
+    assert_type pixelratio, :Number
+
+    ffile = pixelratio_file(file, pixelratio)
     Sass::Script::String.new("data:image/" + File.extname(ffile)[1..-1] \
       + ";base64," + Base64.encode64(File.read(ffile)).strip.gsub(/\n/,''))
   rescue
      raise Sass::SyntaxError.new("File " + file.value + " not found")
   end
+
+  declare :datauri, :args => [:file]
+  declare :datauri, :args => [:file, :pixelratio]
+
+  # Given a file and a pixel ratio, return best candidate file
+  # For example, looking for file img.png with pixelratio 2 will
+  # return img.2.png if it exits, img.png otherwise
+  # A pixelratio will end with no suffix appended
+  def pixelratio_file(file, pixelratio)
+    assert_type file, :String
+    assert_type pixelratio, :Number
+
+    filepath = "../.." + file.value
+    if pixelratio.value != 1
+      ext = File.extname(file.value)
+      basename = File.basename(file.value, ext)
+      dir = File.dirname(file.value)
+      pixelratio_filepath = "../.." + dir + "/" + basename + pixelratio.value.to_s + "x" + ext
+      if File.exists?(pixelratio_filepath)
+        filepath = pixelratio_filepath
+      else
+        Sass::Util.sass_warn("Warning: using default pixel ratio file for " + file.value)
+      end
+    end
+    filepath
+  end
+
+  # Given a file, returns its width
+  # and height
+  def file_dimensions(file)
+    assert_type file, :String
+
+    filepath = "../.." + file.value
+    size = %x[identify -format "%[width]px %[height]px" #{filepath}]
+    Sass::Script::String.new(size.strip)
+  end
+
+  declare :file_dimensions, :args => [:file]
 
   # Returns a new list after removing any non-true values
   def compact(*args)
@@ -47,8 +92,5 @@ module Sass::Script::Functions
       raise ArgumentError.new("#{value.inspect} is not a list")
     end
   end
-
-  declare :c2chash, :args => [:file]
-  declare :datauri, :args => [:file]
 
 end
