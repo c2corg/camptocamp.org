bbc3cd1003ea0fa35a5df56b409d9b43a0a11662
Lionel Besson <lionel.besson@gmail.com>
2012-12-10 16:49:02 +0000

    http://www.camptocamp.org/forums/viewtopic.php?pid=1764899#p1764899
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5427 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/articles/templates/_data.php
+++ apps/frontend/modules/articles/templates/_data.php
@@ -1,10 +1,9 @@
 <?php use_helper('Field'); ?>
-
-    <ul id="article_gauche_5050" class="data">
-        <?php
-        disp_doc_type('article');
-        li(field_data_from_list($document, 'categories', 'mod_articles_categories_list', array('raw' => true)));
-        li(field_activities_data_if_set($document));
-        li(field_data_from_list($document, 'article_type', 'mod_articles_article_types_list'));
-        ?>
-    </ul>
+<ul id="article_gauche_5050" class="data">
+    <?php
+    disp_doc_type('article');
+    li(field_data_from_list($document, 'categories', 'mod_articles_categories_list', array('multiple' => true)));
+    li(field_activities_data_if_set($document));
+    li(field_data_from_list($document, 'article_type', 'mod_articles_article_types_list'));
+    ?>
+</ul>
