ee5fbf2372346b562b5b4c19ae1be2dc1266cce1
Lionel Besson <lionel.besson@gmail.com>
2013-02-01 15:12:10 +0000

    all image types can be updated
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5534 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/images/templates/_form_fields.php
+++ apps/frontend/modules/images/templates/_form_fields.php
@@ -28,10 +28,7 @@ echo object_group_dropdown_tag($document, 'categories', 'mod_images_categories_l
                                false, null, 'image_categories');
 
 echo form_section_title('Description', 'form_desc', 'preview_desc');
-if ($image_type == 1 || $image_type == 3) // collaborative or copyright image
-{
-    echo file_upload_tag('image_new_version', false, 'file', true);
-}
+echo file_upload_tag('image_new_version', false, 'file', true);
 echo object_group_bbcode_tag($document, 'description', null, array('class' => 'mediumtext', 'abstract' => true));
 
 include_partial('documents/form_history');
