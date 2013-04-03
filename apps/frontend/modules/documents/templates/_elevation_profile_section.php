ac95de1c35349f4801d56987995f8220c96b8de4
Lionel Besson <lionel.besson@gmail.com>
2013-04-03 17:50:08 +0000

    elevation profile
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5630 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/documents/templates/_elevation_profile_section.php
+++ apps/frontend/modules/documents/templates/_elevation_profile_section.php
@@ -2,23 +2,35 @@
 use_helper('MyMinify');
 echo start_section_tag('Elevation profile', 'elevation_profile_container', 'closed');
 ?>
-<!-- TODO make that cleaner -->
-<form>
-  <label><input type="radio" name="profile_mode" value="distance" checked /> Distance</label>
-  <label><input type="radio" name="profile_mode" value="time" /> Time</label>
-</form>
+<div class="elevation_profile_loading ui-spinner">
+   <span class="side side-left"><span class="fill"></span></span>
+  <span class="side side-right"><span class="fill"></span></span>
+</div>
+<div class="elevation_profile_controls" style="display: none;">
+  <div>
+  <!-- TODO add some information here? like number of points, ...
+       And later on, options like move the map, ... -->
+  </div>
+  <form class="xaxis-dimension">
+    <?php echo __('x axis:') ?><br />
+    <label><input type="radio" name="profile_mode" value="distance" checked /> Distance</label>
+    <label><input type="radio" name="profile_mode" value="time" /> Time</label>
+  </form>
+</div>
+<div id="elevation_profile">
+</div>
 <?php
 echo end_section_tag();
 
-// FIXME d3js uses function sunsupported by ie<=8 but
+// FIXME d3js uses functions unsupported by ie<=8 but
 // feature detecting svg support should be enough//
 $script_url = minify_get_combined_files_url(array('/static/js/d3.v3.js',
                                                   '/static/js/elevation_profile.js'),
                                             (bool) sfConfig::get('app_minify_debug'));
-$js = "var div = document.createElement('div');
+$js = "(function() { \"use strict\";
+var div = document.createElement('div');
 div.innerHTML = '<svg/>';
 var svg_supported = (div.firstChild && div.firstChild.namespaceURI) == 'http://www.w3.org/2000/svg';
-
 if (!svg_supported) {
   $('elevation_profile_container_tbg').hide();
 } else {
@@ -37,23 +49,31 @@ if (!svg_supported) {
       kilometers: '" . __('kilometers') . "'
     }
   };
+  // we test if css animations are supported, else replace the spinner by
+  // an animated gif
+  // we don't test for css transform support (which is also needed) since every
+  // browser that supports animations also supports transforms
+  var animation = false;
+  var props = ['animationName', 'WebkitAnimationName', 'MozAnimationName',
+               'oAnimationName', 'msAnimationName'];
+  var elt = $$('.ui-spinner .fill')[0];
+  for (var i in props) {
+    var prop = props[i];
+    if (elt.style[prop] !== undefined) {
+      animation = true;
+      break;
+    }
+  }
+  if (!animation) {
+    elt = $$('.elevation_profile_loading')[0];
+    elt.innerHTML = '" . image_tag('/static/images/indicator.gif') . "';
+    elt.removeClassName('ui-spinner');
+  }
   // define function for opening+change class once intializaed
-  function c2c_load_elevation_profile() {
+  window.c2c_load_elevation_profile = function() {
     c2c_asyncload('$script_url');
     $('elevation_profile_container_section_container').addClassName('profile_loaded');
   }
-}";
-
-// TODO
-// - i18n
-// - waiting UI (use beautiful waiting svg :p)
-// - nav
-// - not supproted -> also hide nav?
-// - mobile -> no animation !
-// mobile version
-// is y'a pas d'altitude ??
-// function()()
-// gpx sans time
-// check also altitude...
+}})()";
 
 echo javascript_tag($js);
--- web/static/js/elevation_profile.js
+++ web/static/js/elevation_profile.js
@@ -1,258 +1,278 @@
 (function() {
   "use strict";
 
-  var size = { width: 560, height: 300 },
+  var i18n = window.c2cprofile.i18n;
+  var mobile = $$('html')[0].hasClassName('mobile');
+
+  // we don't use #elevation_profile_container_section_container
+  var width = $('elevation_profile_container_tbg').getWidth();
+  if (!mobile || window.innerWidth > 1000) {
+    width -= 250;
+    $$('.elevation_profile_controls')[0].addClassName('left');
+  }
+
+  var size = { width: width, height: 300 },
     margin = { top: 20, right: 20, bottom: 30, left: 50 },
     width = size.width - margin.left - margin.right,
     height = size.height - margin.top - margin.bottom;
 
-  var i18n = window.c2cprofile.i18n;
-  var mobile = $$('html')[0].hasClassName('mobile');
-
   // d3.xml() is not working well with ie9 and ie10
   // so instead, we retrieve the text and use DOMParser on it
-  d3.xhr(window.c2cprofile.track, "application/xml", function(request) {
-    var greatArc = d3.geo.greatArc();
-    var mode = "distance";
-    var data = [];
-    var dtot = 0;
-    var d;
-    var duration;
+  d3.xhr(window.c2cprofile.track, "application/xml")
+    // TODO progress events?
+    //.on("progress", function() {
+    //  console.log(d3.event.loaded);
+    //})
+    .on("error", function(error) {
+      alert("Something went wrong (" + error + "). Please notify us");
+    })
+    .get(function(error, request) {
+      var greatArc = d3.geo.greatArc();
+      var mode = "distance";
+      var data = [];
+      var dtot = 0;
+      var d;
+      var duration;
 
-    var parser = new DOMParser();
-    var points = parser.parseFromString(request.responseText, 'text/xml')
-                       .documentElement.getElementsByTagName("trkpt");
+      // hide loading div, displat regular ones
+      $$('.elevation_profile_controls')[0].show();
+      $$('.elevation_profile_loading')[0].hide();
 
-    // some files won't have time elements. We need to handle that
-    var time_available = !!points[0].getElementsByTagName("time").length;
+      var parser = new DOMParser();
+      var points = parser.parseFromString(request.responseText, 'text/xml')
+                         .documentElement.getElementsByTagName("trkpt");
 
-    // TODO hide things
-    // if (!time_available) alert('hide!');
+      // some files won't have time elements. We need to handle that
+      var time_available = !!points[0].getElementsByTagName("time").length;
 
-    // build data points from gpx information
-    var startDate = time_available ? new Date((points[0].getElementsByTagName("time"))[0].textContent) : null;
-    data.push({
-      date: startDate,
-      ele: +points[0].getElementsByTagName("ele")[0].textContent,
-      d: 0,
-      elapsed: time_available ? 0 : null
-    });
-    for (var i=1; i<points.length; i++) {
-      d = greatArc.distance({
-        source: [points[i].getAttribute("lon"), points[i].getAttribute("lat")],
-        target: [points[i-1].getAttribute("lon"), points[i-1].getAttribute("lat")]
-      }) * 6371;
-      var date = time_available ? new Date(points[i].getElementsByTagName("time")[0].textContent) : null;
+      if (!time_available) {
+        $$('.xaxis-dimension')[0].hide();
+      }
+
+      // build data points from gpx information
+      var startDate = time_available ? new Date((points[0].getElementsByTagName("time"))[0].textContent) : null;
       data.push({
-        date: date,
-        ele: +points[i].getElementsByTagName("ele")[0].textContent,
-        d: dtot + d,
-        elapsed: time_available ? date - startDate : null,
+        date: startDate,
+        ele: +points[0].getElementsByTagName("ele")[0].textContent,
+        d: 0,
+        elapsed: time_available ? 0 : null
       });
-      dtot +=d;
-    }
+      for (var i=1; i<points.length; i++) {
+        d = greatArc.distance({
+          source: [points[i].getAttribute("lon"), points[i].getAttribute("lat")],
+          target: [points[i-1].getAttribute("lon"), points[i-1].getAttribute("lat")]
+        }) * 6371;
+        var date = time_available ? new Date(points[i].getElementsByTagName("time")[0].textContent) : null;
+        data.push({
+          date: date,
+          ele: +points[i].getElementsByTagName("ele")[0].textContent,
+          d: dtot + d,
+          elapsed: time_available ? date - startDate : null,
+        });
+        dtot +=d;
+      }
 
-    // Add an SVG element with the desired dimensions and margin
-    var svg = d3.select("#elevation_profile_container_section_container").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      // Add an SVG element with the desired dimensions and margin
+      var svg = d3.select("#elevation_profile").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // Scales and axes
-    var x1 = d3.scale.linear()
-      .range([0, width]);
-
-    var y = d3.scale.linear()
-      .range([height, 0]);
-  
-    var x1Axis = d3.svg.axis()
-      .scale(x1)
-      .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-      .scale(y)
-      .tickFormat(d3.format(".0f"))
-      .orient("left");
-
-    x1.domain(d3.extent(data, function(d) { return d.d; }))
-      .nice();
-
-    var yExtent = d3.extent(data, function(d) { return d.ele; });
-    y.domain(yExtent)
-      .nice();
-
-    if (time_available) {
-      var x2 = d3.time.scale()
+      // Scales and axes
+      var x1 = d3.scale.linear()
         .range([0, width]);
 
-      var x2Axis = d3.svg.axis()
-        .scale(x2)
-        .tickFormat(function(t) { // force display of elapsed time as hrs:mins. It is not datetime!
-          return ~~(t/3600000) + ":" + d3.format("02d")(~~(t%3600000/60000));
-        })
+      var y = d3.scale.linear()
+        .range([height, 0]);
+  
+      var x1Axis = d3.svg.axis()
+        .scale(x1)
         .orient("bottom");
 
-      x2.domain(d3.extent(data, function(d) { return d.elapsed; }))
-        .nice(d3.time.hour);
-    }
+      var yAxis = d3.svg.axis()
+        .scale(y)
+        .tickFormat(d3.format(".0f"))
+        .orient("left");
 
+      x1.domain(d3.extent(data, function(d) { return d.d; }))
+        .nice();
 
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text(i18n.yLegend);
-
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(x1Axis)
-    .append("text")
-      .attr("x", size.width - margin.left - margin.right)
-      .attr("dy", "-.71em")
-      .attr("class", "x axis legend")
-      .style("text-anchor", "end")
-      .text(i18n.x1Legend);
-
-    if (!mobile) {
-      // horizontal line, but with same number of points
-      // for eye-friendly transition NOT FOR MOBILE MAYBE
-      var startline = d3.svg.line()
-        .x(function(d) { return x1(d.d); })
-        .y(function(d) { return y(yExtent[0]); });
-    }
-
-    // data lines
-    var dLine = d3.svg.line()
-      .x(function(d) { return x1(d.d); })
-      .y(function(d) { return y(d.ele); });
-
-    if (time_available) {
-      var tLine = d3.svg.line()
-        .x(function(d) { return x2(d.elapsed); })
-        .y(function(d) { return y(d.ele); });
-    }
-
-    // display line path
-    var line = svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", mobile ? dLine : startline);
-
-    if (!mobile) {
-      line.transition()
-        .duration(1000)
-        .attr("d", dLine);
-
-      // Display point information one hover
-      var focusv = svg.append("line")
-        .attr("class", "target")
-        .attr("x1", 0)
-        .attr("y1", 0)
-        .attr("x2", 0)
-        .attr("y2", size.height - margin.bottom - margin.top)
-        .style("display", "none");
-
-      var focush = svg.append("line")
-        .attr("class", "target")
-        .attr("x1", 0)
-        .attr("y1", 0)
-        .attr("x2", size.width - margin.right - margin.left)
-        .attr("y2", 0)
-        .style("display", "none");
-
-      var focus = svg.append("circle")
-        .attr("class", "circle")
-        .attr("r", 4.5)
-        .style("display", "none");
-
-      var bubble = svg.append("text")
-        .attr("x", (size.width - margin.left - margin.right) / 2)
-        .attr("dy", "-.71em")
-        .attr("class", "bubble")
-        .style("text-anchor", "middle")
-        .style("display", "none")
-        .text("");
-
-      svg.append("rect")
-        .attr("class", "overlay")
-        .attr("width", width)
-        .attr("height", height)
-      .on("mouseover", function() {
-        focus.style("display", null);
-        focush.style("display", null);
-        focusv.style("display", null);
-        bubble.style("display", null);
-      })
-      .on("mouseout", function() {
-        focus.style("display", "none");
-        focush.style("display", "none");
-        focusv.style("display", "none");
-        bubble.style("display", "none");
-      })
-      .on("mousemove", mousemove);
-    }
-
-    var bisectDistance = d3.bisector(function(d) { return d.d; }).left;
-    var bisectDate = d3.bisector(function(d) { return d.elapsed; }).left;
-    var formatDistance = d3.format(".2f");
-    var formatDate = d3.time.format("%H:%M");
-    var formatMinutes = d3.format("02d");
-
-    function mousemove() {
-      var bisect = (mode === "distance") ? bisectDistance : bisectDate;
-      var x0 = (mode === "distance") ? x1.invert(d3.mouse(this)[0])
-                                     : x2.invert(d3.mouse(this)[0]);
-      var i = bisect(data, x0, 1, data.length - 1);
-      var d0 = data[i - 1];
-      var d1 = data[i];
-      var d = (mode === "distance") ? (x0 - d0.d > d1.d - x0 ? d1 : d0)
-                                    : (x0 - d0.elapsed > d1.elapsed - x0 ? d1 : d0);
-
-      var dy = y(d.ele);
-      var dx = (mode === "distance") ? x1(d.d) : x2(d.elapsed);
-
-      focus.attr("transform", "translate(" + dx + "," + dy + ")");
-      focush.attr("transform", "translate(0," + dy + ")");
-      focusv.attr("transform", "translate(" + dx + ",0)");
+      var yExtent = d3.extent(data, function(d) { return d.ele; });
+      y.domain(yExtent)
+        .nice();
 
       if (time_available) {
-        var elapsed = d.elapsed/1000;
-        bubble.text(i18n.elevation + " " + d.ele + i18n.meters + " / " + i18n.distance + " " + formatDistance(d.d) + 
-          i18n.kilometers + " / " + i18n.time + " " + formatDate(d.date) + " / " + i18n.duration + " " +
-          ~~(elapsed/3600) + ":" +formatMinutes(~~(elapsed%3600/60)));
-      } else {
-        bubble.text(i18n.elevation + " " + d.ele + i18n.meters + " / " + i18n.distance + " " +
-          formatDistance(d.d) + i18n.kilometers);
+        var x2 = d3.time.scale()
+          .range([0, width]);
+
+        var x2Axis = d3.svg.axis()
+          .scale(x2)
+          .tickFormat(function(t) { // force display of elapsed time as hrs:mins. It is not datetime!
+            return ~~(t/3600000) + ":" + d3.format("02d")(~~(t%3600000/60000));
+          })
+          .orient("bottom");
+
+        x2.domain(d3.extent(data, function(d) { return d.elapsed; }))
+          .nice(d3.time.hour);
       }
-    }
 
-    // switch xAxis between distance and time
-    if (time_available) {
-      d3.selectAll("input[name=profile_mode]").on("change", function() {
-        var nLine = (this.value === "distance") ? dLine : tLine;
-        var axis = (this.value === "distance") ? x1Axis : x2Axis;
-        var legend = (this.value === "distance") ? i18n.x1Legend : i18n.x2Legend;
-        mode = this.value;
+      svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text(i18n.yLegend);
 
-        if (mobile) {
-          line.attr("d", nLine);
+      svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(x1Axis)
+      .append("text")
+        .attr("x", size.width - margin.left - margin.right)
+        .attr("dy", "-.71em")
+        .attr("class", "x axis legend")
+        .style("text-anchor", "end")
+        .text(i18n.x1Legend);
+
+      if (!mobile) {
+        // horizontal line, but with same number of points
+        // for eye-friendly transition
+        var startline = d3.svg.line()
+          .x(function(d) { return x1(d.d); })
+          .y(function(d) { return y(yExtent[0]); });
+      }
+
+      // data lines
+      var dLine = d3.svg.line()
+        .x(function(d) { return x1(d.d); })
+        .y(function(d) { return y(d.ele); });
+
+      if (time_available) {
+        var tLine = d3.svg.line()
+          .x(function(d) { return x2(d.elapsed); })
+          .y(function(d) { return y(d.ele); });
+      }
+
+      // display line path
+      var line = svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", mobile ? dLine : startline);
+
+      if (!mobile) {
+        line.transition()
+          .duration(1000)
+          .attr("d", dLine);
+
+        // Display point information one hover
+        var focusv = svg.append("line")
+          .attr("class", "target")
+          .attr("x1", 0)
+          .attr("y1", 0)
+          .attr("x2", 0)
+          .attr("y2", size.height - margin.bottom - margin.top)
+          .style("display", "none");
+
+        var focush = svg.append("line")
+          .attr("class", "target")
+          .attr("x1", 0)
+          .attr("y1", 0)
+          .attr("x2", size.width - margin.right - margin.left)
+          .attr("y2", 0)
+          .style("display", "none");
+
+        var focus = svg.append("circle")
+          .attr("class", "circle")
+          .attr("r", 4.5)
+          .style("display", "none");
+
+        var bubble = svg.append("text")
+          .attr("x", (size.width - margin.left - margin.right) / 2)
+          .attr("dy", "-.71em")
+          .attr("class", "bubble")
+          .style("text-anchor", "middle")
+          .style("display", "none")
+          .text("");
+
+        svg.append("rect")
+          .attr("class", "overlay")
+          .attr("width", width)
+          .attr("height", height)
+        .on("mouseover", function() {
+          focus.style("display", null);
+          focush.style("display", null);
+          focusv.style("display", null);
+          bubble.style("display", null);
+        })
+        .on("mouseout", function() {
+          focus.style("display", "none");
+          focush.style("display", "none");
+          focusv.style("display", "none");
+          bubble.style("display", "none");
+        })
+        .on("mousemove", mousemove);
+      }
+
+      var bisectDistance = d3.bisector(function(d) { return d.d; }).left;
+      var bisectDate = d3.bisector(function(d) { return d.elapsed; }).left;
+      var formatDistance = d3.format(".2f");
+      var formatDate = d3.time.format("%H:%M");
+      var formatMinutes = d3.format("02d");
+
+      function mousemove() {
+        var bisect = (mode === "distance") ? bisectDistance : bisectDate;
+        var x0 = (mode === "distance") ? x1.invert(d3.mouse(this)[0])
+                                     : x2.invert(d3.mouse(this)[0]);
+        var i = bisect(data, x0, 1, data.length - 1);
+        var d0 = data[i - 1];
+        var d1 = data[i];
+        var d = (mode === "distance") ? (x0 - d0.d > d1.d - x0 ? d1 : d0)
+                                      : (x0 - d0.elapsed > d1.elapsed - x0 ? d1 : d0);
+
+        var dy = y(d.ele);
+        var dx = (mode === "distance") ? x1(d.d) : x2(d.elapsed);
+
+        focus.attr("transform", "translate(" + dx + "," + dy + ")");
+        focush.attr("transform", "translate(0," + dy + ")");
+        focusv.attr("transform", "translate(" + dx + ",0)");
+
+        if (time_available) {
+          var elapsed = d.elapsed/1000;
+          bubble.text(i18n.elevation + " " + d.ele + i18n.meters + " / " + i18n.distance + " " + formatDistance(d.d) + 
+            i18n.kilometers + " / " + i18n.time + " " + formatDate(d.date) + " / " + i18n.duration + " " +
+            ~~(elapsed/3600) + ":" +formatMinutes(~~(elapsed%3600/60)));
         } else {
-          line.transition()
-            .duration(1000)
-            .attr("d", nLine);
+          bubble.text(i18n.elevation + " " + d.ele + i18n.meters + " / " + i18n.distance + " " +
+            formatDistance(d.d) + i18n.kilometers);
         }
+      }
 
-        d3.select(".x.axis")
-          .call(axis)
-        d3.select(".x.axis.legend")
-          .text(legend);
-      });
-    }
-  });
+      // switch xAxis between distance and time
+      if (time_available) {
+        d3.selectAll("input[name=profile_mode]").on("change", function() {
+          var nLine = (this.value === "distance") ? dLine : tLine;
+          var axis = (this.value === "distance") ? x1Axis : x2Axis;
+          var legend = (this.value === "distance") ? i18n.x1Legend : i18n.x2Legend;
+          mode = this.value;
+
+          if (mobile) {
+            line.attr("d", nLine);
+          } else {
+            line.transition()
+              .duration(1000)
+              .attr("d", nLine);
+          }
+
+          d3.select(".x.axis")
+            .call(axis)
+          d3.select(".x.axis.legend")
+            .text(legend);
+        });
+      }
+    });
+
 })()
