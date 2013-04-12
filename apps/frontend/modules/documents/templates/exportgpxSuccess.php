7e59bb1a331dd8288e3c9c6cd1eb7a32e2a6cfad
Lionel Besson <lionel.besson@gmail.com>
2013-04-12 16:51:39 +0000

    use routes when exporting route tracks as gpx
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5655 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/documents/templates/exportgpxSuccess.php
+++ apps/frontend/modules/documents/templates/exportgpxSuccess.php
@@ -1,4 +1,8 @@
-<?php 
+<?php
+// TODO
+// for now we only support single lines, but in the future we might support multilines
+// in that case each line would correspond to one <rte> (for routes)
+//                                         to one <trkseg> in the only <trk> (for outings)
 $points = $sf_data->getRaw('points');
 $nbpts = count($points);
 $id = $sf_params->get('id');
@@ -10,40 +14,48 @@ $id = $sf_params->get('id');
       <text>Camptocamp.org</text>
     </link>
   </metadata>
-<?php if ($nbpts > 1): ?>
-  <trk>
+<?php
+if ($nbpts > 1): /////// use track or route, depending on module //////// 
+switch ($sf_context->getModuleName())
+{
+    case 'routes':
+        $line_tag = 'rte';
+        $point_tag = 'rtept';
+        $inner_tag = null;
+        break;
+    case 'outings':
+    default:
+        $line_tag = 'trk';
+        $point_tag = 'trkpt';
+        $inner_tag = 'trkseg';
+}
+?>
+  <<?php echo $line_tag ?>>
     <name><?php echo $sf_data->getRaw('name') ?></name>
-    <trkseg>
+    <?php if (isset($inner_tag)) echo "<$inner_tag>\n"; ?>
 <?php 
-$minlat = 90;  $minlon = 180;  $maxlat = -90; $maxlon = -180;
 foreach ($points as $point): ?>
 <?php $_point = explode(' ', trim($point)); 
 $nb_fields = count($_point); 
 $lon = number_format($_point[0], 6, '.', '');
 $lat = number_format($_point[1], 6, '.', '');
-$minlon = min($minlon, $lon);
-$minlat = min($minlat, $lat);
-$maxlon = max($maxlon, $lon);
-$maxlat = max($maxlat, $lat);
 ?>
-      <trkpt lat="<?php echo $lat ?>" lon="<?php echo $lon ?>"> 
+      <<?php echo $point_tag ?> lat="<?php echo $lat ?>" lon="<?php echo $lon ?>"> 
 <?php if ($nb_fields > 2): ?>
         <ele><?php echo (abs($_point[2])<1) ? '0' : round($_point[2]) ?></ele>
 <?php endif ?>
 <?php if ($nb_fields == 4 && $_point[3]!=0): ?>
         <time><?php echo date('c', $_point[3]) ?></time>
 <?php endif ?>
-      </trkpt>
+      </<?php echo $point_tag ?>>
 <?php endforeach ?>
-    </trkseg>
-  </trk>
-  <?php //echo '<bounds minlat="'.$minlat.'" minlon="'.$minlon.'" maxlat="'.$maxlat.'" maxlon="'.$maxlon.'"/>'; ?>
-<?php elseif ($nbpts = 1): ?>
+    <?php if (isset($inner_tag)) echo "</$inner_tag>\n"; ?>
+  </<?php echo $line_tag ?>>
+<?php elseif ($nbpts = 1): /////// use a waypoint ////////// ?>
 <?php $_point = explode(' ', trim($points[0]));
         $lon = number_format($_point[0], 6, '.', '');
         $lat = number_format($_point[1], 6, '.', '');
 ?>
-  <?php //echo '<bounds minlat="'.$lat.'" minlon="'.$lon.'" maxlat="'.$lat.'" maxlon="'.$lon.'"/>'; ?>
   <wpt lat="<?php echo $lat ?>" lon="<?php echo $lon ?>">
 <?php if (count($_point) > 2): ?>
     <ele><?php echo (abs($_point[2])<1) ? '0' : round($_point[2]) ?></ele>
