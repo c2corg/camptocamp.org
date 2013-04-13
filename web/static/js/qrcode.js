f91c97e1974490ca801c88ba939804266f936d95
Lionel Besson <lionel.besson@gmail.com>
2013-04-13 08:57:29 +0000

    indent
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5658 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- web/static/js/qrcode.js
+++ web/static/js/qrcode.js
@@ -1,14 +1,14 @@
 (function() {
 
-"use strict";
+  "use strict";
 
-Event.observe(window, 'load', function()
-{
-  if ($$('.license_box')) {
-    $$('.license_box').first().insert({ top: '<img class="qrcode printonly" src="https://chart.googleapis.com/chart?chs=70x70&amp;cht=qr&amp;choe=UTF-8&amp;chl='
-                                             + encodeURIComponent($$('.mobile_link').first().href + /\d+/.exec(window.location.href))
-                                             + '"><br />' });
-  }
-});
+  Event.observe(window, 'load', function()
+  {
+    if ($$('.license_box')) {
+      $$('.license_box').first().insert({ top: '<img class="qrcode printonly" src="https://chart.googleapis.com/chart?chs=70x70&amp;cht=qr&amp;choe=UTF-8&amp;chl='
+                                               + encodeURIComponent($$('.mobile_link').first().href + /\d+/.exec(window.location.href))
+                                               + '"><br />' });
+    }
+  });
 
 })();
