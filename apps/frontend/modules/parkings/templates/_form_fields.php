31b7db39b861a04bad539df565e12a7df9070c27
Lionel Besson <lionel.besson@gmail.com>
2013-04-13 09:03:58 +0000

    do not pollute global namespace
    git-svn-id: https://dev.camptocamp.org/svn/c2corg@5659 5b3c568a-86f9-4a59-a47a-7be284f604b4

--- apps/frontend/modules/parkings/templates/_form_fields.php
+++ apps/frontend/modules/parkings/templates/_form_fields.php
@@ -21,7 +21,7 @@ echo form_section_title('Information', 'form_info', 'preview_info');
 echo object_group_tag($document, 'elevation', array('suffix' => 'meters', 'class' => 'short_input'));
 echo object_group_tag($document, 'lowest_elevation', array('suffix' => 'meters', 'class' => 'short_input'));
 include_partial('documents/oam_coords', array('document' => $document));
-echo object_group_dropdown_tag($document, 'public_transportation_rating', 'app_parkings_public_transportation_ratings', array('onchange' => 'hide_parkings_unrelated_fields()'));
+echo object_group_dropdown_tag($document, 'public_transportation_rating', 'app_parkings_public_transportation_ratings', array('onchange' => 'C2C.hide_parkings_unrelated_fields()'));
 ?>
 <div id="tp_types">
 <?php
@@ -30,7 +30,7 @@ echo object_group_dropdown_tag($document, 'public_transportation_types', 'app_pa
 ?>
 </div>
 <?php
-echo object_group_dropdown_tag($document, 'snow_clearance_rating', 'mod_parkings_snow_clearance_ratings_list', array('onchange' => 'hide_parkings_unrelated_fields()'));
+echo object_group_dropdown_tag($document, 'snow_clearance_rating', 'mod_parkings_snow_clearance_ratings_list', array('onchange' => 'C2C.hide_parkings_unrelated_fields()'));
 
 echo form_section_title('Description', 'form_desc', 'preview_desc');
 
--- web/static/js/parkings.js
+++ web/static/js/parkings.js
@@ -1,26 +1,23 @@
-function hide_parkings_unrelated_fields()
-{
+(function(C2C) {
+
+  C2C.hide_parkings_unrelated_fields = function() {
     var value = $('public_transportation_rating').options[$('public_transportation_rating').selectedIndex].value;
-    if(value != '3' && value != '0')
-    {
-        $('tp_types').show();
-        $('tp_desc').show();
-    }
-    else
-    {
-        $('tp_types').hide();
-        $('tp_desc').hide();
+    if(value != '3' && value != '0') {
+      $('tp_types').show();
+      $('tp_desc').show();
+    } else {
+      $('tp_types').hide();
+      $('tp_desc').hide();
     }
     
     value = $('snow_clearance_rating').options[$('snow_clearance_rating').selectedIndex].value;
-    if(value != '4' && value != '0')
-    {
-        $('snow_desc').show();
+    if(value != '4' && value != '0') {
+      $('snow_desc').show();
+    } else{
+      $('snow_desc').hide();
     }
-    else
-    {
-        $('snow_desc').hide();
-    }
-}
+  }
 
-document.observe('dom:loaded', hide_parkings_unrelated_fields);
+  document.observe('dom:loaded', C2C.hide_parkings_unrelated_fields);
+
+})(window.C2C = window.C2C || {});
