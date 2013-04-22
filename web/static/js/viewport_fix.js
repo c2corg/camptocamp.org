a0cfb377851b3fc8f776b19546edce746a7230d7
lbesson <lionel.besson@gmail.com>
2013-04-22 17:07:33 +0000

    use jquery instead of prototype

--- web/static/js/viewport_fix.js
+++ web/static/js/viewport_fix.js
@@ -3,12 +3,13 @@
 // http://www.blog.highub.com/mobile-2/a-fix-for-iphone-viewport-scale-bug/
 // https://github.com/h5bp/mobile-boilerplate/blob/master/js/helper.js
 
-(function(){
-if (/iPhone|iPad|iPod/.test(navigator.userAgent) && !/Opera Mini/.test(navigator.userAgent)) {
-  var viewport = $$('meta[name="viewport"]')[0];
-  viewport.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-  document.addEventListener("gesturestart", function () {
-    viewport.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-  }, false);
-}
-})();
+(function($){
+  var ua = navigator.userAgent;
+  if (/iPhone|iPad|iPod/.test(ua) && !/Opera Mini/.test(ua)) {
+    var viewport = $('meta[name="viewport"]').first();
+    viewport.attr("content", "width=device-width, minimum-scale=1.0, maximum-scale=1.0");
+    document.on("gesturestart", function () {
+      viewport.attr("content", "width=device-width, minimum-scale=0.25, maximum-scale=1.6");
+    }, false);
+  }
+})(jQuery);
