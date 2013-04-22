58351729fbe7c919e5854dd64fbeb9ba0eef45d9
lbesson <lionel.besson@gmail.com>
2013-04-22 17:27:19 +0000

    use jquery instead of prototype

--- web/static/js/qrcode.js
+++ web/static/js/qrcode.js
@@ -1,6 +1,7 @@
 (function($) {
 
-  $(document).ready(function() {
+  // add the qrcode to the document once dom is loaded
+  $(function() {
     $('.license_box').first().prepend('<img class="qrcode printonly" ' +
         'src="https://chart.googleapis.com/chart?chs=70x70&amp;cht=qr&amp;choe=UTF-8&amp;chl=' +
         encodeURIComponent($('.mobile_link').first().href + /\d+/.exec(window.location.href)) + '"><br>');
