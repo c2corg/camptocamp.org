54f32bf834af91eca10e123b89ff2f2d858eb780
lbesson <lionel.besson@gmail.com>
2013-04-24 21:27:03 +0200

    use jquery instead of prototype

--- web/static/js/filter.js
+++ web/static/js/filter.js
@@ -1,210 +1,158 @@
-(function(C2C) {
+(function(C2C, $) {
 
-"use strict";
+  var activities = [];
 
-var activities = [];
+  // for some search inputs like date selection, we hide some parts
+  // depending on the first field value
+  // e.g. only display one input for selecting summits higher than 4000m,
+  // two inputs for selecting summits between 2000m and 3000m
+  C2C.update_on_select_change = function(field, optionIndex) {
+    var index = $('#' + field + '_sel').val();
 
-C2C.update_on_select_change = function(field, optionIndex)
-{
-    var index = $(field + '_sel').value;
-    if (index == '0' || index === ' ' || index == '-' || index >= 4)
-    {
-        $(field + '_span1').hide();
-        $(field + '_span2').hide();
-        if (optionIndex >= 4)
-        {
-            if (index == 4)
-            {
-                $(field + '_span3').show();
-            }
-            else
-            {
-                $(field + '_span3').hide();
-            }
+    if (index == '0' || index === ' ' || index == '-' || index >= 4) {
+
+      $('#' + field + '_span1').hide();
+      $('#' + field + '_span2').hide();
+
+      if (optionIndex >= 4) {
+        if (index == 4) {
+          $('#' + field + '_span3').show();
+        } else {
+          $('#' + field + '_span3').hide();
         }
+      }
+
+    } else {
+
+      $('#' + field + '_span1').show();
+
+      if (index == '~' || index == 3) {
+        $('#' + field + '_span2').show();
+      } else {
+        $('#' + field + '_span2').hide();
+      }
+
+      if (optionIndex >= 4) {
+        $('#' + field + '_span3').hide();
+      }
+
     }
-    else
-    {
-        $(field + '_span1').show();
-        if (index == '~' || index == 3)
-        {
-            $(field + '_span2').show();
-        }
-        else
-        {
-            $(field + '_span2').hide();
-        }
-        if (optionIndex >= 4)
-        {
-            $(field + '_span3').hide();
-        }
-    }
-};
+  };
 
-function initialize_select()
-{
-    var re = new RegExp('_sel$', 'i');
-    var sel_list = document.getElementsByTagName('SELECT');
-    for (var i = 0; i < sel_list.length; ++i)
-    {
-        if (sel_list[i].id.search(re) != -1)
-        {
-            sel_list[i].onchange(true);
-        }
-    }
-}
 
-// see hide_unrelated_fields() in routes.js
-C2C.hide_unrelated_filter_fields = function(current_activity)
-{
-    if (activities.indexOf(current_activity) != -1)
-    {
-         // if activity is already selected, unselect it
-         activities = activities.without(current_activity);
-    }
-    else
-    {
-         // else add it to the selection
-         activities.push(current_activity);
+  // see hide_unrelated_fields() in routes.js
+  // hide some fields depending onf the activity selected
+  C2C.hide_unrelated_filter_fields = function(current_activity) {
+
+    if (activities.indexOf(current_activity) != -1) {
+       // if activity is already selected, unselect it
+       activities = activities.without(current_activity);
+    } else {
+       // else add it to the selection
+       activities.push(current_activity);
     }
 
     var show_flags = [
-        'ski',
-        'ski_snow_mountain',
-        'ski_snow_mountain_rock',
-        'ski_snow_mountain_rock_ice',
-        'snow_ice',
-        'snow_mountain_rock_ice',
-        'rock_mountain',
-        'hiking',
-        'snowshoeing'
+      'ski',
+      'ski_snow_mountain',
+      'ski_snow_mountain_rock',
+      'ski_snow_mountain_rock_ice',
+      'snow_ice',
+      'snow_mountain_rock_ice',
+      'rock_mountain',
+      'hiking',
+      'snowshoeing'
     ];
-    var show_ski, show_ski_snow_mountain, show_ski_snow_mountain_rock, show_ski_snow_mountain_rock_ice,
-        show_snow_ice, show_snow_mountain_rock_ice, show_rock_mountain, show_hiking, show_snowshoeing;
 
-    show_flags.each(function(flag)
-    {
-        eval('show_' + flag + ' = false');
-    });
-    var show_snow = false;
+    var show = {};
+    for (var i = 0; i < show_flags.length; i++) {
+      show[show_flags[i]] = false;
+    }
+    show.snow = false;
 
-    activities.each(function(activity)
-    {
-        switch (activity)
-        {
-            case 1: // skitouring
-                show_ski = true;
-                show_ski_snow_mountain = true;
-                show_ski_snow_mountain_rock = true;
-                show_ski_snow_mountain_rock_ice = true;
-                break;
+    //activities.each(function(activity)
+    $.each(activities, function(i, activity) {
+      switch (activity) {
+        case 1: // skitouring
+          show.ski = true;
+          show.ski_snow_mountain = true;
+          show.ski_snow_mountain_rock = true;
+          show.ski_snow_mountain_rock_ice = true;
+          break;
 
-            case 2: // snow_ice_mixed
-                show_ski_snow_mountain = true;
-                show_ski_snow_mountain_rock = true;
-                show_ski_snow_mountain_rock_ice = true;
-                show_snow = true;
-                show_snow_ice = true;
-                show_snow_mountain_rock_ice = true;
-                break;
+        case 2: // snow_ice_mixed
+          show.ski_snow_mountain = true;
+          show.ski_snow_mountain_rock = true;
+          show.ski_snow_mountain_rock_ice = true;
+          show.snow = true;
+          show.snow_ice = true;
+          show.snow_mountain_rock_ice = true;
+          break;
 
-            case 3: // mountain_climbing
-                show_ski_snow_mountain = true;
-                show_ski_snow_mountain_rock = true;
-                show_ski_snow_mountain_rock_ice = true;
-                show_snow_mountain_rock_ice = true;
-                show_rock_mountain = true;
-                break;
+        case 3: // mountain_climbing
+          show.ski_snow_mountain = true;
+          show.ski_snow_mountain_rock = true;
+          show.ski_snow_mountain_rock_ice = true;
+          show.snow_mountain_rock_ice = true;
+          show.rock_mountain = true;
+          break;
 
-            case 4: // rock_climbing
-                show_ski_snow_mountain_rock = true;
-                show_ski_snow_mountain_rock_ice = true;
-                show_snow_mountain_rock_ice = true;
-                show_rock_mountain = true;
-                break;
+        case 4: // rock_climbing
+          show.ski_snow_mountain_rock = true;
+          show.ski_snow_mountain_rock_ice = true;
+          show.snow_mountain_rock_ice = true;
+          show.rock_mountain = true;
+          break;
 
-            case 5: // ice_climbing
-                show_ski_snow_mountain_rock_ice = true;
-                show_snow_ice = true;
-                show_snow_mountain_rock_ice = true;
-                break;
+        case 5: // ice_climbing
+          show.ski_snow_mountain_rock_ice = true;
+          show.snow_ice = true;
+          show.snow_mountain_rock_ice = true;
+          break;
 
-            case 6: // hiking
-                show_hiking = true;
-                break;
+        case 6: // hiking
+          show.hiking = true;
+          break;
 
-            case 7:
-                show_snowshoeing = true;
+        case 7: // paragliding
+          show.snowshoeing = true;
         }
     });
 
-    show_flags.each(function(flag)
-    {
-        var div_id = flag + '_fields';
-        if (eval('show_' + flag))
-        {
-            $(div_id).show();
-        }
-        else
-        {
-            $(div_id).hide();
-        }
+    // hide / show sections
+    $.each(show_flags, function(i, flag) {
+      $('#' + flag + '_fields').toggle(show[flag]);
     });
-    
-    if (document.getElementById('conf') && show_ski_snow_mountain_rock)
-    {
-        var select_size = 6;
-        if (show_snow)
-        {
-            $('conf').options[4].show();
-            $('conf').options[5].show();
-        }
-        else
-        {
-            $('conf').options[4].hide();
-            $('conf').options[5].hide();
-            select_size -= 2;
-        }
-        
-        $('conf').size = select_size;
-    }
-};
 
-function initialize_activities()
-{
-    var act_form = document.getElementById('actform');
-    var act_list = [];
-    if(act_form)
+    // route configuration
+    var conf = document.getElementById('conf');
+    if (conf && show.ski_snow_mountain_rock)
     {
-        act_list = act_form.getElementsByTagName("INPUT");
-        for (var i = 0; i < act_list.length; ++i)
-        {
-            if(act_list[i].checked)
-            {
-                act_list[i].onclick(true);
-            }
-        }
-    }
-}
+      var select_size = 6;
+      if (show.snow) {
+        $('#conf option:eq(4)').show();
+        $('#conf option:eq(5)').show();
+      } else {
+        $('#conf option:eq(4)').hide();
+        $('#conf option:eq(5)').hide();
+        select_size -= 2;
+      }
 
-document.observe('dom:loaded', function()
-{
-    initialize_activities();
-    initialize_select();
-});
-
-C2C.changeSelectSize = function(select_id, up_down)
-{
-    var height = $(select_id).offsetHeight;
-    if(up_down)
-    {
-        height += 150; 
+      conf.size = select_size;
     }
-    else
-    {
-        height = Math.max(100, height - 150);
-    }
-    $(select_id).style.height = height + "px";
-};
+  };
 
-})(window.C2C = window.C2C || {});
+  C2C.changeSelectSize = function(id, up_down) {
+    var select = $('#' + id);
+    var height = select.height();
+
+    if (up_down) {
+      height += 150;
+    } else {
+      height = Math.max(100, height - 150);
+    }
+
+    select.height(height);
+  };
+
+})(window.C2C = window.C2C || {}, jQuery);
