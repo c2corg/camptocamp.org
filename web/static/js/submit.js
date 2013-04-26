738f1c8e313e360dd7cd7ea79c2b0c479cde6998
lbesson <lionel.besson@gmail.com>
2013-04-26 15:51:40 +0000

    use jquery instead of prototype

--- web/static/js/submit.js
+++ web/static/js/submit.js
@@ -1,156 +1,79 @@
-(function(C2C) {
-
-  "use strict";
+(function(C2C, $) {
 
   C2C.submitonce = function(aform) {
     C2C.switchFormButtonsStatus(aform, true);
   };
 
-  C2C.switchFormButtonsStatus = function(aform, disable) {
-    if (document.all || document.getElementById){
-      var tmp_dom_obj;
-      for (var i=0; i < aform.length; i++){        
-        tmp_dom_obj = aform.elements[i];
-        if ((tmp_dom_obj.type.toLowerCase() == "submit") || (tmp_dom_obj.type.toLowerCase() == "button"))
-            tmp_dom_obj.disabled = disable;
-      }
-    }
-  }
-
-  C2C.getWizardRouteRatings = function(div_id) {
-    new Ajax.Updater(
-      div_id + '_descr', '/routes/getratings', {
-        asynchronous:true,
-        evalScripts:false,
-        onComplete: function(request, json) { Element.hide("indicator"); },
-        onFailure: function(request, json) { Element.hide("wizard_" + div_id + "_descr"); },
-        onLoading: function(request, json) { Element.show("indicator"); },
-        onSuccess: function(request, json){ Element.show("wizard_" + div_id + "_descr"); },
-        parameters:"id=" + $(div_id).value
-      });
+  C2C.switchFormButtonsStatus = function(form, disable) {
+    $(form).find('input[type=submit], input[type=button]').attr('disabled', 'disabled');
   };
 
+  // outings wizard: retrieve route ratings
+  C2C.getWizardRouteRatings = function(div_id) {
+    var indicator = $('#indicator');
+
+    indicator.show();
+    
+    $.get('/routes/getratings', {
+      'id': $('#' + div_id).val()
+    }).always(function() {
+      indicator.hide();
+    }).done(function(data) {
+      $('#' + div_id + '_descr').html(data);
+      $('#wizard_' + div_id + '_descr').show();
+    }).fail(function() {
+      $('#wizard_' + div_id + '_descr').hide();
+    });
+  };
+
+  // unlink a document
   C2C.remLink = function(link_type, main_id, linked_id, main_doc, strict) {
-    if (confirm(confirm_msg))
-    {
+
+    if (confirm(confirm_msg)) {
       var type_linked_id = link_type + '_';
       if (main_doc) {
         type_linked_id = type_linked_id + linked_id;
       } else {
         type_linked_id = type_linked_id + main_id;
       }
-      new Ajax.Updater({
-          success: type_linked_id,
-          failure: 'ajax_feedback_failure'
-        },
-        '/documents/removeAssociation/main_' + link_type + '_id/' + main_id + '/linked_id/' + linked_id + '/type/' + link_type + '/strict/' + strict, {
-          asynchronous: true,
-          evalScripts: false,
-          method: 'post',
-          onComplete: function(request, json) {
-            Element.hide('indicator');
-            setTimeout('C2C.emptyFeedback("ajax_feedback_failure")', 4000);
-          },
-          onFailure: function(request, json) {
-            Element.show('ajax_feedback_failure');
-          },
-          onLoading: function(request, json) {
-            Element.hide('del_' + type_linked_id);
-            Element.show('indicator');
-          },
-          onSuccess: function(request, json) {
-            $$('#'+type_linked_id+', .'+type_linked_id).each(Element.hide);
-          }
-        });
+
+      var indicator = $('#indicator');
+
+      indicator.show();
+      $('#del_' + type_linked_id).hide();
+
+      $.post('/documents/removeAssociation/main_' + link_type + '_id/' + main_id +
+             '/linked_id/' + linked_id + '/type/' + link_type + '/strict/' + strict
+      ).always(function() {
+        indicator.hide();
+        setTimeout(function() {
+          C2C.emptyFeedback('ajax_feedback_failure');
+        }, 4000);
+      }).fail(function(data) {
+        $('#ajax_feedback_failure').html(data).show();
+      }).done(function(data) {
+        $('#' + type_linked_id).html(data);
+        $('#' + type_linked_id + ', .' + type_linked_id).hide();
+      });
     }
+
     return false;
   };
 
-  function showFieldDefault(field, enable) {
-    if (field_default[field][2]) {
-      var field_id = field_default[field][0];
-      if (enable) {
-        $(field_id).value = field_default[field][1];
-        $(field_id).style.color = 'gray';
-      } else {
-        $(field_id).value = '';
-        $(field_id).style.color = 'black';
-      }
-    }
-  }
 
-  C2C.showAllFieldDefault = function(enable) {
-    if (typeof(field_default) != 'undefined') {
-      for (var i=0; i < field_default.length; i++) {
-        showFieldDefault(i, enable);
-      }
-    }
-  };
+  // bbcode toolbar, initiated once dom loaded
+  $(function() {
+    var bbcode_toolbars = $('.bbcodetoolcontainer');
+    var textareas = bbcode_toolbars.next();
 
-  function initFieldDefault() {
-    if (typeof(field_default)!='undefined' && typeof(ga_done)!='undefined') {
-      var field_id;
-      for (var i=0; i < field_default.length; i++) {
-        field_id = field_default[i][0];
-        if ($(field_id).value == '') {
-          field_default[i][2] = true;
-          showFieldDefault(i, true);
-        } else {
-          field_default[i][2] = false;
-        }
-      }
-    }
-  }
+    // hide all bbcode toolbars
+    bbcode_toolbars.attr('visibility', 'hidden');
 
-  function hideFieldDefault(field) {
-    if (field_default[field][2]) {
-      showFieldDefault(field, false);
-      field_default[field][2] = false;
-    }
-  }
-
-  function hideAllFieldDefault() {
-    if (typeof(field_default)!='undefined') {
-      for (var i=0; i < field_default.length; i++) {
-        hideFieldDefault(i);
-      }
-    }
-  }
-
-  var bbcode_toolbar;
-  function initBBcode() {
-    bbcode_toolbar = $$('.bbcodetoolcontainer');
-    var textbox_list = Array();
-    if (bbcode_toolbar.length > 0) {
-      bbcode_toolbar.each(function(b) {
-        b.setStyle({'visibility': 'hidden'});
-        textbox_list.push($w(b.className).last());
-      });
-        
-      textbox_list.each(function(t) {
-        $(t).observe('focus', showBBcode);
-      });
-    }
-  }
-
-  function showBBcode() {
-    var field_id = this.identify();
-    bbcode_toolbar.each(function(b)
-    {
-      if ($w(b.className).last() == field_id) {
-        b.setStyle({'visibility': 'visible'});
-      } else {
-        b.setStyle({'visibility': 'hidden'});
-      }
+    bbcode_toolbars.next().focus(function() {
+      $(this).prev().attr('visibility', 'visible');
+    }).blur(function() {
+      $(this).prev().attr('visibility', 'hidden');
     });
-  }
-
-  document.observe('dom:loaded', function() {
-    initBBcode();
-    initFieldDefault();
-    if ($('editform')) {
-      Event.observe('editform', 'submit', hideAllFieldDefault);
-    }
   });
 
-})(window.C2C = window.C2C || {});
+})(window.C2C = window.C2C || {}, jQuery);
