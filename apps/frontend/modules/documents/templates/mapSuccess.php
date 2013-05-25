eb9ef0232a5e0ee967cdd0eb025b8cad90b83a46
lbesson <lionel.besson@gmail.com>
2013-05-25 14:14:25 +0000

    carto css should not be combined with normal css

--- apps/frontend/modules/documents/templates/mapSuccess.php
+++ apps/frontend/modules/documents/templates/mapSuccess.php
@@ -8,11 +8,11 @@ if ($debug)
 }
 else
 {
-    use_stylesheet('/static/css/carto_base.css', 'last');
+    use_stylesheet('/static/css/carto_base.css', 'custom');
     use_javascript('/static/js/carto/build/app.js', 'maps');
 }
-use_stylesheet('/static/css/carto.css', 'last');
-use_stylesheet('/static/css/viewer.css', 'last');
+use_stylesheet('/static/css/carto.css', 'custom');
+use_stylesheet('/static/css/viewer.css', 'custom');
 use_javascript("/static/js/carto/build/lang-$lang.js", 'maps');
 use_javascript('/static/js/carto/viewer.js', 'maps');
 ?>
