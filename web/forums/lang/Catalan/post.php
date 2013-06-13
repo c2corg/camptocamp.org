eb01ca42f3bbd5265ea2480316278392b5a834dc
lbesson <lionel.besson@gmail.com>
2013-06-13 17:15:06 +0000

    forum i18n catalan (on behalf of Cesc Grau)

--- web/forums/lang/Catalan/post.php
+++ web/forums/lang/Catalan/post.php
@@ -8,7 +8,7 @@ $lang_post = array(
 'Too long subject'		=>	'Els assumptes no poden contenir més de 70 caràcters.',
 'No message'			=>	'Heu d\'introduir un missatge.',
 'Too long message'		=>	'Els missatges no poden contenir més de 65535 caràcters (64 KB).',
-'New posts error'		=>	'Au moins une nouvelle réponse a été postée depuis votre dernière lecture de la discussion. Elle est visible sous le formulaire.',
+'New posts error'		=>	'Hi ha almenys una nova resposta penjada d\'ençà del darrer cop que heu llegit la discussió. La podeu veure sota el formulari.',
 
 // Posting
 'Post errors'			=>	'Errors del missatge',
@@ -18,16 +18,16 @@ $lang_post = array(
 'Post redirect'			=>	'Missatge introduït. Redirigint &hellip;',
 'Post a reply'			=>	'Publica resposta',
 'Post new topic'		=>	'Nou tema',
-'Hide smilies'			=>	'No mostrar els smilies com a icones',
+'Hide smilies'			=>	'No mostris els smilies com a icones',
 'Subscribe'				=>	'Subscriure\'m a aquest tema',
 'Topic review'			=>	'Revisa tema (els més nous primer)',
-'Flood start'			=>	'Al menys',
-'flood end'				=>	'segons han de transcórrer entre missatges. Per favor espereu un poc i proveu-ho de nou.',
+'Flood start'			=>	'Almenys',
+'flood end'				=>	'segons han de transcórrer entre missatges. Si us plau, espereu-vos una mica i proveu-ho de nou.',
 'Preview'				=>	'Previsualitza',	// submit button to preview message
 
 // Edit post
 'Edit post legend'		=>	'Edita el missatge i envia els canvis',
-'Silent edit'			=>	'Edició silenciosa (no mostra "Editat per ..." al veure el tema)',
+'Silent edit'			=>	'Edició silenciosa (no mostra "Editat per ..." en veure el tema)',
 'Edit post'				=>	'Edita missatge',
 'Edit redirect'			=>	'Missatge actualitzat. Redirigint &hellip;',
 
--- web/forums/lang/Catalan/search.php
+++ web/forums/lang/Catalan/search.php
@@ -5,17 +5,17 @@ $lang_search = array(
 
 // The search form
 'User search'				=>	'Cerca d\'usuaris',
-'No search permission'		=>	'No teniu permís per a usar la opció de cerca.',
+'No search permission'		=>	'No teniu permís per fer servir l\'opció de cerca.',
 'Search'					=>	'Cerca',
 'Search criteria legend'	=>	'Introduïu el criteri de cerca',
-'Search info'				=>	'Per a buscar per paraula clau, introduïu un terme o termes que buscar. Separeu els termes amb espais. Useu AND (i), OR (o) i NOT (no) per a detallar la cerca. Per a buscar per autor introduïu el nom d\'usuari de l\'autor dels missatges que voleu buscar. Useu el caràcter asterisc * per a coincidències parcials.',
+'Search info'				=>	'Per cercar per paraula clau, introduïu el terme o termes a buscar. Separeu els termes amb espais. Useu AND (i), OR (o) i NOT (no) per a detallar la cerca. Per cercar per autor introduïu el nom d\'usuari de l\'autor dels missatges que voleu buscar. Useu el caràcter asterisc * per a coincidències parcials.',
 'Keyword search'			=>	'Cerca per paraula clau',
 'Author search'				=>	'Cerca per autor',
 'Search in legend'			=>	'Seleccioneu on voleu buscar',
-'Search in info'			=>	'Trieu el fòrum on voleu cercar i si voleu fer-ho als assumptes dels missatges, al text dels missatges o en ambdós.',
+'Search in info'			=>	'Trieu el fòrum on voleu cercar, i si voleu fer-ho als assumptes dels missatges, al text dels missatges o en ambdós.',
 'Forum search'				=>	'Fòrum',
 'All forums'				=>	'Tots els fòrums',
-'Search in'					=>	'Cercar al',
+'Search in'					=>	'Cerca al',
 'Message and subject'		=>	'Text i assumpte del missatge',
 'Message only'				=>	'Només al text del missatge',
 'Topic only'				=>	'Només a l\'assumpte del missatge',
@@ -32,7 +32,7 @@ $lang_search = array(
 'Show as'					=>	'Mostra els resultats com',
 'Show as topics'			=>	'Temes',
 'Show as posts'				=>	'Missatges',
-'Google Search'             =>  'Google Search',
+'Google Search'             =>  'Cerca amb Google',
 
 // Results
 'Search results'			=>	'Resultats de cerca',
@@ -40,16 +40,16 @@ $lang_search = array(
 'Unanswered topics'			=>	'Temes sense resposta',
 'Posts from user'			=>	'Missatges del ',
 'Topics from user'			=>	'Temes del ',
-'Subscriptions topics'		=>	'Meus temes subscrits',
+'Subscriptions topics'		=>	'Els meus temes subscrits',
 'New posts'					=>	'Missatges nous des de l\'última visita',
 'News'                      =>  'News',
-'No terms'					=>	'Heu d\'introduir almenys una paraula clau i/o autor per a cercar.',
+'No terms'					=>	'Heu d\'introduir almenys una paraula clau i/o autor per cercar.',
 'No hits'					=>	'No s\'han trobat coincidències.',
-'No user posts'				=>	'No hi han missatges d\'aquest usuari en aquest fòrum.',
+'No user posts'				=>	'No hi ha missatges d\'aquest usuari en aquest fòrum.',
 'No subscriptions'			=>	'Actualment no esteu subscrit a cap tema.',
-'No new posts'				=>	'No hi han temes amb missatges nous des de l\'última visita.',
+'No new posts'				=>	'No hi ha temes amb missatges nous des de l\'última visita.',
 'No recent posts'			=>	'No s\'han introduït missatges nous durant les últimes 24 hores.',
-'No unanswered'				=>	'No hi han missatges sense resposta en aquest fòrum.',
-'Go to post'				=>	'Anar al missatge'
+'No unanswered'				=>	'No hi ha missatges sense resposta en aquest fòrum.',
+'Go to post'				=>	'Vés al missatge'
 
 );
