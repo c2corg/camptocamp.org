f7135275050f0396f3304238ca2b7f143ccb0228
lbesson <lionel.besson@gmail.com>
2013-06-15 11:03:30 +0000

    use jquery instead of prototype

--- apps/frontend/modules/images/templates/_temp_image_error.php
+++ apps/frontend/modules/images/templates/_temp_image_error.php
@@ -1,7 +1,7 @@
 <?php
 use_helper('MyForm', 'Javascript');
 echo link_to_function(picto_tag('action_cancel', __('close')),
-                      '$(this).up().hide();Modalbox.resizeToContent();',
+                      '$(this).up().remove();Modalbox.resizeToContent()',
                       array('style' => 'float:right;'));
 echo $image_name;
 echo global_form_errors_tag();
