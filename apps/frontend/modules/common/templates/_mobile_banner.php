153309740b887fa70c3ae22ef9cb96b0f390a02e
lbesson <lionel.besson@gmail.com>
2013-07-03 17:34:30 +0000

    use async adsense snippet

--- apps/frontend/modules/common/templates/_mobile_banner.php
+++ apps/frontend/modules/common/templates/_mobile_banner.php
@@ -1,24 +1,8 @@
 <div id="mobile_banner">
 <?php if (sfConfig::get('app_production') == 1): ?>
-<script type="text/javascript"><!--
- /* <![CDATA[ */
- window.googleAfmcRequest = {
-   client: 'ca-mb-pub-8662990478599655',
-   ad_type: 'text',
-   output: 'html',
-   channel: '',
-   format: '320x50_mb',
-   oe: 'utf8',
-   alt_color: 'FFFFFF',
-   color_border: 'FFFFFF',
-   color_bg: 'FFFFFF',
-   color_link: 'FF9933',
-   color_text: '000000',
-   color_url: '808080',
- };
- /* ]]> */
-//--></script>
-<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_afmc_ads.js"></script>
+<script async src="http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle" style="display:inline-block;width:320px;height:50px" data-ad-client="ca-pub-8662990478599655" data-ad-slot="3178153545"></ins>
+<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 <?php else: ?>
 <div style="text-align: center"><img src="http://fakeimg.pl/320x50/282828/eae0d0/?text=Publicit%C3%A9" /></div>
 <?php endif ?>
