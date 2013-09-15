ecd6f041fcd6902f9c89a3d3ac6c100ed151bbc0
lbesson <lionel.besson@gmail.com>
2013-09-15 16:32:03 +0200

    bad merge conflict resolution

--- lib/model/doctrine/Summit.class.php
+++ lib/model/doctrine/Summit.class.php
@@ -457,17 +457,8 @@ class Summit extends BaseSummit
     // sub summits are defined by the association order
     public static function getSubSummits($id)
     {
-<<<<<<< HEAD
-        $query = 'SELECT m.id, m.elevation '
-               . 'FROM summits m '
-               . 'WHERE m.id IN '
-               . '((SELECT a.main_id FROM app_documents_associations a WHERE a.linked_id = ? AND type = ?) '
-               . 'UNION (SELECT a.linked_id FROM app_documents_associations a WHERE a.main_id = ? AND type = ?)) '
-               . 'AND m.elevation <= ? '
-=======
         $query = 'SELECT m.id FROM summits m WHERE m.id IN '
                . '(SELECT a.linked_id FROM app_documents_associations a WHERE a.main_id = ? AND type = ?) '
->>>>>>> hierarchy-hell
                . 'ORDER BY m.id ASC';
 
         $results = sfDoctrine::connection()
