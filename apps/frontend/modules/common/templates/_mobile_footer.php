96cd24699dba752539f1823f30943a8199367c59
lbesson <lionel.besson@gmail.com>
2013-09-19 01:03:47 +0200

    sometimes referer is not set when updating document.location.href, try a workaround

--- apps/frontend/modules/common/templates/_mobile_footer.php
+++ apps/frontend/modules/common/templates/_mobile_footer.php
@@ -20,7 +20,10 @@ if ((bool)sfConfig::get('app_mobile_version_ads'))
     <div id="footer_cc">
         <div id="footer_cc_text">
             <p><?php echo link_to(__('Home'), '@homepage'), ' | ', link_to(__('web version of the site'), 'http://'.$classic_hostname,
-                                  array('onclick' => "document.location.href = document.location.href.replace('$mobile_hostname', '$classic_hostname'); return false;")) ?></p>
+                                  array('id' => 'www-link')) ?></p>
+            <script type="text/javascript">
+               document.getElementById('www-link').href = document.location.href.replace('<?php echo $mobile_hostname ?>', '<?php echo $classic_hostname ?>');
+            </script>
             <p>&copy; 1997-<?php echo date('Y') . ' ' . link_to('Camptocamp-Association', getMetaArticleRoute('association')) ?></p>
             <p><?php echo __('CNIL declaration #') ?>1175560 - <?php echo __('disclaimer notice') ?></p>
         </div>
