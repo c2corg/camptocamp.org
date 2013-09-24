03783279aef09eb8ea27e9f34bfddf4bedf0076c
lbesson <lionel.besson@gmail.com>
2013-09-24 23:11:57 +0200

    possibility to categorize parking as "cable car" type even if TC type is 'none' or unset

--- apps/frontend/modules/parkings/templates/_form_fields.php
+++ apps/frontend/modules/parkings/templates/_form_fields.php
@@ -25,12 +25,13 @@ echo object_group_dropdown_tag($document, 'public_transportation_rating', 'app_p
 // special handling for public_transportation_types. Cablecar (9) should be presented separately
 echo object_group_dropdown_tag($document, 'public_transportation_types', 'app_parkings_public_transportation_types',
                                array('multiple' => true, 'na' => array('cable_car' => 9)));
-echo start_group_tag(), label_tag('cable_car_access'), ' <span>',
-     checkbox_tag('public_transportation_types[]', 9, in_array(9, $document->getRaw('public_transportation_types')),
-                  array('id' => 'cable_car_access')), '</span>', end_group_tag();
 ?>
 </div>
 <?php
+echo start_group_tag(), label_tag('cable_car_access'), ' <span>',
+     checkbox_tag('public_transportation_types[]', 9, in_array(9, $document->getRaw('public_transportation_types')),
+                  array('id' => 'cable_car_access')), '</span>', end_group_tag();
+
 echo object_group_dropdown_tag($document, 'snow_clearance_rating', 'mod_parkings_snow_clearance_ratings_list', array('onchange' => 'C2C.hide_parkings_unrelated_fields()'));
 
 echo form_section_title('Description', 'form_desc', 'preview_desc');
