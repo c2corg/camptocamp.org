7d7594607ea2a05a9a54d5895102ff167f22380f
lbesson <lionel.besson@gmail.com>
2013-09-27 19:13:52 +0200

    pt types validation wrt pt rating

--- apps/frontend/lib/myPtTypesValidator.class.php
+++ apps/frontend/lib/myPtTypesValidator.class.php
@@ -1,35 +1,42 @@
 <?php
 class myPtTypesValidator extends sfValidator
 {
-  public function execute (&$value, &$error)
-  {
-    // check if we have a list
-    $array_choice = $this->getParameter('array_choice', array());
-
-    // the void option should not be selected along with other tc types, unless
-    // there is only one and it is cablecar
-    $config_choice = sfConfig::get('app_parkings_public_transportation_ratings');
-    unset($config_choice[0]);
-    unset($config_choice[9]);
-    $config_choice = array_keys($config_choice);
-
-    if (in_array(0, $value) && sizeof(array_intersect($config_choice, $value)))
+    public function execute (&$value, &$error)
     {
-        $error = $this->getParameter('exclusive_choice_error', 'bad choice error');
-        return false;
+        // list of possible pt_types, excluding 0 (=no choice)
+        // and 9 (cablecar, specific behaviour)
+        $config_choice = sfConfig::get('app_parkings_public_transportation_ratings');
+        unset($config_choice[0]);
+        unset($config_choice[9]);
+        $config_choice = array_keys($config_choice);
+
+        // check that pt ratings is compliant with a pt types
+        $pt_rating_param = $this->getParameterHolder()->get('pt_rating');
+        $pt_rating_value = $this->getContext()->getRequest()->getParameter($pt_rating_param);
+        if (($pt_rating_value == "0" || $pt_rating_value == "3") && sizeof(array_intersect($config_choice, $value)))
+        {
+            $error = $this->getParameter('pt_rating_choice_error', 'bad choice error');
+            return false;
+        }
+    
+        // the void option should not be selected along with other tc types, unless
+        // there is only one and it is cablecar
+        if (in_array(0, $value) && sizeof(array_intersect($config_choice, $value)))
+        {
+            $error = $this->getParameter('exclusive_choice_error', 'bad choice error');
+            return false;
+        }
+
+        return true;
     }
 
+    public function initialize ($context, $parameters = null)
+    {
+        // initialize parent
+        parent::initialize($context);
 
-    return true;
-  }
+        $this->getParameterHolder()->add($parameters);
 
-  public function initialize ($context, $parameters = null)
-  {
-    // initialize parent
-    parent::initialize($context);
-
-    $this->getParameterHolder()->add($parameters);
-
-    return true;
-  }
+        return true;
+    }
 }
