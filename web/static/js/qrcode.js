4d79aa5bf24e035bd4d848f00a8a78a0f3c24e43
lbesson <lionel.besson@gmail.com>
2013-09-30 19:21:49 +0200

    correct qrcode source (mobile link changed)

--- web/static/js/qrcode.js
+++ web/static/js/qrcode.js
@@ -6,9 +6,9 @@
   {
     if ($$('.license_box')) {
       $$('.license_box').first().insert({ top: '<img class="qrcode printonly" src="https://chart.googleapis.com/chart?chs=70x70&amp;cht=qr&amp;choe=UTF-8&amp;chl='
-                                               + encodeURIComponent($$('.mobile_link').first().href + /\d+/.exec(window.location.href))
+                                               + encodeURIComponent($('m-link').hostname + '/' + /\d+/.exec(window.location.href))
                                                + '"><br />' });
-    } // TODO
+    }
   });
 
 })();
