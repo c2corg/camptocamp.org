646e904387c1300ac470b1b608d3142e5ccee8af
lbesson <lionel.besson@gmail.com>
2013-10-03 20:39:26 +0200

    incorrect conflict resolution

--- web/static/js/qrcode.js
+++ web/static/js/qrcode.js
@@ -1,22 +1,10 @@
 (function($) {
 
-<<<<<<< HEAD
   // add the qrcode to the document once dom is loaded
   $(function() {
     $('.license_box').prepend('<img class="qrcode printonly" ' +
         'src="https://chart.googleapis.com/chart?chs=70x70&amp;cht=qr&amp;choe=UTF-8&amp;chl=' +
-        encodeURIComponent($('.mobile_link').first().href + /\d+/.exec(window.location.href)) + '"><br>');
-=======
-  "use strict";
-
-  Event.observe(window, 'load', function()
-  {
-    if ($$('.license_box')) {
-      $$('.license_box').first().insert({ top: '<img class="qrcode printonly" src="https://chart.googleapis.com/chart?chs=70x70&amp;cht=qr&amp;choe=UTF-8&amp;chl='
-                                               + encodeURIComponent($('m-link').hostname + '/' + /\d+/.exec(window.location.href))
-                                               + '"><br />' });
-    }
->>>>>>> master
+        encodeURIComponent($('#m-link').hostname + '/' + /\d+/.exec(window.location.href)) + '"><br>');
   });
 
 })(jQuery);
