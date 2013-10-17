6e508bc458356f3a47c9683f0051efd11450b5ec
lbesson <lionel.besson@gmail.com>
2013-10-17 19:41:13 +0200

    add an indicator in favicon in case of unread PMs

--- apps/frontend/modules/documents/templates/_getMsg.php
+++ apps/frontend/modules/documents/templates/_getMsg.php
@@ -1,1 +1,10 @@
-<?php echo '('.f_link_to($UnreadMsg, 'message_list.php').')'; ?>
+<?php
+echo '('.f_link_to($UnreadMsg, 'message_list.php').')';
+
+if ($UnreadMsg && !c2cTools::mobileVersion())
+{
+    use_helper('JavascriptQueue');
+    echo javascript_queue("$.ajax({ url: '" . minify_get_combined_files_url('/static/js/tinycon.min.js') . "', dataType: 'script', cache: true })
+      .done(function() { Tinycon.setBubble($UnreadMsg); });");
+}
+?>
