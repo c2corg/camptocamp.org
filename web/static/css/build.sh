64f745e8b62a1fb02a1d51185bde9e928a97fcd1
Marc Fournier <marc.fournier@camptocamp.org>
2013-10-22 00:47:15 +0200

    don't fail when no previous CSS files are present (typically on 1st build)

--- web/static/css/build.sh
+++ web/static/css/build.sh
@@ -3,7 +3,7 @@
 set -e # exit on error
 
 # delete existing css files, prevents keeping obsolete ones forever
-rm *.css
+rm -f *.css
 
 for i in `ls sass/*.scss | cut -c6- | grep -v '^_'`; do
   sass --unix-newlines -t expanded -r c2c_sass_functions.rb sass/$i ${i%scss}css --trace
