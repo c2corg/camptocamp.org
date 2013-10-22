b6d37f9920080d4a272e1ef4458911dbe5ce3e73
lbesson <lionel.besson@gmail.com>
2013-10-22 19:53:06 +0200

    enable climbing sites selection in outings wizard

--- apps/frontend/modules/outings/templates/wizardSuccess.php
+++ apps/frontend/modules/outings/templates/wizardSuccess.php
@@ -9,32 +9,57 @@ use_helper('JavascriptQueue', 'AutoComplete', 'Form', 'MyForm');
 
 <hr />
 <h4><?php echo __('Step 1: choose a summit'); ?></h4>
-<div id="wizard_summit">
+<div>
 <?php 
 echo global_form_errors_tag();
-echo __('Summit:') . input_tag('summits_name', '', array('size' => '35'));
+echo select_tag('wizard_type', options_for_select(array('summits' => __('Summit:'), 'sites' => __('Site:')), 0));
+echo input_tag('summits_name', '', array('size' => '35'));
+echo input_tag('sites_name', '', array('size' => '35', 'style' => 'display:none'));
 echo javascript_queue("var indicator = $('#indicator');
+$('#wizard_type').change(function() {
+  $('#summits_name, #sites_name').val('').toggle();
+  $('#wizard_no_route, #wizard_route, #summit_link, #last_ok').hide();
+
+  var summit = $(this).val() == 'summits';
+  $('#wizard_routes_hints').toggle(summit);
+  $('#wizard_sites_hints').toggle(!summit);
+  
+});
+
 $('#summits_name').c2cAutocomplete({
   url: '" . url_for('summits/autocomplete') . "',
   minChars: " . sfConfig::get('app_autocomplete_min_chars') . "
 }).on('itemselect', function(e, item) {
+  indicator.show();
   $('#summit_id').val(item.id);
-  $('#wizard_no_route, #wizard_hints').hide();
+  $('#wizard_no_route, .wizard_hints').hide();
   $.get('" . url_for('summits/getroutes') . "',
              'summit_id=' + $('#summit_id').val() + '&div_name=routes')
     .always(function() { indicator.hide(); })
     .done(function(data) {
       $('#divRoutes').html(data);
+      $('#last_ok h4').html($('#wizard_routes_hints h4').last().html());
       $('#wizard_no_route').hide();
       $('#summit_link, #wizard_route, #last_ok').show();
       C2C.getWizardRouteRatings('routes');
     })
     .fail(function(data) {
-      $('#wizard_route, #wizard_hints, #wizard_route_descr').hide();
+      $('#wizard_route, #wizard_hints, #wizard_route_descr, #last_ok').hide();
       $('#summit_link, #wizard_no_route').show();
       C2C.showFailure(data.responseText);
     })
-});");
+});
+
+$('#sites_name').c2cAutocomplete({
+  url: '" . url_for('sites/autocomplete') . "',
+  minChars: " . sfConfig::get('app_autocomplete_min_chars') . "
+}).on('itemselect', function(e, item) {
+  $('.wizard_hints').hide();
+  $('#last_ok h4').html($('#wizard_sites_hints h4').last().html());
+  $('#last_ok').show();
+  $('#link').val(item.id);
+});
+");
 
 echo form_tag('outings/wizard');
 echo input_hidden_tag('summit_id', '0');
@@ -45,12 +70,16 @@ echo input_hidden_tag('summit_id', '0');
 <p id="wizard_summit_create" class="wizard_tip"><?php echo __('No summit matching your search?') . ' ' . 
 link_to(__('Add your summit'), '@document_edit?module=summits&id=&lang='); ?></p>
 </form>
-</div> <!-- wizard_summit -->
+</div>
 
-<div id="wizard_hints">
+<div id="wizard_routes_hints" class="wizard_hints">
 <hr />
 <h4><?php echo __('Step 2: choose a route')  ?></h4>
 <h4><?php echo __('Step 3: confirm to create outing')  ?></h4>
+</div>
+<div id="wizard_sites_hints" class="wizard_hints" style="display: none">
+<hr />
+<h4><?php echo __('Step 2: confirm to create outing')  ?></h4>
 </div>
 
 <div id="wizard_no_route" style="display: none">
@@ -73,12 +102,13 @@ link_to(__('Add your summit'), '@document_edit?module=summits&id=&lang='); ?></p
 <?php echo __('Short description: '); ?>
 <span id="routes_descr"><?php echo __('not available'); ?></span>
 <br />
-<a href="#" onclick="window.open('/routes/' + $('routes').val();">
+<a href="#" onclick="window.open('/routes/' + $('#routes').val();">
   <?php echo __('Show the route') ?>
 </a>
 </p> <!-- wizard_route_descr -->
 <p class="wizard_tip"><?php echo __('No route matching your search?') . ' '; ?>
 <a href="#" onclick="window.location.href='/routes/edit/link/' + $('#summit_id').val(); return false;"><?php echo __('Add your route') ?></a></p>
+</div>
 
 <div id="last_ok" style="display: none;">
 <hr />
@@ -86,13 +116,12 @@ link_to(__('Add your summit'), '@document_edit?module=summits&id=&lang='); ?></p
 <?php 
 echo form_tag('outings/edit', 'method=get');
 echo input_hidden_tag('link', '0');
-echo c2c_submit_tag(__('New outing'), array('onclick' => '$("#link").val($("#routes").val());',
+echo c2c_submit_tag(__('New outing'), array('onclick' => 'if ($("#wizard_type") == "summits") $("#link").val($("#routes").val());',
                                             'title' => __('Add your outing'),
                                             'picto' => 'action_create'));
 ?>
 </form>
 </div> <!-- last_ok -->
-</div> <!-- wizard_route -->
 </div> <!-- outing_wizard -->
 
 
