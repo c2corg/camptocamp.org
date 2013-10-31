c197ca459b82eef98a5e1240f0fc0a466d173bca
Marc Fournier <marc.fournier@camptocamp.org>
2013-11-01 00:27:10 +0100

    indentation consistency

--- web/static/js/metac2crss.js
+++ web/static/js/metac2crss.js
@@ -1,18 +1,18 @@
 (function(C2C, $) {
   $(function() {
     $.get(C2C.meta_feed_url, function(data) {
-        var $xml = $(data);
-        $xml.find("item").each(function() {
-            var $this = $(this),
-                item = {
-                    title: $this.find("title").text(),
-                    link: $this.find("link").text(),
-                    description: $this.find("description").text(),
-                    pubDate: $this.find("pubDate").text(),
-                    author: $this.find("author").text()
-            }
-            $("#on_the_web_section_list").append("<li>" + item["title"] + "</li>");
-        });
+      var $xml = $(data);
+      $xml.find("item").each(function() {
+        var $this = $(this),
+          item = {
+            title: $this.find("title").text(),
+            link: $this.find("link").text(),
+            description: $this.find("description").text(),
+            pubDate: $this.find("pubDate").text(),
+            author: $this.find("author").text()
+          }
+        $("#on_the_web_section_list").append("<li>" + item["title"] + "</li>");
+      });
     });
   });
 })(window.C2C = window.C2C || {}, jQuery);
