69c26e5ce9388d2645ee660fb8761536701024a2
lbesson <lionel.besson@gmail.com>
2013-11-11 16:22:24 +0100

    qrcode

--- web/static/js/qrcode.js
+++ web/static/js/qrcode.js
@@ -4,7 +4,7 @@
   $(function() {
     $('.license_box').prepend('<img class="qrcode printonly" ' +
         'src="//chart.googleapis.com/chart?chs=70x70&amp;cht=qr&amp;choe=UTF-8&amp;chl=' +
-        encodeURIComponent($('#m-link').hostname + '/' + /\d+/.exec(window.location.href)) + '"><br>');
+        encodeURIComponent($('#m-link')[0].hostname + '/' + /\d+/.exec(window.location.href)) + '"><br>');
   });
 
 })(jQuery);
