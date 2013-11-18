765c9f23ca167667017407ef39f7626ae065a264
lbesson <lionel.besson@gmail.com>
2013-11-18 19:28:13 +0100

    json list fixes
    http://www.camptocamp.org/forums/viewtopic.php?id=230596
    - conditions and hastrack boolean correctly done
    - empty total items & co
    
    fixes #862

--- apps/frontend/modules/documents/templates/jsonlistSuccess.php
+++ apps/frontend/modules/documents/templates/jsonlistSuccess.php
@@ -9,17 +9,20 @@ if (!isset($items) && $nb_results > 0)
 {
     $items = $pager->getResults('array', ESC_RAW);
     $items = Language::parseListItems($items, c2cTools::module2model($module));
-
-    $totalItems = $pager->getNbResults();
-    $startIndex = $pager->getMaxPerPage() * ($pager->getPage() - 1) + 1;
-    $count = min($pager->getMaxPerPage(), $pager->getNbResults() - ($pager->getPage() - 1) * ($pager->getMaxPerPage()));
-    $hasPreviousPage = ($pager->getPage() != 1);
-    $hasNextPage = ($pager->getPage() != $pager->getLastPage() && $nb_results);
 }
 elseif ($nb_results == 0)
 {
     $items = array();
     $totalItems = $startIndex = $count = $hasPreviousPage = $hasNextPage = 0;
+}
+
+if (isset($pager))
+{
+    $totalItems = $pager->getNbResults();
+    $startIndex = $pager->getMaxPerPage() * ($pager->getPage() - 1) + 1;
+    $count = min($pager->getMaxPerPage(), $pager->getNbResults() - ($pager->getPage() - 1) * ($pager->getMaxPerPage()));
+    $hasPreviousPage = ($pager->getPage() != 1);
+    $hasNextPage = ($pager->getPage() != $pager->getLastPage() && $nb_results);
 }
 
 // compute prev and next uris
