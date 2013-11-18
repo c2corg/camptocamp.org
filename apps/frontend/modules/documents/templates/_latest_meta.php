4c2b9ab7587e076dd96adc9f79568b1f2808fc72
Marc Fournier <marc.fournier@camptocamp.org>
2013-11-18 01:08:57 +0100

    inline JS metaengine rss parser

--- apps/frontend/modules/documents/templates/_latest_meta.php
+++ apps/frontend/modules/documents/templates/_latest_meta.php
@@ -1,9 +1,8 @@
 <div id="on_the_web" class="latest">
 <?php
-use_helper('SmartDate', 'Javascript');
+use_helper('SmartDate');
 
 $response = sfContext::getInstance()->getResponse();
-$response->addJavascript('/static/js/metac2crss.js');
 
 if (!isset($default_open))
 {
@@ -22,10 +21,51 @@ include_partial('documents/home_section_title',
                       'custom_title_icon' => 'outings'));
 ?>
 <?php
-echo javascript_tag('
-(function(C2C) {
-  C2C.meta_feed_url = "' . html_entity_decode(html_entity_decode($feed_url)) . '";
-})(window.C2C = window.C2C || {});
+echo javascript_queue('
+$.get("' . html_entity_decode(html_entity_decode($feed_url)) . '").done(function(data) {
+  var $xml = $(data);
+  var count = 0;
+  var date;
+
+  $xml.find("item").each(function() {
+    var $this = $(this),
+      item = {
+        title: $this.find("title").text(),
+        link: $this.find("link").text(),
+        description: $this.find("description").text(),
+        pubDate: $this.find("pubDate").text(),
+        author: $this.find("author").text()
+      }
+
+    if (count == 0) {
+      $("#on_the_web_section_list").html("");
+    }
+    count++;
+    if (count > 10) { return; }
+    li_class = (count % 2) ? "even" : "odd";
+
+    if (date != item["pubDate"]) {
+      elems = item["pubDate"].split("-");
+      datespan = "<span class=\"date\">" + elems[2] + "/" + elems[1] + "</span>";
+    } else {
+      datespan = "";
+    }
+
+    date = item["pubDate"];
+    lang = item["description"].split(" , ")[5];
+    author = item["author"].substring(item["author"].indexOf("(") - 1);
+
+    $("#on_the_web_section_list").append(
+      "<li class=\"" + li_class + "\">"
+      + datespan
+      + "<a href=\"" + item["link"] + "\" "
+      + "hreflang=\"" + lang + "\""
+      + ">" + item["title"] + "</a> "
+      + "<span class=\"meta\">" + author + "</span>"
+      + "</li>"
+    );
+  });
+});
 ');
 ?>
 <div id="on_the_web_section_container" class="home_container_text">
