30a7455e7fceedea6c98ca4d90e399bb4015a2b9
lbesson <lionel.besson@gmail.com>
2013-11-30 17:16:18 +0100

    more compass

--- web/static/sass/build.sh
+++ web/static/sass/build.sh
@@ -3,11 +3,16 @@
 set -e # exit on error
 
 # delete obsolete css files
-for i in `ls ../css | grep '\.css$'`; do
-  if [ ! -f ${i%css}scss ]; then
-    rm -f ../css/$i
-  fi
-done
+#for i in `ls ../css | grep '\.css$'`; do
+#  if [ ! -f ${i%css}scss ]; then
+#    rm -f ../css/$i
+#  fi
+#done
+
+# delete all css files
+# we want to be sure that they are up-to-date
+# even if referred images changed
+rm -f ../css/*.css
 
 # compile stylesheets
 compass compile
--- web/static/sass/config.rb
+++ web/static/sass/config.rb
@@ -1,9 +1,26 @@
-require 'c2c_sass_functions'
+require "c2c"
+require "md5"
 
 css_dir = "../css"
 sass_dir = "."
+images_dir = "../.."
 
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :expanded
 
 line_comments = false
+
+# append hash before url for versioning
+# (which is in fact the 8 first chars of mda5, which is shorter and
+# provides enough differentiation between two versions of a file)   
+asset_cache_buster do |path, real_path|
+  if File.exists?(real_path)
+    pathname = Pathname.new(path)
+    hash = MD5.file(real_path.path)
+    new_path = "/%s%s" % [hash.to_s[0,8], pathname.cleanpath]
+
+    {:path => new_path, :query => nil}
+  else
+    raise Compass::Error, "File not found: %s" % Pathname.new(real_path.path).cleanpath
+  end
+end
