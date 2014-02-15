cfb1169af1a140731151448cadbcf086fa7b5ea5
fbunoz <frederic.bunoz@camptocamp.org>
2014-02-15 18:35:35 +0100

    update function argument (use an array for options)

--- apps/frontend/modules/outings/templates/popupSuccess.php
+++ apps/frontend/modules/outings/templates/popupSuccess.php
@@ -31,22 +31,24 @@ if ($image) {
 $activities = $document->getRaw('activities');
 li(field_activities_data($document));
 li(field_bool_data($document, 'partial_trip'));
-li(field_data_range_if_set($document, 'min_elevation', 'max_elevation', 'elevation separator', '', '', 'meters'));
-li(field_data_range_if_set($document, 'height_diff_up', 'height_diff_down', 'height diff separator', '+', '-', 'meters', true));
+li(field_data_range_if_set($document, 'min_elevation', 'max_elevation', array('separator' => 'elevation separator', 'suffix' => 'meters')));
+li(field_data_range_if_set($document, 'height_diff_up', 'height_diff_down', array('separator' => 'height diff separator', 'prefix_min' => '+',
+        'prefix_max' => '-', 'suffix' => 'meters', 'range_only' => true)));
 li(field_bool_data($document, 'outing_with_public_transportation'));
-$access_elevation = field_data_if_set($document, 'access_elevation', '', 'meters');
+$access_elevation = field_data_if_set($document, 'access_elevation', array('suffix' => 'meters'));
 if (empty($access_elevation))
 {
     li(field_data_from_list_if_set($document, 'access_status', 'mod_outings_access_statuses_list'));
 }
 else
 {
-    $access_status = field_data_from_list_if_set($document, 'access_status', 'mod_outings_access_statuses_list', false, true, ' - ');
+    $access_status = field_data_from_list_if_set($document, 'access_status', 'mod_outings_access_statuses_list', array('raw' => true, 'prefix' => ' - '));
     li($access_elevation . $access_status);
 }
 if (array_intersect(array(1,2,5), $activities)) // ski, snow or ice_climbing
 {
-    li(field_data_range_if_set($document, 'up_snow_elevation', 'down_snow_elevation', 'elevation separator', '', '', 'meters'));
+    li(field_data_range_if_set($document, 'up_snow_elevation', 'down_snow_elevation', array('separator' => 'elevation separator',
+            'suffix' => 'meters')));
 }
 li(field_data_from_list_if_set($document, 'conditions_status', 'mod_outings_conditions_statuses_list'));
 li(field_data_from_list_if_set($document, 'glacier_status', 'mod_outings_glacier_statuses_list'));
