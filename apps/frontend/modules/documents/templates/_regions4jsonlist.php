9d63c076ec292240de4eeab07dd222da8ee87139
lbesson <lionel.besson@gmail.com>
2014-03-20 18:13:59 +0100

    json lists: give area type
    this is useful for stats

--- apps/frontend/modules/documents/templates/_regions4jsonlist.php
+++ apps/frontend/modules/documents/templates/_regions4jsonlist.php
@@ -1,11 +1,18 @@
-<?php 
+<?php
+$at = array(
+    'dc' => 'country',
+    'dd' => 'admin_limits',
+    'dr' => 'range',
+    'dv' => 'valley_area');
+
 foreach ($geoassociations as $geo_id => $geoP)
 {
     $i18n = $geoP['AreaI18n'][0];
     $a[$geoP['type'].$geo_id] = array(
         'name' => $i18n->getRaw('name'),
         'url' => absolute_link(url_for("@document_by_id_lang_slug?module=areas&id=$geo_id&lang="
-                     .$i18n['culture'].'&slug='.make_slug($i18n['name'])))
+                     .$i18n['culture'].'&slug='.make_slug($i18n['name']))),
+        'type' => $at[$geoP['type']]
     );
 }
 
