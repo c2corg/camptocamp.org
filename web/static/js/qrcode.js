95b5cbc93c2a35f28d27428caeb05b8da82b90a9
lbesson <lionel.besson@gmail.com>
2014-10-18 13:03:49 +0200

    remove references to old mobile version m.*

--- web/static/js/qrcode.js
+++ web/static/js/qrcode.js
@@ -1,10 +1,10 @@
-(function($) {
+(function($, l) {
 
   // add the qrcode to the document once dom is loaded
   $(function() {
     $('.license_box').prepend('<img class="qrcode printonly" ' +
         'src="//chart.googleapis.com/chart?chs=70x70&amp;cht=qr&amp;choe=UTF-8&amp;chl=' +
-        encodeURIComponent('http://' + $('#m-link')[0].host + '/' + /\d+/.exec(window.location.href)) + '"><br>');
+        encodeURIComponent(l.protocol + '//' + l.host + '/' + /\d+/.exec(l.href)) + '"><br>');
   });
 
-})(jQuery);
+})(jQuery, window.location);
