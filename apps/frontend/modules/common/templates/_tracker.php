ea9468f24bb396321e19ee51b3004721738053a4
lbesson <lionel.besson@gmail.com>
2014-10-18 10:52:52 +0200

    analytics tracker update for tracking form factor
    - only one view for desktop and mobile version on www.camptocamp.org
    - use dimesnion3 to track form factor at session level

--- apps/frontend/modules/common/templates/_tracker.php
+++ apps/frontend/modules/common/templates/_tracker.php
@@ -1,15 +1,16 @@
 <?php
 // google analytics tracker notes
-// - we set coockie domain to www.camptocamp.org / m.camptocamp.org because analytic would otherwise use *.camptocamp.org
+// - we set coockie domain to www.camptocamp.org because analytic would otherwise use *.camptocamp.org
 //   we don't want cookies to be sent with s.camptocamp.org
-// - dimensions are used for tracking forum pageviews, and user status (connected or not). Since we use universal analytics,
-//   the scope is configured on analyrics website 
+// - dimensions are used for tracking forum pageviews, form factor and user status (connected or not).
+//   Since we use universal analytics, the scope is configured on analyrics website 
 // - we use IP anonymization, we loose some geographic accuracy, but...
 
-$k = c2cTools::mobileVersion() ? sfConfig::get('app_mobile_ganalytics_key') : sfConfig::get('app_ganalytics_key');
-// keep connection status with analytics
+// keep connection status with analytics (dimension1)
 $status = $sf_user->isConnected() ? 'Member' : 'Visitor';
-// track forum id on viewtopic and viewforum pages
+// track form factor (dimension3)
+$form_factor = $sf_user->getAttribute('form_factor');
+// track forum id on viewtopic and viewforum pages (dimension2)
 $forum_track = isset($tracker_forum_id) ? ",'dimension2':'$tracker_forum_id'" : '';
 ?>
 <script>
@@ -17,8 +18,8 @@ $forum_track = isset($tracker_forum_id) ? ",'dimension2':'$tracker_forum_id'" : 
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create','<?php echo $k ?>',{'cookieDomain':window.location.host});
-ga('send','pageview',{'anonymizeIp':true,'dimension1':'<?php echo $status?>'<?php echo $forum_track ?>});
+ga('create','<?php echo sfConfig::get('app_ganalytics_key') ?>',{'cookieDomain':window.location.host});
+ga('send','pageview',{'anonymizeIp':true,'dimension1':'<?php echo $status ?>','dimension3':'<?php echo $form_factor ?>'<?php echo $forum_track ?>});
 <?php 
 // addthis script must be added after ga tracker for google analytics integration, it will be loaded asynchronously
 if (isset($addthis) && $addthis): ?>
