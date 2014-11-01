e4043163a3b78b101f92552a832e07209b6ed02a
lbesson <lionel.besson@gmail.com>
2014-11-01 10:56:11 +0100

    https friendly banners

--- apps/frontend/modules/common/templates/_mobile_banner.php
+++ apps/frontend/modules/common/templates/_mobile_banner.php
@@ -1,9 +1,9 @@
 <div id="mobile_banner">
 <?php if (sfConfig::get('app_production') == 1): ?>
-<script async src="http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <ins class="adsbygoogle" style="display:inline-block;width:320px;height:50px" data-ad-client="ca-pub-8662990478599655" data-ad-slot="3178153545"></ins>
 <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 <?php else: ?>
-<div style="text-align: center"><img src="http://fakeimg.pl/320x50/282828/eae0d0/?text=Publicit%C3%A9" /></div>
+<div style="text-align: center"><img src="//fakeimg.pl/320x50/282828/eae0d0/?text=Publicit%C3%A9" /></div>
 <?php endif ?>
 </div>
