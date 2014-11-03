5e2c1a736f0c628d330c300ae9a402513c775fd1
fbunoz <frederic.bunoz@camptocamp.org>
2014-11-03 20:52:07 +0100

    ajoute fonction multibyte_ucfirst

--- apps/frontend/lib/c2cTools.class.php
+++ apps/frontend/lib/c2cTools.class.php
@@ -2,6 +2,12 @@
 
 class c2cTools
 {
+    public static function multibyte_ucfirst($str)
+    {
+        $fc = mb_strtoupper(mb_substr($str, 0, 1));
+        return $fc.mb_substr($str, 1);
+    }
+    
     public static function getArrayElement($elements_list, $key, $alt_key = null, $alt_value = null)
     {
         $result = null;
