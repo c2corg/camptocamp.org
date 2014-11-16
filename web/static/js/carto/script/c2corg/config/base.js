635b56cfa4100d136ea2c9bb8613459adff9b7a2
Alexandre Saunier <alexandre.saunier@camptocamp.org>
2014-11-16 15:41:45 +0100

    Add swisstopo pixelkarte-farbe

--- web/static/js/carto/script/c2corg/config/base.js
+++ web/static/js/carto/script/c2corg/config/base.js
@@ -1,5 +1,6 @@
 /**
  * @requires c2corg/config/config.js
+ * @requires c2corg/widgets/Map.js
  * @requires OpenLayers/Control/Navigation.js
  * @requires OpenLayers/Control/KeyboardDefaults.js
  * @requires OpenLayers/Control/PanZoomBar.js
@@ -30,6 +31,29 @@ c2corg.base = {
                      '<img src="//api.ign.fr/geoportail/api/js/2.0.0beta/theme/geoportal/img/logo_gp.gif">' +
                      '</a> <a href="http://www.geoportail.gouv.fr/depot/api/cgu/licAPI_CGUF.pdf" ' +
                      'alt="TOS" title="TOS" target="_blank">' + OpenLayers.i18n('Terms of Service') + '</a>'
+    },
+
+    swisstopoOptions: {
+        version: "1.0.0",
+        requestEncoding: "REST",
+        url: ['http://wmts0.geo.admin.ch/', 'http://wmts1.geo.admin.ch/', 'http://wmts2.geo.admin.ch/',
+              'http://wmts3.geo.admin.ch/', 'http://wmts4.geo.admin.ch/'],
+        style: "default",
+        matrixSet: "21781",
+        formatSuffix: 'jpeg',
+        dimensions: ['TIME'],
+        params: {
+            'time': '20140520'
+        },
+        projection: new OpenLayers.Projection('EPSG:21781'),
+        units: 'm',
+        format: 'image/jpeg',
+        buffer: 0,
+        opacity: 1.0,
+        maxExtent: new OpenLayers.Bounds(420000, 30000, 900000, 350000),
+        tileOrigin: OpenLayers.LonLat(420000, 350000),
+        resolutions: [650, 500, 250, 100, 50, 20, 10, 5, 2.5],
+        serverResolutions: [4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250, 2000, 1750, 1500, 1250, 1000, 750, 650, 500, 250, 100, 50, 20, 10, 5, 2.5, 2, 1.5, 1, 0.5]
     },
 
     // layer sources
@@ -106,6 +130,17 @@ c2corg.base = {
                 visibility: false,
                 group : "background"
             }, c2corg.base.ignOptions)]
+        }, {
+            source: "olsource",
+            type: "OpenLayers.Layer.WMTS",
+            group: "background",
+            args: [Ext.applyIf({
+                name: OpenLayers.i18n("swisstopo CN"),
+                layer: "ch.swisstopo.pixelkarte-farbe",
+                ref: "swisstopo_pixelkarte_farbe",
+                visibility: false,
+                group : "background"
+            }, c2corg.base.swisstopoOptions)]
         }];
     },
 
@@ -137,13 +172,15 @@ c2corg.base.getMap = function(options) {
     options = Ext.applyIf(options || {}, {
         controls: {}
     });
+    // Note: because of map projection must sometimes be changed when setting
+    // a new baselayer, OpenLayers.setBaseLayer() is overridden
+    // in c2corg/widgets/Map.js
     return {
         xtype: "cgxp_mappanel",
         extent: c2corg.base.initialExtent,
         maxExtent: c2corg.base.restrictedExtent,
         restrictedExtent: c2corg.base.restrictedExtent,
         stateId: "map",
-        //projection: new OpenLayers.Projection("EPSG:900913"),
         projection: new OpenLayers.Projection("EPSG:3857"),
         units: "m",
         controls: c2corg.base.getControls(options.controls),
