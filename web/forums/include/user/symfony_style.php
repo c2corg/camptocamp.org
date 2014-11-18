bbe2f466110113d6da988351a5d01b54be88a609
lbesson <lionel.besson@gmail.com>
2014-11-18 20:21:29 +0100

    forum & mobile version

--- web/forums/include/user/symfony_style.php
+++ web/forums/include/user/symfony_style.php
@@ -19,7 +19,6 @@ sfLoader::loadHelpers(array('Helper', 'MyMinify', 'Asset'));
 $debug = defined('PUN_DEBUG');
 
 if ($mobile_version): ?>
-<meta name="viewport" content="initial-scale=1" />
 <script type="text/javascript">
 (function(m,w){var l='<?php echo trim(minify_get_main_stylesheets(!$debug, $debug)); ?>',r=1
 w.devicePixelRatio?r=w.devicePixelRatio:"matchMedia"in w&&w.matchMedia&&(w.matchMedia("(min-resolution: 2dppx)").matches||w.matchMedia("(min-resolution: 192dpi)").matches?r=2:(w.matchMedia("(min-resolution: 1.5dppx)").matches||w.matchMedia("(min-resolution: 144dpi)").matches)&&(r=1.5))
@@ -33,5 +32,5 @@ if (!$mobile_version): ?>
 <!--[if IE 7]><link rel="stylesheet" type="text/css" media="all" href="<?php echo  minify_get_combined_files_url('/static/css/ie7.css') ?>" /><![endif]-->
 <!--[if lt IE 9]><script src="<?php echo minify_get_combined_files_url(array('/static/js/html5shiv.js','/static/js/autofocus.js', '/static/js/indexof.js')) ?>"></script><![endif]-->
 <?php else: ?>
-<link rel="apple-touch-icon-precomposed" href="<?php echo PUN_STATIC_URL; ?>/apple-touch-icon-precomposed.png"" />
+<link rel="apple-touch-icon-precomposed" href="<?php echo PUN_STATIC_URL; ?>/apple-touch-icon-precomposed.png" />
 <?php endif; ?>
