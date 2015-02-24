2e12b98ebba3357638c86163749989d4f03913a4
stef74 <stephane.peiti@gmail.com>
2015-02-24 22:22:44 +0100

    Fix search by ID in association search #2
    array init missing

--- lib/model/doctrine/generated/BaseDocument.class.php
+++ lib/model/doctrine/generated/BaseDocument.class.php
@@ -1878,6 +1878,7 @@ class BaseDocument extends sfDoctrineRecordI18n
         // - ...
         if ($use_docid)
         {
+            $where_vars=array();
             $where_clause = 'm.redirects_to IS NULL AND mi.id = ?';
             array_push($where_vars, $name);
         }
