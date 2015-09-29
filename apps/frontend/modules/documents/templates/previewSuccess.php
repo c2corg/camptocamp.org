602b46c9388436cbbd21f99dde856065d33d0fed
fbunoz <frederic.bunoz@camptocamp.org>
2015-09-29 22:45:19 +0200

    use preview section functions

--- apps/frontend/modules/documents/templates/previewSuccess.php
+++ apps/frontend/modules/documents/templates/previewSuccess.php
@@ -1,5 +1,5 @@
 <?php 
-use_helper('Viewer');
+use_helper('Viewer', 'Sections');
 
 if ($concurrent_edition)
 {
@@ -18,28 +18,22 @@ else
 <h2><?php echo __($title) ?></h2>
 <p class="preview_warning"><?php echo __($warning) ?></p>
 
-<?php echo display_title(isset($title_prefix) ? $title_prefix.__('&nbsp;:').' '.$document->get('name') : $document->get('name')) ?>
+<?php
+echo display_title(isset($title_prefix) ? $title_prefix.__('&nbsp;:').' '.$document->get('name') : $document->get('name'));
 
+echo start_preview_section_tag('Information', 'data', 'info');
+include_partial('data', array('document' => $document, 'preview' => true));
+echo end_preview_section_tag();
 
-<div class="title" id="preview_info">
-<?php echo '<a href="#form_info">' . __('Information') . '<span class="tips">[' . __('Go back to form') . ']</span></a>'?>
-</div><hr />
-<div id="data_section_container" class="section">
-<?php include_partial('data', array('document' => $document, 'preview' => true)); ?>
-</div>
+echo start_preview_section_tag('Description', 'description', 'desc');
+?><div class="article_contenu"><?php 
+include_partial('i18n', array('document' => $document, 'needs_translation' => false,
+                              'associated_books' => isset($associated_books) ? $associated_books : null,
+                              'images' => $associated_images, 'filter_image_type' => $filter_image_type, 'preview' => true));
+?></div><?php 
+echo end_preview_section_tag();
 
-
-<div class="title" id="preview_desc">
-<?php echo '<a href="#form_desc">' . __('Description') . '<span class="tips">[' . __('Go back to form') . ']</span></a>'?>
-</div><hr />
-<div id="description_section_container" class="section">
-    <div class="article_contenu">
-<?php include_partial('i18n', array('document' => $document, 'needs_translation' => false,
-                                    'associated_books' => isset($associated_books) ? $associated_books : null,
-                                    'images' => $associated_images, 'filter_image_type' => $filter_image_type, 'preview' => true)); ?>
-    </div>
-</div>
-<div class="clearer"></div>
+?><div class="clearer"></div>
 <br /><hr />
 <div class="title">
 <?php echo '<a href="#form_desc"><span class="tips">[' . __('Go back to form') . ']</span></a>' ?>
