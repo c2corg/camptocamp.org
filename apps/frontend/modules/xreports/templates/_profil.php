c14a13a9f78c2400077e9a3b8ccc7cc93cb4aa53
fbunoz <frederic.bunoz@camptocamp.org>
2015-10-04 11:47:00 +0200

    change title o nb_outings field

--- apps/frontend/modules/xreports/templates/_profil.php
+++ apps/frontend/modules/xreports/templates/_profil.php
@@ -5,7 +5,7 @@ use_helper('Field');
     <?php
     li(field_data_from_list_if_set($document, 'author_status', 'mod_xreports_author_status_list'));
     li(field_data_from_list_if_set($document, 'activity_rate', 'mod_xreports_activity_rate_list'));
-    li(field_data_from_list_if_set($document, 'nb_outings', 'mod_xreports_nb_outings_list'));
+    li(field_data_from_list_if_set($document, 'nb_outings', 'mod_xreports_nb_outings_list', array('title' => 'nb_outings_per_year')));
     li(field_data_from_list_if_set($document, 'autonomy', 'mod_xreports_autonomy_list'));
     li(field_data_if_set($document, 'age'));
     li(field_data_from_list_if_set($document, 'gender', 'mod_xreports_gender_list'));
