7171ed657353087c9a5067df31bcd7fdfcb1e6be
fbunoz <frederic.bunoz@camptocamp.org>
2015-10-10 01:30:33 +0200

    design filtre

--- apps/frontend/modules/xreports/templates/_xreports_filter.php
+++ apps/frontend/modules/xreports/templates/_xreports_filter.php
@@ -1,25 +1,26 @@
 <?php
 use_helper('General');
-?>
-<br />
-<?php
-echo picto_tag('picto_xreports') . __('Xreport:') . ' ' . (isset($autofocus) ? input_tag('xnam', null, array('autofocus' => 'autofocus')) : input_tag('xnam'));
+
+echo '<br />';
+echo picto_tag('picto_xreports') . __('name') . ' ' . (isset($autofocus) ? input_tag('xnam', null, array('autofocus' => 'autofocus')) : input_tag('xnam'));
 echo __('elevation') . ' ' . elevation_selector('falt');
-echo georef_selector();
-?>
-<br />
-<?php
+echo '<br />';
 $activities_raw = $sf_data->getRaw('activities');
-echo activities_selector(true, true, $activities_raw);
-echo __('event_type') . ' ' . field_value_selector('xtyp', 'mod_xreports_event_type_list', array('keepfirst' => false, 'multiple' => true));
-echo __('severity') . ' ' . field_value_selector('xsev', 'mod_xreports_severity_list', array('keepfirst' => false, 'multiple' => true));
+echo    '<div class="section_subtitle">' . __('activities') . '</div>'
+      . activities_selector(true, true, $activities_raw);
+echo    '<div class="col_left">'
+      . '<div class="section_subtitle">' . __('event_type') . '</div>'
+      . field_value_selector('xtyp', 'mod_xreports_event_type_list', array('keepfirst' => false, 'multiple' => true))
+      . '</div>';
+echo    '<div class="col">'
+      . '<div class="section_subtitle">' . __('severity') . '</div>'
+      . field_value_selector('xsev', 'mod_xreports_severity_list', array('keepfirst' => false, 'multiple' => true))
+      . '</div>';
+echo '<p></p>';
 echo __('rescue') . ' ' . bool_selector('xres');
-?>
-<br />
-<?php
+echo '<br />';
 echo __('nb_participants') . ' ' . elevation_selector('xpar', '');
+echo '<br />';
 echo __('nb_impacted') . ' ' . elevation_selector('ximp', '');
-?>
-<br /><br />
-<?php
+echo '<br /><br />';
 echo __('Date') . __('&nbsp;:') . ' ' . date_selector(array('month' => true, 'year' => true, 'day' => true));
