d22ff5dec15bfefb95a928ee64183fcdf4e301bf
fbunoz <frederic.bunoz@camptocamp.org>
2015-10-10 09:17:26 +0200

    design filtre

--- apps/frontend/modules/xreports/templates/_xreports_filter.php
+++ apps/frontend/modules/xreports/templates/_xreports_filter.php
@@ -6,14 +6,14 @@ echo picto_tag('picto_xreports') . __('name') . ' ' . (isset($autofocus) ? input
 echo __('elevation') . ' ' . elevation_selector('falt');
 echo '<br />';
 $activities_raw = $sf_data->getRaw('activities');
-echo    '<div class="section_subtitle">' . __('activities') . '</div>'
+echo    '<div class="field_title">' . __('activities') . '</div>'
       . activities_selector(true, true, $activities_raw);
 echo    '<div class="col_left">'
-      . '<div class="section_subtitle">' . __('event_type') . '</div>'
+      . '<div class="field_title">' . __('event_type') . '</div>'
       . field_value_selector('xtyp', 'mod_xreports_event_type_list', array('keepfirst' => false, 'multiple' => true))
       . '</div>';
 echo    '<div class="col">'
-      . '<div class="section_subtitle">' . __('severity') . '</div>'
+      . '<div class="field_title">' . __('severity') . '</div>'
       . field_value_selector('xsev', 'mod_xreports_severity_list', array('keepfirst' => false, 'multiple' => true))
       . '<br />'
       . __('rescue') . ' ' . bool_selector('xres')
