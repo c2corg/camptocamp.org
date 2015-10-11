a0db7797e5533d74587ec98f06568697b88b7927
fbunoz <frederic.bunoz@camptocamp.org>
2015-10-11 21:37:30 +0200

    min/max on numeric fields

--- apps/frontend/modules/sites/templates/_form_fields.php
+++ apps/frontend/modules/sites/templates/_form_fields.php
@@ -17,14 +17,14 @@ include_partial('documents/oam_coords', array('document' => $document));
 ?>
 <div class="article_gauche_5050">
 <?php
-echo object_group_tag($document, 'elevation', array('suffix' => 'meters', 'class' => 'short_input', 'type' => 'number'));
-echo object_group_tag($document, 'routes_quantity', array('class' => 'short_input', 'type' => 'number'));
+echo object_group_tag($document, 'elevation', array('suffix' => 'meters', 'class' => 'short_input', 'type' => 'number', 'min' => '0', 'max' => '8900'));
+echo object_group_tag($document, 'routes_quantity', array('class' => 'short_input', 'type' => 'number', 'min' => '1'));
 echo object_group_dropdown_tag($document, 'max_rating', 'app_routes_rock_free_ratings');
 echo object_group_dropdown_tag($document, 'min_rating', 'app_routes_rock_free_ratings');
 echo object_group_dropdown_tag($document, 'mean_rating', 'app_routes_rock_free_ratings');
-echo object_group_tag($document, 'max_height', array('suffix' => 'meters', 'class' => 'short_input', 'type' => 'number'));
-echo object_group_tag($document, 'min_height', array('suffix' => 'meters', 'class' => 'short_input', 'type' => 'number'));
-echo object_group_tag($document, 'mean_height', array('suffix' => 'meters', 'class' => 'short_input', 'type' => 'number'));
+echo object_group_tag($document, 'max_height', array('suffix' => 'meters', 'class' => 'short_input', 'type' => 'number', 'min' => '0'));
+echo object_group_tag($document, 'min_height', array('suffix' => 'meters', 'class' => 'short_input', 'type' => 'number', 'min' => '0'));
+echo object_group_tag($document, 'mean_height', array('suffix' => 'meters', 'class' => 'short_input', 'type' => 'number', 'min' => '0'));
 echo object_group_dropdown_tag($document, 'equipment_rating', 'app_equipment_ratings_list');
 echo object_group_dropdown_tag($document, 'climbing_styles', 'app_climbing_styles_list',
                                array('multiple' => true));
