fc7350d9aa155384db51b3cc363d67ddf4b80589
fbunoz <frederic.bunoz@camptocamp.org>
2015-10-20 01:37:46 +0200

    change link to accident portal

--- apps/frontend/modules/documents/templates/_toolbox.php
+++ apps/frontend/modules/documents/templates/_toolbox.php
@@ -18,12 +18,7 @@ if (!isset($default_open))
                 <li><?php echo m_link_to(__('cotometre'), '@tool?action=cotometre',
                                          array('title'=> __('cotometre long')),
                                          array('width' => 600)) ?></li>
-                <li><?php
-                    $portal_config = sfConfig::get('app_portals_accident');
-                    $text = __($portal_config['name']);
-                    $portal_url = '@document_by_id?module=portals&id=' . $portal_config['id'];
-                    echo link_to($text, $portal_url);
-                ?></li>
+                <li><?php echo link_to(ucfirst(__('xreports')), '@xreports') ?></li>
                 <li><?php
                     $portal_config = sfConfig::get('app_portals_firstascent');
                     $text = __($portal_config['name']);
