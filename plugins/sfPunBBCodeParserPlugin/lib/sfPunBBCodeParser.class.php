ee189c8658ea719cc67db05054c8ffd1e69bb91b
fbunoz <frederic.bunoz@camptocamp.org>
2015-10-19 22:55:08 +0200

    debug suppression d'espace entre nombre et unité

--- plugins/sfPunBBCodeParserPlugin/lib/sfPunBBCodeParser.class.php
+++ plugins/sfPunBBCodeParserPlugin/lib/sfPunBBCodeParser.class.php
@@ -2016,8 +2016,8 @@ class sfPunBBCodeParser
         }
         $text = str_replace($pattern, $replace, $text);
         
-        $pattern = array('#(\d) ( *)((m(i?n)?|[cdhkm]m|hr?|s|°\w?)(\W|$))#i', '#((^|\W)\d+) (\w)#', '# ([?!:])#', '#(?<!(\s|\.))\.\.(?!(\s|\.))#');
-        $replace = array('\1\2\3', '\1&nbsp;\3', '&nbsp;\1', '&nbsp;');
+        $pattern = array('#(\d) ( *)((m(i?n)?|[cdhkm]m|hr?|s|°\w?)([^\w\'-]|$))#i', '#((^|\W)\d+) (\w)#', '# ([?!:])#', '#(?<!(\s|\.))\.\.(?!(\s|\.))#');
+        $replace = array('\1\3', '\1&nbsp;\3', '&nbsp;\1', '&nbsp;');
         $text = preg_replace($pattern, $replace, $text);
         
         return $text;
