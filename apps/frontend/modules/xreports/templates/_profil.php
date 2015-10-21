3cdf4bbfd70b11eeb59ceb1383478b83f3e9d4a2
fbunoz <frederic.bunoz@camptocamp.org>
2015-10-21 20:18:06 +0200

    cosmetic

--- apps/frontend/modules/xreports/templates/_profil.php
+++ apps/frontend/modules/xreports/templates/_profil.php
@@ -13,7 +13,7 @@ $is_moderator = $sf_user->hasCredential(sfConfig::get('app_credentials_moderator
         li(field_data_from_list_if_set($document, 'activity_rate', 'mod_xreports_activity_rate_list'));
         li(field_data_from_list_if_set($document, 'nb_outings', 'mod_xreports_nb_outings_list', array('title' => 'nb_outings_per_year')));
         li(field_data_from_list_if_set($document, 'autonomy', 'mod_xreports_autonomy_list'));
-        li(field_data_if_set($document, 'age', array('suffix' => __('years'))));
+        li(field_data_if_set($document, 'age', array('suffix' => '&nbsp;' . __('years'))));
         li(field_data_from_list_if_set($document, 'gender', 'mod_xreports_gender_list'));
         li(field_data_from_list_if_set($document, 'previous_injuries', 'mod_xreports_previous_injuries_list'));
     }
