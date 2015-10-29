8eab57fe22dd0e5d34dbaa8b4ea200f11627554d
fbunoz <frederic.bunoz@camptocamp.org>
2015-10-29 22:06:43 +0100

    improve nb impacted in xreport list

--- apps/frontend/modules/xreports/templates/_list_body.php
+++ apps/frontend/modules/xreports/templates/_list_body.php
@@ -13,7 +13,7 @@ $date_class = $date_light ? ' class="light"' : '';
 <td><?php echo get_paginated_value_from_list($item['event_type'], 'mod_xreports_event_type_list') ?></td>
 <td><?php echo get_paginated_value($item['severity'], 'mod_xreports_severity_list') ?></td>
 <td><?php echo (isset($item['rescue']) && !empty($item['rescue'])) ? __('yes') : '' ?></td>
-<td><?php echo (isset($item['nb_impacted'])) ? $item['nb_impacted'] : '0' ?></td>
+<td><?php echo __('nb_impacted') , ' ' , $item['nb_impacted'] , '&nbsp;/&nbsp;' , $item['nb_participants'] ?></td>
 <td><?php include_partial('documents/regions4list', array('geoassociations' => $item['geoassociations']))?></td>
 <td><?php echo (isset($item['nb_images'])) ?  $item['nb_images'] : '' ;?></td>
 <td><?php echo (isset($item['nb_comments'])) ?
