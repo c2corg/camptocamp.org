1a822fb4e37b6d948b0b212a16b4f8c4ea06a964
fbunoz <frederic.bunoz@camptocamp.org>
2015-10-29 22:09:29 +0100

    improve xreport list in mobile version

--- apps/frontend/modules/xreports/templates/_mobile_list_body.php
+++ apps/frontend/modules/xreports/templates/_mobile_list_body.php
@@ -5,14 +5,27 @@ $item_i18n = $item['XreportI18n'][0];
 $activities = $item['activities'];
 $date_class = $date_light ? ' class="light"' : '';
 ?>
+<div class="right"><?php echo get_paginated_activities($activities) ?></div>
 <div><?php echo list_link($item_i18n, 'xreports') ?></div>
-<div<?php echo $date_class ?>><time datetime="<?php echo $item['date'] ?>"><?php echo format_date($item['date'], 'D') ?></time></div>
-<div><?php echo get_paginated_activities($activities) ?></div>
-<div><?php echo displayWithSuffix($item['elevation'], 'meters') ?></div>
-<div><?php echo get_paginated_value_from_list($item['event_type'], 'mod_xreports_event_type_list') ?></div>
-<div><?php echo get_paginated_value($item['severity'], 'mod_xreports_severity_list') ?></div>
-<div><?php echo (isset($item['rescue'])) ? 'x' : '' ?></div>
-<div><?php echo (isset($item['nb_impacted'])) ? $item['nb_impacted'] : '0' ?></div>
+<?php
+echo    '<div' , $date_class , '>'
+      , implode(' - ', array(
+            '<time datetime="' . $item['date'] . '">' . format_date($item['date'], 'D') . '</time>'
+          , displayWithSuffix($item['elevation'], 'meters')
+          , get_paginated_value_from_list($item['event_type'], 'mod_xreports_event_type_list')
+        ))
+      , '</div>'
+?>
+<div><?php echo __('nb_impacted') , ' ' , $item['nb_impacted'] , '&nbsp;/&nbsp;' , $item['nb_participants'] ?></div>
+<div><?php
+echo    __('severity short') , ' '
+      , get_paginated_value($item['severity'], 'mod_xreports_severity_list')
+;
+if (isset($item['rescue']) && !empty($item['rescue']))
+{
+    echo ' - ' , __('rescue short');
+}
+?></div>
 <div><?php include_partial('documents/regions4list', array('geoassociations' => $item['geoassociations']))?></div>
 <div><?php echo picto_tag('picto_images', __('nb_linked_images')), ' ', (isset($item['nb_images'])) ?  $item['nb_images'] : '0', ' ',
                 picto_tag('action_comment', __('nb_comments')), ' ', (isset($item['nb_comments'])) ?
