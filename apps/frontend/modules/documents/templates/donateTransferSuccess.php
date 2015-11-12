02ded32247cadb056ef3c7807f8c4f1226fbbfe2
Lionel Besson <lionel.besson@gmail.com>
2015-11-12 09:34:21 +0100

    text

--- apps/frontend/modules/documents/templates/donateTransferSuccess.php
+++ apps/frontend/modules/documents/templates/donateTransferSuccess.php
@@ -19,9 +19,16 @@ echo display_content_top('list_content');
 echo start_content_tag();
 
 ?>
-Texte qui explique comment faire un virement (IBAN,...) Quide de chf/eur ?
+Merci pour votre soutien !<br>
+Coordonnées bancaires de camptocamp-association :<br>
+CREDIT COOPERATIF GRENOBLE<br>
+Domiciliation : CREDITCOOP Grenoble<br>
+RIB : 42559 00016 21029256603 07<br>
+IBAN : FR76 4255 9000 1621 0292 5660 307<br>
+BIC : CCOPFRPPXXX<br>
 
-penser � changer les boutons de gauche pour renvoyer vers de l'aide via email ou autre
+<br>
+penser à changer les boutons de gauche pour renvoyer vers de l'aide via email ou autre
 
 <?php
 echo end_content_tag();
