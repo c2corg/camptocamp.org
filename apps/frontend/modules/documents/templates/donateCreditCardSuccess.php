27ba6330a274f82bf244039793cd780a5e95ddee
Lionel Besson <lionel.besson@gmail.com>
2015-11-16 21:31:27 +0100

    text

--- apps/frontend/modules/documents/templates/donateCreditCardSuccess.php
+++ apps/frontend/modules/documents/templates/donateCreditCardSuccess.php
@@ -31,9 +31,7 @@ Merci de votre soutien pour camptocamp !
 <p>
 Pour proc&eacute;der au paiement de <?php echo $amount; ?> <?php echo ($currency == 'CHF' ? 'CHF' : '&euro;'); ?> par carte bancaire en faveur de l'association Camptocamp, cliquez sur le bouton qui s'affiche ci-dessous. Vous serez alors redirig&eacute; vers le site de paiement en ligne. 
 </p>
-<p>                                                     
-penser &agrave; changer les boutons de gauche pour renvoyer vers de l'aide via email ou autre
-</p>                     
+ 
 <?php
 echo '<form method="post" action="' . sfConfig::get('app_donate_vads_url') . '">';
 foreach ($params as $key => $value)
