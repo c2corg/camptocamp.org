fd2762675b7d016e658a5663db850976184029ce
lbesson <lionel.besson@gmail.com>
2015-11-17 22:55:47 +0100

    Donation design

--- apps/frontend/modules/common/templates/_donate.php
+++ apps/frontend/modules/common/templates/_donate.php
@@ -1,20 +1,18 @@
 <aside id="donate" style="display:none">
-
+<button class="donate-notnow cross">X</button>
 <span>La mise en place de la nouvelle version de c2c nécessite un financement supplémentaire de 110.000 € (<a href="http://www.pre-prod.dev.camptocamp.org/articles/526958/fr/nouvelle-version-v6-de-camptocamp-financement-participatif">explications</a>). Nous faisons donc appel à vous, à votre générosité, pour boucler le projet. Si chacun de vous donne 2 €, la levée de fonds sera bouclée en 2 semaine! Camptocamp.org, c'est votre site, une mine d'infos pour passionnés des cimes, entièrement gratuit, géré par des bénévoles (association). Vous trouvez Camptocamp utile ? Alors aidez nous à le faire vivre !</span>
 
-<div>
+<div class="donate-quote">
   <img class="donate-image" />
   <span class="people"></span>, <span class="role"></span>
-</div>
-<div>
   <span class="presentation"></span>
 </div>
 
-<button class="donate-notnow">X</button>
+<span class="donate-buttons">
 <button class="donate-notnow">Me le rappeler plus tard</button>
 <button class="donate-never">Non merci</button>
 <button class="donate-change">Autre bannière</button>
-
+</span>
 <?php
 use_helper('Link');
 if (sfConfig::get('app_production') != 1)
